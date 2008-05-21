8e1b6a3521e89b84b125aa8117856dcf35fd9698
Daniel Bildhauer <dbildh@uni-koblenz.de>
2008-05-21 12:42:48 +0000

    Added "id" as reserved tg word

--- src/de/uni_koblenz/jgralab/schema/Schema.java
+++ src/de/uni_koblenz/jgralab/schema/Schema.java
@@ -74,7 +74,7 @@ public interface Schema extends NamedElement {
 					"EnumDomain", "f", "from", "Graph", "GraphClass",
 					"Integer", "List", "Long", "Object", "Package",
 					"RecordDomain", "redefines", "role", "Schema", "Set",
-					"String", "t", "to", "VertexClass" }));
+					"String", "t", "to", "VertexClass", "id" }));
 
 	/**
 	 * sets the factory that is used to create graphs, vertices and edges
