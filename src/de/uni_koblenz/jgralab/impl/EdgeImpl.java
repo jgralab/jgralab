e29a345ceb6923ac4415a9dc20ef911b3ad3cf30
Daniel Bildhauer <dbildh@uni-koblenz.de>
2008-05-26 09:07:13 +0000

    Fixed error that causes the following problem: Creating a vertex v with an outgoing edge e1 and an incomming one e2. Iteration over all outgoing edges of v with v.getFirstEdge(EdgeDirection.OUT) and e.getNextEdge(EdgeDirection.OUT) leads to an iteration over e1 and e2.

--- src/de/uni_koblenz/jgralab/impl/EdgeImpl.java
+++ src/de/uni_koblenz/jgralab/impl/EdgeImpl.java
@@ -93,7 +93,7 @@ public abstract class EdgeImpl extends GraphElementImpl implements Edge {
 
 	@Override
 	public Edge getNextEdge(EdgeDirection orientation) {
-		return myGraph.getNextEdge(this);
+		return myGraph.getNextEdge(this, orientation);
 	}
 
 	@Override
