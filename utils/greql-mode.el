318146c1f9f406a61963192b758fce7fe216132b
Tassilo Horn <horn@uni-koblenz.de>
2008-11-12 15:08:21 +0000

    Merged changes from list impl branch into trunk

--- utils/greql-mode.el
+++ utils/greql-mode.el
@@ -20,115 +20,240 @@
 
 ;;; Commentary:
 
-;; Major mode for editing GReQL2 files with Emacs
+;; Major mode for editing GReQL2 files with Emacs and executing queries.
 
 
-;;; History:
-;; <2007-07-30 Mon>: First version
+;;; Version:
+;; <2008-11-09 Sun 11:38>
 
 ;;; Code:
 
-(define-generic-mode greql-mode
+(defvar greql-keywords
+  '("E" "V" "as" "bag" "eSubgraph" "end" "exists!" "exists" "forall"
+    "from" "in" "let" "list" "path" "pathSystem" "rec" "report"
+    "reportBag" "reportSet" "set" "store" "tup" "using" "vSubgraph"
+    "where" "with")
+  "GReQL keywords that should be highlighted.")
+
+(put 'greql-keywords 'risky-local-variable-p t)
+
+(defvar greql-functions
+  '("and" "avg" "children" "contains" "count" "degree" "depth"
+    "difference" "distance" "dividedBy" "edgesConnected" "edgesFrom"
+    "edgesTo" "edgeTrace" "edgeTypeSet" "endVertex" "equals"
+    "extractPath" "getEdge" "getValue" "getVertex" "grEqual" "grThan"
+    "hasAttribute" "hasType" "id" "inDegree" "innerNodes"
+    "intersection" "isAcyclic" "isA" "isCycle" "isIn" "isIsolated"
+    "isLoop" "isNeighbour" "isParallel" "isPrime" "isReachable"
+    "isSibling" "isSubPathOfPath" "isSubSet" "isSuperSet" "isTrail"
+    "isTree" "leaves" "leEqual" "leThan" "matches" "maxPathLength"
+    "minPathLength" "minus" "modulo" "nequals" "nodeTrace" "not"
+    "nthElement" "or" "outDegree" "parent" "pathConcat" "pathLength"
+    "pathSystem" "plus" "pos" "reachableVertices" "reMatch"
+    "schemaFunctions" "siblings" "squareRoot" "startVertex"
+    "subtypes" "sum" "supertypes" "symDifference" "times" "toString"
+    "type" "typeName" "typeSet" "types" "uminus" "union"
+    "vertexTypeSet" "weight" "xor")
+  "GReQL functions that should be highlighted.")
+
+(put 'greql-functions 'risky-local-variable-p t)
+
+(dolist (ext '("\\.greqlquery$" "\\.grq$" "\\.greql$"))
+  (add-to-list 'auto-mode-alist (cons ext 'greql-mode)))
+
+(defvar greql-tab-width 2
+  "Distance between tab stops (for display of tab characters), in columns.")
+
+(defvar greql-script-program "~/bin/greqlscript"
+  "The program to execute GReQL queries.")
+
+(define-derived-mode greql-mode text-mode "GReQL"
+  "A major mode for GReQL2."
   ;; Comments
-  '(("//" . nil) ("/*" . "*/"))
+  (setq comment-start "//"
+	comment-start-skip "/\\*"
+	comment-end "\\*/")
   ;; Keywords
-  '("E" "V" "as" "bag" "eSubgraph" "end" "exists!" "exists" "forall" "from" "in"
-    "let" "list" "path" "pathSystem" "rec" "report" "reportBag" "reportSet"
-    "set" "store" "tup" "using" "vSubgraph" "where" "with")
-  ;; Additional expressions to highlight 
-  '(
-    ;; Funlib Functions
-    ("\\<and\\>" . font-lock-builtin-face)
-    ("\\<avg\\>" . font-lock-builtin-face)
-    ("\\<children\\>" . font-lock-builtin-face)
-    ("\\<contains\\>" . font-lock-builtin-face)
-    ("\\<count\\>" . font-lock-builtin-face)
-    ("\\<degree\\>" . font-lock-builtin-face)
-    ("\\<depth\\>" . font-lock-builtin-face)
-    ("\\<difference\\>" . font-lock-builtin-face)
-    ("\\<distance\\>" . font-lock-builtin-face)
-    ("\\<dividedBy\\>" . font-lock-builtin-face)
-    ("\\<edgesConnected\\>" . font-lock-builtin-face)
-    ("\\<edgesFrom\\>" . font-lock-builtin-face)
-    ("\\<edgesTo\\>" . font-lock-builtin-face)
-    ("\\<edgeTrace\\>" . font-lock-builtin-face)
-    ("\\<edgeTypeSet\\>" . font-lock-builtin-face)
-    ("\\<endVertex\\>" . font-lock-builtin-face)
-    ("\\<equals\\>" . font-lock-builtin-face)
-    ("\\<extractPath\\>" . font-lock-builtin-face)
-    ("\\<getEdge\\>" . font-lock-builtin-face)
-    ("\\<getValue\\>" . font-lock-builtin-face)
-    ("\\<getVertex\\>" . font-lock-builtin-face)
-    ("\\<grEqual\\>" . font-lock-builtin-face)
-    ("\\<grThan\\>" . font-lock-builtin-face)
-    ("\\<hasAttribute\\>" . font-lock-builtin-face)
-    ("\\<hasType\\>" . font-lock-builtin-face)
-    ("\\<id\\>" . font-lock-builtin-face)
-    ("\\<inDegree\\>" . font-lock-builtin-face)
-    ("\\<innerNodes\\>" . font-lock-builtin-face)
-    ("\\<intersection\\>" . font-lock-builtin-face)
-    ("\\<isAcyclic\\>" . font-lock-builtin-face)
-    ("\\<isA\\>" . font-lock-builtin-face)
-    ("\\<isCycle\\>" . font-lock-builtin-face)
-    ("\\<isIn\\>" . font-lock-builtin-face)
-    ("\\<isIsolated\\>" . font-lock-builtin-face)
-    ("\\<isLoop\\>" . font-lock-builtin-face)
-    ("\\<isNeighbour\\>" . font-lock-builtin-face)
-    ("\\<isParallel\\>" . font-lock-builtin-face)
-    ("\\<isPrime\\>" . font-lock-builtin-face)
-    ("\\<isReachable\\>" . font-lock-builtin-face)
-    ("\\<isSibling\\>" . font-lock-builtin-face)
-    ("\\<isSubPathOfPath\\>" . font-lock-builtin-face)
-    ("\\<isSubSet\\>" . font-lock-builtin-face)
-    ("\\<isSuperSet\\>" . font-lock-builtin-face)
-    ("\\<isTrail\\>" . font-lock-builtin-face)
-    ("\\<isTree\\>" . font-lock-builtin-face)
-    ("\\<leaves\\>" . font-lock-builtin-face)
-    ("\\<leEqual\\>" . font-lock-builtin-face)
-    ("\\<leThan\\>" . font-lock-builtin-face)
-    ("\\<matches\\>" . font-lock-builtin-face)
-    ("\\<maxPathLength\\>" . font-lock-builtin-face)
-    ("\\<minPathLength\\>" . font-lock-builtin-face)
-    ("\\<minus\\>" . font-lock-builtin-face)
-    ("\\<modulo\\>" . font-lock-builtin-face)
-    ("\\<nequals\\>" . font-lock-builtin-face)
-    ("\\<nodeTrace\\>" . font-lock-builtin-face)
-    ("\\<not\\>" . font-lock-builtin-face)
-    ("\\<nthElement\\>" . font-lock-builtin-face)
-    ("\\<or\\>" . font-lock-builtin-face)
-    ("\\<outDegree\\>" . font-lock-builtin-face)
-    ("\\<package-info\\>" . font-lock-builtin-face)
-    ("\\<parent\\>" . font-lock-builtin-face)
-    ("\\<pathConcat\\>" . font-lock-builtin-face)
-    ("\\<pathLength\\>" . font-lock-builtin-face)
-    ("\\<pathSystem\\>" . font-lock-builtin-face)
-    ("\\<plus\\>" . font-lock-builtin-face)
-    ("\\<pos\\>" . font-lock-builtin-face)
-    ("\\<reachableVertices\\>" . font-lock-builtin-face)
-    ("\\<reMatch\\>" . font-lock-builtin-face)
-    ("\\<schemaFunctions\\>" . font-lock-builtin-face)
-    ("\\<siblings\\>" . font-lock-builtin-face)
-    ("\\<squareRoot\\>" . font-lock-builtin-face)
-    ("\\<startVertex\\>" . font-lock-builtin-face)
-    ("\\<subtypes\\>" . font-lock-builtin-face)
-    ("\\<sum\\>" . font-lock-builtin-face)
-    ("\\<supertypes\\>" . font-lock-builtin-face)
-    ("\\<symDifference\\>" . font-lock-builtin-face)
-    ("\\<times\\>" . font-lock-builtin-face)
-    ("\\<toString\\>" . font-lock-builtin-face)
-    ("\\<type\\>" . font-lock-builtin-face)
-    ("\\<typeName\\>" . font-lock-builtin-face)
-    ("\\<typeSet\\>" . font-lock-builtin-face)
-    ("\\<types\\>" . font-lock-builtin-face)
-    ("\\<uminus\\>" . font-lock-builtin-face)
-    ("\\<union\\>" . font-lock-builtin-face)
-    ("\\<vertexTypeSet\\>" . font-lock-builtin-face)
-    ("\\<weight\\>" . font-lock-builtin-face)
-    ("\\<xor\\>" . font-lock-builtin-face))
-  ;; Enable greql-mode for files matching this patterns
-  '("\\.greqlquery$" "\\.grq$" "\\.greql$")
+  (setq font-lock-defaults
+	(let (list)
+	  (push (concat "\\_<" (regexp-opt greql-keywords t) "\\_>")
+		list)
+	  (push (cons (concat "\\_<" (regexp-opt greql-functions t) "\\_>")
+		      font-lock-builtin-face)
+		list)
+	  (list list)))
+
+  (setq tab-width greql-tab-width)
   ;; List of functions to be run when mode is activated
-  nil)
+  (define-key greql-mode-map (kbd "M-TAB")   'greql-complete)
+  (define-key greql-mode-map (kbd "C-c C-v") 'greql-complete-vertexclass)
+  (define-key greql-mode-map (kbd "C-c C-e") 'greql-complete-edgeclass)
+  (define-key greql-mode-map (kbd "C-c C-d") 'greql-complete-domain)
+  (define-key greql-mode-map (kbd "C-c C-s") 'greql-set-graph)
+  (define-key greql-mode-map (kbd "C-c C-c") 'greql-execute))
+
+(defvar greql-graph nil
+  "The graph which is used to extract schema information on which
+queries are evaluated.  Set it with `greql-set-graph'.")
+
+(make-variable-buffer-local 'greql-graph)
+
+(defvar greql-schema-alist nil)
+(make-variable-buffer-local 'greql-schema-alist)
+
+(defun greql-set-graph (graph)
+  "Set `greql-graph' to GRAPH and parse it with
+`greql-parse-schema'."
+  (interactive "fGraph file: ")
+  (setq greql-graph graph)
+  (let ((g greql-graph)
+	schema-alist)
+    (with-temp-buffer
+      (insert-file-contents g)
+      (setq schema-alist (greql-parse-schema)))
+    (setq greql-schema-alist schema-alist))
+  ;; add keywords and functions, too
+  (dolist (key greql-keywords)
+    (setq greql-schema-alist (cons (cons 'keyword key)
+				   greql-schema-alist)))
+  (dolist (fun greql-functions)
+    (setq greql-schema-alist (cons (cons 'function fun)
+				   greql-schema-alist))))
+
+(defun greql-parse-schema ()
+  "Parse `greql-graph' and extract schema information into
+`greql-schema-alist'."
+  (goto-char (point-min))
+  (let ((current-package "")
+	schema-alist)
+    (while (re-search-forward
+	    (rx bol   ;; Anchor at the beginning of line.
+		(and
+		 ;; First, there may be the keyword abstract followed by
+		 ;; whitespaces.
+		 (zero-or-one "abstract")
+		 (zero-or-more (syntax whitespace))
+		 ;; Now comes the Meta-Meta-Model type.  We need it later, so
+		 ;; extract it.
+		 (group
+		  (or "VertexClass" "Package" "AggregationClass" "CompsitionClass"
+		      "EdgeClass" "Schema" "GraphClass"
+		      (and (one-or-more (or (syntax word) (any "_."))) "Domain")))
+		 ;; Then there may be whitespaces again...
+		 (one-or-more (syntax whitespace))
+		 ;; Now comes the Meta-Modes type name.  That's needed, too.
+		 (group (one-or-more (or (syntax word) (any "_."))))))
+	    nil t)
+      (let ((vc-or-pkg (buffer-substring-no-properties (match-beginning 1)
+						       (match-end 1)))
+	    (name (buffer-substring-no-properties (match-beginning 2)
+						  (match-end 2))))
+	(if (string= vc-or-pkg "Package")
+	    ;; All following elements belong to this package, so make it
+	    ;; current.
+	    (setq current-package (concat name "."))
+	  ;; That's a usual element, so add a cons of the form (MM-TYPE
+	  ;; . M-TYPE), where MM-TYPE is the Meta-Meta-Model type as symbol and
+	  ;; M-TYPE is the Meta-Model type as string.
+	  (setq schema-alist (cons (cons (intern vc-or-pkg)
+					 (concat current-package name))
+				   schema-alist)))))
+    schema-alist))
+
+(defun greql-completion-list (&optional types)
+  (when greql-schema-alist
+    (let (completions)
+      (dolist (cell greql-schema-alist)
+	(when (or (null types) (member (car cell) types))
+	  (let ((elem (cdr cell)))
+	    (setq completions (cons elem completions)))))
+      completions)))
+
+(defun greql-complete-1 (&optional types)
+  (let* ((window (get-buffer-window "*Completions*" 0))
+	 (beg (save-excursion
+		(+ 1 (or (re-search-backward "[^[:word:]._]" nil t) 0))))
+	 (word (buffer-substring-no-properties beg (point)))
+	 (compl (try-completion word
+				(greql-completion-list types))))
+    (if (and (eq last-command this-command)
+	     window (window-live-p window) (window-buffer window)
+	     (buffer-name (window-buffer window)))
+	;; If this command was repeated, and there's a fresh completion window
+	;; with a live buffer, and this command is repeated, scroll that
+	;; window.
+	(with-current-buffer (window-buffer window)
+	  (if (pos-visible-in-window-p (point-max) window)
+	      (set-window-start window (point-min))
+	    (save-selected-window
+	      (select-window window)
+	      (scroll-up))))
+      (cond
+       ((null compl)
+	(message "No completion possible.  Did you set a graph?"))
+       ((stringp compl)
+	(if (string= word compl)
+	    ;; Show completion buffer
+	    (let ((list (all-completions word
+					 (greql-completion-list types))))
+	      (setq list (sort list 'string<))
+	      (with-output-to-temp-buffer "*Completions*"
+		    (display-completion-list list word)))
+	  ;; Complete
+	  (delete-region beg (point))
+	  (insert compl)
+	  ;; close completion buffer if there's one
+	  (let ((win (get-buffer-window "*Completions*" 0)))
+	    (if win (quit-window nil win)))))
+       (t (message "That's the only possible completion."))))))
+
+(defun greql-complete ()
+  "Complete word at point somehow intelligently."
+  (interactive)
+  (cond
+   ((greql-vertex-set-expression-p) (greql-complete-vertexclass))
+   ((or (greql-edge-set-expression-p)
+	(greql-edge-restriction-p))
+    (greql-complete-edgeclass))
+   (t (greql-complete-keyword-or-function))))
+
+(defun greql-complete-vertexclass ()
+  (interactive)
+  (greql-complete-1 '(VertexClass)))
+
+(defun greql-complete-edgeclass ()
+  (interactive)
+  (greql-complete-1 '(EdgeClass AggregationClass CompositionClass)))
+
+(defun greql-complete-domain ()
+  (interactive)
+  (greql-complete-1 '(EnumDomain RecordDomain ListDomain SetDomain BagDomain)))
+
+(defun greql-complete-keyword-or-function ()
+  (interactive)
+  (greql-complete-1 '(keyword function)))
+
+(defun greql-execute ()
+  "Execute the query in the current buffer on `greql-graph'."
+  (interactive)
+  (let ((buffer (get-buffer-create "*GReQL*"))
+	(evalstr (buffer-substring-no-properties (point-min) (point-max))))
+    (with-current-buffer buffer
+      (erase-buffer))
+    (start-process "GReQL process" buffer
+		   greql-script-program
+		   "-e" evalstr
+		   "-g" (expand-file-name greql-graph))
+    (display-buffer buffer)))
+
+(defun greql-vertex-set-expression-p ()
+  (looking-back "V{[[:word:]._]*"))
+
+(defun greql-edge-set-expression-p ()
+  (looking-back "E{[[:word:]._]*"))
+
+(defun greql-edge-restriction-p ()
+  (looking-back "--{[[:word:]._]*"))
 
 (provide 'greql-mode)
 
