4c34153be73184789efba28fa4f0efa56cd6252b
Tassilo Horn <horn@uni-koblenz.de>
2008-12-04 12:51:39 +0000

    new function

---
+++ src/de/uni_koblenz/jgralab/greql2/funlib/KeySet.java
@@ -0,0 +1,65 @@
+package de.uni_koblenz.jgralab.greql2.funlib;
+
+import java.util.ArrayList;
+
+import de.uni_koblenz.jgralab.BooleanGraphMarker;
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.greql2.exception.EvaluateException;
+import de.uni_koblenz.jgralab.greql2.exception.WrongFunctionParameterException;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueType;
+
+/**
+ * Retrieves the value associated with a given key in the given map.
+ *
+ * <dl>
+ * <dt><b>GReQL-signature</b></dt>
+ * <dd><code>SET keySet(m:MAP)</code></dd>
+ * <dd>&nbsp;</dd>
+ * </dl>
+ * <dl>
+ * <dt></dt>
+ * <dd>
+ * <dl>
+ * <dt><b>Parameters:</b></dt>
+ * <dd><code>m</code> - map to be used</dd>
+ * <dt><b>Returns:</b></dt>
+ * <dd>the SET of keys in <code>m</code>.</dd>
+ * </dl>
+ * </dd>
+ * </dl>
+ *
+ * @author ist@uni-koblenz.de
+ *
+ */
+public class KeySet extends AbstractGreql2Function {
+	{
+		JValueType[][] x = { { JValueType.MAP } };
+		signatures = x;
+	}
+
+	@Override
+	public JValue evaluate(Graph graph, BooleanGraphMarker subgraph,
+			JValue[] arguments) throws EvaluateException {
+		if (checkArguments(arguments) == -1) {
+			throw new WrongFunctionParameterException(this, null, arguments);
+		}
+		return arguments[0].toJValueMap().keySet();
+	}
+
+	@Override
+	public long getEstimatedCardinality(int inElements) {
+		return 1;
+	}
+
+	@Override
+	public long getEstimatedCosts(ArrayList<Long> inElements) {
+		return 1;
+	}
+
+	@Override
+	public double getSelectivity() {
+		return 1;
+	}
+
+}
