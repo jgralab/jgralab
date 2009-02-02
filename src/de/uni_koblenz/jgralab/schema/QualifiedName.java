dd778e6937569be1ad01eca2789f34da671a8f32
Grégory Catellani <gcatellani@uni-koblenz.de>
2009-02-02 23:15:47 +0000

    Added/completed specifications for:
    _AttributedElementClass_:
    
    - Class
    - getOwnAttribute(String)
    - getOwnAttributeCount()
    - getOwnAttributeList()
    - getPackage()
    - getPackageName()
    
    _GraphClass_:
    
    - Class
    
    _NamedElement_:
    
    - Class
    - getPackageName()
    
    _QualifiedName_:
    
    - Class
    - Fields: packageName, simpleName, qualifiedName, uniqueName
    - QualifiedName(String)
    - getQualifiedName()
    
    
    Minor modifications to other specifications.

--- src/de/uni_koblenz/jgralab/schema/QualifiedName.java
+++ src/de/uni_koblenz/jgralab/schema/QualifiedName.java
@@ -32,18 +32,117 @@ import de.uni_koblenz.jgralab.schema.exception.SchemaException;
 /**
  * QualifiedName is used to identify any Schema element (Graph/Vertex/Edge
  * classes, Packages, Domains, etc.).
- *
+ * 
+ * <p>
+ * A <code>QualifiedName</code> stores informations about an element´s name and
+ * package:
+ * <ul>
+ * <li><code>packageName</code> the fully qualified name of the package the
+ * element is located in.</li>
+ * <li><code>simpleName</code> unique name of the element in the package without
+ * the fully qualified package name</li>
+ * <li><code>qualifiedName</code> the fully qualified name of an element in the
+ * schema<br />
+ * <code>qualifiedName = packageName + "." + simpleName</code></li>
+ * <li><code>uniqueName</code> the unique name of the element in the schema.<br />
+ * If there is only one class in the schema with this short name, the unique
+ * name is the short name. Otherwise, the unique name is the same as the
+ * qualified name.</li>
+ * </ul>
+ * </p>
+ * 
+ * <p>
+ * <b>Note:</b> in the following, <code>qname</code>, and <code>qname'</code> , will
+ * represent the states of the given <code>QualifiedName</code> before,
+ * respectively after, any operation.
+ * </p>
+ * 
+ * <p>
+ * <b>Note:</b> in the following it is understood that method arguments differ
+ * from <code>null</code>. Therefore there will be no preconditions addressing
+ * this matter.
+ * </p>
+ * 
  * @author ist@uni-koblenz.de
  */
 public class QualifiedName implements Comparable<QualifiedName> {
+	/**
+	 * The fully qualified name of the package an element is located in.
+	 */
 	private String packageName;
 
+	/**
+	 * Unique name of an element in a package without the fully qualified
+	 * package name.
+	 */
 	private String simpleName;
 
+	/**
+	 * The fully qualified name of an element in a schema. It is composed of the
+	 * {@link #packageName name of the package} the element is located in and
+	 * the {@link #simpleName simple name} of the element. <br />
+	 * <code>qualifiedName = packageName + "." + simpleName</code>
+	 */
 	private String qualifiedName;
 
+	/**
+	 * The unique name of an element in a schema. If there is only one class in
+	 * the schema with this short name, the unique name is the short name.
+	 * Otherwise, the unique name is the same as the qualified name.
+	 */
 	private String uniqueName;
 
+/**
+	 * Creates a new instance of <code>QualifiedName</code>.
+	 * 
+	 * <p>
+	 * <b>Pattern:</b> <code>qname = new getQualifiedName(qn);</code>
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>Preconditions:</b> none //TODO: Potential subject to change
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>Postconditions:</b>
+	 * <ul>
+	 * <li><code>qname'.packageName</code> has the following value:
+	 * <ul>
+	 * <li>empty String if one of the following occurs:
+	 * <ul>
+	 * <li><code>qn</code> starts with the character sequence "List<", representing a <code>QualifiedName</code> for an element from the <code>ListDomain</code></li>
+	 * <li><code>qn</code> starts with the character sequence "Set<", representing a <code>QualifiedName</code> for an element from the <code>SetDomain</code></li>
+	 * <li><code>qn.lastIndexOf(".") == 0</code>, representing a <code>QualifiedName</code> for an element not contained in any package</li>
+	 * <li><code>qn</code> is an empty String, representing a <code>QualifiedName</code> for an element not contained in any package and without a simple name</li>
+	 * </ul>
+	 * </li>
+	 * <li><code>qn.substring(0, qn.lastIndexOf("."))</code>, the fully qualified name of the package the
+	 * element is located in.<br />The last "." character seperates the <code>packageName</code> from the <code>simpleName</code>.</li>
+	 * </ul>
+	 * </li>
+	 * <li><code>qname'.simpleName</code> has the following value:</li>
+	 * <ul>
+	 * <li>empty String if one of the following occurs:</li>
+	 * <ul>
+	 * <li><code>qn.substring(qn.lastIndexOf(".") + 1)</code> is an empty String, representing a <code>QualifiedName</code> for an element which has no simple name</li> //TODO: Potential subject to change
+	 * <li><code>qn</code> is an empty String, representing a <code>QualifiedName</code> for an element not contained in any package and without a simple name</li>
+	 * </ul>
+	 * <li><code>(subString =)qn.substring(qn.lastIndexOf(".") + 1) && subString.length > 0</code>, the unique name of an element in a package without
+	 * the fully qualified package name</li>
+	 * </ul>
+	 * <li><code>qname'.qualifiedName</code> has the following value:</li>
+	 * <ul>
+	 * <li>empty String, if <code>qn</code> is an empty String, representing a <code>QualifiedName</code> for an element not contained in any package and without a simple name</li>
+	 * <li>equals <code>simpleName</code>, representing a <code>QualifiedName</code> for an element not contained in any package.<br />Implies that: <code>qname'.packageName.equals("")</code></li>
+	 * <li>equals <code>qn</code>, representing a <code>QualifiendName</code> for an element contained in a package and which has a simple name.</li>
+	 * </ul>
+	 * <li><code>qname'.uniqueName.equals(qname'.simpleName)</code><br />This holds for as long as there is only one class in
+	 * the schema with this short name, which is the case upon instantiation. Otherwise, the unique name is the same as the qualified name.</li>
+	 * </p>
+	 * 
+	 * @param qn
+	 *            an element´s fully qualified name
+	 */
 	public QualifiedName(String qn) {
 		if (qn.startsWith("List<") || qn.startsWith("Set<")) {
 			setPackageName("");
@@ -91,6 +190,33 @@ public class QualifiedName implements Comparable<QualifiedName> {
 		return packageName;
 	}
 
+	/**
+	 * 
+	 * Returns the fully qualified name of an element with this
+	 * <code>QualifiedName</code>.
+	 * 
+	 * <p>
+	 * <b>Pattern:</b> <code>qn = qname.getQualifiedName();</code>
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>Preconditions:</b> none
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>Postconditions:</b>
+	 * <ul>
+	 * <li><code>qn != null</code></li>
+	 * <li><code>qn.length >= 0</li></li>
+	 * <li><code>qn</code> is the fully qualified name of an element with this
+	 * <code>QualifiedName</code><br />
+	 * <code>qn = qname.packageName + "." + qname.simpleName</code></li>
+	 * </ul>
+	 * </p>
+	 * 
+	 * @return the fully qualified name of an element with this
+	 *         <code>QualifiedName</code>
+	 */
 	public String getQualifiedName() {
 		return qualifiedName;
 	}
@@ -123,7 +249,7 @@ public class QualifiedName implements Comparable<QualifiedName> {
 	 * really unique in the whole schema, the element which should have this
 	 * unique name needs to be specified. It there is any other element in the
 	 * schema which owns the same unique name, a SchemaException is thrown
-	 *
+	 * 
 	 * @param element
 	 *            The element which should be identified with the unique name
 	 * @param uniqueName
