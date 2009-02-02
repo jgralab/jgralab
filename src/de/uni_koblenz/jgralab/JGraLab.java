dce4bb3e4cb33e59444a136047ee1079b0ac7973
Tassilo Horn <horn@uni-koblenz.de>
2009-02-02 15:23:48 +0000

    Save JGraLab version & infos as comment while saving graph.

--- src/de/uni_koblenz/jgralab/JGraLab.java
+++ src/de/uni_koblenz/jgralab/JGraLab.java
@@ -36,6 +36,7 @@ public class JGraLab {
 
 	// look but don't touch, both values are updated automatically
 	private final String revision = "$Revision$";
+
 	private final String buildID = "63";
 	//
 	// to use this information inside the text place $rev for the revision
