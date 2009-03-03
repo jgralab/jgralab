d46c231a89a3d6f5370ffa2ad78fe43dcdbd2bc8
Elisa Ziegler <eziegler@uni-koblenz.de>
2009-03-03 12:37:16 +0000

    finished documentation

--- src/de/uni_koblenz/jgralab/codegenerator/CodeBlock.java
+++ src/de/uni_koblenz/jgralab/codegenerator/CodeBlock.java
@@ -29,24 +29,57 @@ import java.util.Map;
 import java.util.Stack;
 
 /**
- * TODO add comment
+ * basic class to create and change all kind of code segments offering the necessary
+ * methods 
+ * offers the possibilities to create code out of the stored Strings, store 
+ * variables and their values, influence the relationship between different 
+ * <code>CodeBlock</code>s and others  
  * 
  * @author ist@uni-koblenz.de
  * 
  */
 public abstract class CodeBlock {
+	/**
+	 * a map storing variables of <code>this</code> with their corresponding values
+	 */
 	private Map<String, String> variables;
 
+	/**
+	 * stores the lines of code
+	 */
 	private Stack<String> replacementStack;
 
+	/**
+	 * stores the parental <code>CodeList</code> of this,
+	 * if a circle is to be created, by (indirectly) setting <code>this</code> as
+	 * its own <code>parent</code> a new <code>CodeList</code> with the old content
+	 * will be the <code>parent</code> of <code>this</code>
+	 * f.ex. if A is <code>parent</code> of B, B is <code>parent</code> of C, C is 
+	 * about to be set as B´s <code>parent</code> than
+	 * the <code>parent</code> of the <code>parent</code> of B, which is the
+	 * <code>parent</code> of C, will be reset as the a <code>CodeList</code> 
+	 * combining A and the old B
+	 */
 	private CodeList parent;
 
+	/**
+	 * a number defining the number of tabulators that are to be set in front of 
+	 * every line of the content of <code>this</code>
+	 */
 	protected int additionalIndent;
 
+	/**
+	 * creates an empty CodeBlock with <code>null</code> as the parent
+	 */
 	protected CodeBlock() {
 		this(null);
 	}
 
+	/**
+	 * creates an empty CodeBlock
+	 * @param parent the <code>CodeList</code> that will be set as the parent of 
+	 * <code>this</code>
+	 */
 	protected CodeBlock(CodeList parent) {
 		additionalIndent = 0;
 		variables = new HashMap<String, String>();
@@ -56,22 +89,54 @@ public abstract class CodeBlock {
 		}
 	}
 
+	/**
+	 * 
+	 * @param indentLevel defines how many tabulators are set in the beginning of 
+	 * each line
+	 * @return the content of <code>this</code> as a String, new lines and
+	 * tabulators are set accordingly
+	 */
 	public abstract String getCode(int indentLevel);
 
+	/**
+	 * clears <code>this</code>
+	 */
 	public abstract void clear();
 
+	/**
+	 * 
+	 * @return the size of <code>this</code>, where size means the number of 
+	 * elements on the <code>replacementStack</code>
+	 */
 	public abstract int size();
 
+	/**
+	 * puts the content of <code>map</code> into <code>variables</code>
+	 * @param map contains variables and their values
+	 */
 	public void addVariables(Map<String, String> map) {
 		for (String key : map.keySet()) {
 			variables.put(key, map.get(key));
 		}
 	}
 
+	/**
+	 * puts the <code>name<code> and the corresponding <code>value<code> into
+	 * <code>variables</code>
+	 * @param name a variable and 
+	 * @param value its value
+	 */
 	public void setVariable(String name, String value) {
 		variables.put(name, value);
 	}
 
+	/**
+	 * 
+	 * @param name
+	 * @return the value of <code>name<code>, if <code>name</code> is not among
+	 * the stored <code>variables</code> a message of the form *UNDEFINED:name* 
+	 * is returned
+	 */
 	public String getVariable(String name) {
 		if (replacementStack.contains(name)) {
 			return name;
@@ -91,6 +156,15 @@ public abstract class CodeBlock {
 		}
 	}
 
+	/**
+	 * 
+	 * @param line a String that possibly contains a variable
+	 * @return the <code>line</code> where a variable starting and ending with # 
+	 * are replaced by their values stored in the map <code>variables</code>, if
+	 * no variable is part of <code>line</code> than line will returned unchanged
+	 * @throws RuntimeException if an end marker (#) does not, as it is required, 
+	 * follow the start marker (#) of <code>line</code>
+	 */
 	protected String replaceVariables(String line) {
 		if (line != null) {
 			StringBuilder sb = new StringBuilder(line);
@@ -130,7 +204,7 @@ public abstract class CodeBlock {
 
 	/**
 	 * sets <code>newParent</code> as <code>parent</code> of this
-	 * @param newParent
+	 * @param newParent a <code>CodeList</code>
 	 */
 	protected void setParent(CodeList newParent) {
 		parent = newParent;
