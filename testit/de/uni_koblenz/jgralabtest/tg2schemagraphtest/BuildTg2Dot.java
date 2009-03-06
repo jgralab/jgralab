e4477dd889038f69ed6a3af3b1e7180872767b9a
Tassilo Horn <horn@uni-koblenz.de>
2009-03-06 12:29:00 +0000

    Added Sebastians new Tg2SchemagraphTest and adapted it to the new grUML schema.

--- testit/de/uni_koblenz/jgralabtest/tg2schemagraphtest/BuildTg2Dot.java
+++ testit/de/uni_koblenz/jgralabtest/tg2schemagraphtest/BuildTg2Dot.java
@@ -4,7 +4,7 @@ import de.uni_koblenz.jgralab.utilities.tg2dot.Tg2Dot;
 import de.uni_koblenz.jgralab.utilities.tg2schemagraph.Tg2SchemaGraph;
 
 public class BuildTg2Dot {
-	final static String[] testgrumlschema = { "testgrumlschema.tg", "testgrumlgraph" };
+	final static String[] testgrumlschema = { "GrumlSchema.tg", "testgrumlgraph" };
 	final static String[] test0 = { "testschema0.tg", "testgraph0" };
 	final static String[] test1 = { "testschema1.tg", "testgraph1" };
 	final static String[] test2 = { "testschema2.tg", "testgraph2" };
@@ -20,18 +20,18 @@ public class BuildTg2Dot {
 	public static void convert(String[] args) {
 		String[] tg2sgargs = new String[4];
 		tg2sgargs[0] = "-s";
-		tg2sgargs[1] = "de/uni_koblenz/jgralabtest/tg2schemagraphtest/"
+		tg2sgargs[1] = "testit/de/uni_koblenz/jgralabtest/tg2schemagraphtest/"
 				+ args[0];
 		tg2sgargs[2] = "-o";
-		tg2sgargs[3] = "de/uni_koblenz/jgralabtest/tg2schemagraphtest/"
+		tg2sgargs[3] = "testit/de/uni_koblenz/jgralabtest/tg2schemagraphtest/"
 				+ args[1] + ".tg";
 		Tg2SchemaGraph.main(tg2sgargs);
 		String[] tg2dargs = new String[4];
 		tg2dargs[0] = "-g";
-		tg2dargs[1] = "de/uni_koblenz/jgralabtest/tg2schemagraphtest/"
+		tg2dargs[1] = "testit/de/uni_koblenz/jgralabtest/tg2schemagraphtest/"
 				+ args[1] + ".tg";
 		tg2dargs[2] = "-o";
-		tg2dargs[3] = "de/uni_koblenz/jgralabtest/tg2schemagraphtest/"
+		tg2dargs[3] = "testit/de/uni_koblenz/jgralabtest/tg2schemagraphtest/"
 				+ args[1] + ".dot";
 		Tg2Dot.main(tg2dargs);
 	}
