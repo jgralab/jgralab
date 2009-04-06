03c6d7ee34f9dfd7611a65a4d8853a0d5049dfe4
Eckhard Großmann <mmce@uni-koblenz.de>
2009-04-06 15:10:13 +0000

    Preliminary implementation of class Schema2SchemaGraph.

--- src/de/uni_koblenz/jgralab/utilities/tg2schemagraph/Schema2SchemaGraph.java
+++ src/de/uni_koblenz/jgralab/utilities/tg2schemagraph/Schema2SchemaGraph.java
@@ -2,16 +2,22 @@ package de.uni_koblenz.jgralab.utilities.tg2schemagraph;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import de.uni_koblenz.jgralab.WorkInProgress;
 import de.uni_koblenz.jgralab.grumlschema.SchemaGraph;
 import de.uni_koblenz.jgralab.grumlschema.domains.Domain;
 import de.uni_koblenz.jgralab.grumlschema.impl.SchemaGraphImpl;
+import de.uni_koblenz.jgralab.grumlschema.structure.AggregationClass;
 import de.uni_koblenz.jgralab.grumlschema.structure.Attribute;
 import de.uni_koblenz.jgralab.grumlschema.structure.AttributedElementClass;
+import de.uni_koblenz.jgralab.grumlschema.structure.Constraint;
+import de.uni_koblenz.jgralab.grumlschema.structure.EdgeClass;
+import de.uni_koblenz.jgralab.grumlschema.structure.From;
 import de.uni_koblenz.jgralab.grumlschema.structure.GraphClass;
 import de.uni_koblenz.jgralab.grumlschema.structure.Package;
 import de.uni_koblenz.jgralab.grumlschema.structure.Schema;
+import de.uni_koblenz.jgralab.grumlschema.structure.To;
 import de.uni_koblenz.jgralab.grumlschema.structure.VertexClass;
 
 /**
@@ -34,8 +40,9 @@ public class Schema2SchemaGraph {
 
 	private Map<de.uni_koblenz.jgralab.schema.Package, Package> packageMap;
 	private Map<de.uni_koblenz.jgralab.schema.AttributedElementClass, AttributedElementClass> attributedElementClassMap;
+	private Map<de.uni_koblenz.jgralab.schema.VertexClass, VertexClass> vertexClassMap;
+	private Map<de.uni_koblenz.jgralab.schema.EdgeClass, EdgeClass> edgeClassMap;
 	private Map<de.uni_koblenz.jgralab.schema.Domain, Domain> domainMap;
-	private Map<de.uni_koblenz.jgralab.Attribute, Attribute> attributeMap;
 
 	private SchemaGraph schemaGraph;
 
@@ -48,7 +55,8 @@ public class Schema2SchemaGraph {
 		packageMap = new HashMap<de.uni_koblenz.jgralab.schema.Package, Package>();
 		attributedElementClassMap = new HashMap<de.uni_koblenz.jgralab.schema.AttributedElementClass, AttributedElementClass>();
 		domainMap = new HashMap<de.uni_koblenz.jgralab.schema.Domain, Domain>();
-		attributeMap = new HashMap<de.uni_koblenz.jgralab.Attribute, Attribute>();
+		vertexClassMap = new HashMap<de.uni_koblenz.jgralab.schema.VertexClass, VertexClass>();
+		edgeClassMap = new HashMap<de.uni_koblenz.jgralab.schema.EdgeClass, EdgeClass>();
 
 		schemaGraph = new SchemaGraphImpl();
 
@@ -58,7 +66,8 @@ public class Schema2SchemaGraph {
 		packageMap = null;
 		attributedElementClassMap = null;
 		domainMap = null;
-		attributeMap = null;
+		vertexClassMap = null;
+		edgeClassMap = null;
 
 		defaultPackage = null;
 		gDefaultPackage = null;
@@ -82,13 +91,159 @@ public class Schema2SchemaGraph {
 
 		createGraphClass();
 
-		createDefaultPackage();
+		createPackages();
+
+		createDomains();
+
+		createVertexClasses();
+
+		createEdgeClasses();
+
+		createSpecializations();
+
+		createAttributes();
+
+		createConstraints();
+
+		createEdges();
 
 		SchemaGraph schemaGraph = this.schemaGraph;
 
 		tearDown();
 
 		return schemaGraph;
+	}
+
+	private void createEdges() {
+
+		for (Entry<de.uni_koblenz.jgralab.schema.EdgeClass, EdgeClass> entry : edgeClassMap
+				.entrySet()) {
+			createEdges(entry.getKey(), entry.getValue());
+		}
+	}
+
+	private void createEdges(de.uni_koblenz.jgralab.schema.EdgeClass edgeClass,
+			EdgeClass gEdgeClass) {
+
+		VertexClass vertexClass = vertexClassMap.get(edgeClass.getTo());
+		To to = schemaGraph.createTo(gEdgeClass, vertexClass);
+		to.setMin(edgeClass.getToMin());
+		to.setMax(edgeClass.getToMax());
+		to.setRoleName(edgeClass.getToRolename());
+		to.setRedefinedRoles(edgeClass.getRedefinedToRoles());
+
+		vertexClass = vertexClassMap.get(edgeClass.getFrom());
+		From from = schemaGraph.createFrom(gEdgeClass, vertexClass);
+		from.setMin(edgeClass.getFromMin());
+		from.setMax(edgeClass.getFromMax());
+		from.setRoleName(edgeClass.getFromRolename());
+		from.setRedefinedRoles(edgeClass.getRedefinedFromRoles());
+
+	}
+
+	private void createConstraints() {
+		for (Entry<de.uni_koblenz.jgralab.schema.AttributedElementClass, AttributedElementClass> entry : attributedElementClassMap
+				.entrySet()) {
+			createConstraints(entry.getKey(), entry.getValue());
+		}
+	}
+
+	private void createConstraints(
+			de.uni_koblenz.jgralab.schema.AttributedElementClass element,
+			AttributedElementClass gElement) {
+
+		Constraint gConstraint;
+
+		for (de.uni_koblenz.jgralab.schema.Constraint constraint : element
+				.getConstraints()) {
+			gConstraint = schemaGraph.createConstraint();
+			gConstraint.setMessage(constraint.getMessage());
+			gConstraint.setPredicateQuery(constraint.getPredicate());
+			gConstraint.setOffendingElementsQuery(constraint
+					.getOffendingElementsQuery());
+
+			schemaGraph.createHasConstraint(gElement, gConstraint);
+		}
+
+	}
+
+	private void createAttributes() {
+		for (Entry<de.uni_koblenz.jgralab.schema.AttributedElementClass, AttributedElementClass> entry : attributedElementClassMap
+				.entrySet()) {
+			createAttributes(entry.getKey(), entry.getValue());
+		}
+	}
+
+	private void createAttributes(
+			de.uni_koblenz.jgralab.schema.AttributedElementClass element,
+			AttributedElementClass gElement) {
+
+		Attribute gAttribute;
+		Domain gDomain;
+
+		for (de.uni_koblenz.jgralab.Attribute attribute : element
+				.getAttributeList()) {
+
+			gAttribute = schemaGraph.createAttribute();
+			gAttribute.setName(attribute.getName());
+
+			gDomain = domainMap.get(attribute.getDomain());
+			assert (gDomain != null) : "";
+
+			schemaGraph.createHasAttribute(gElement, gAttribute);
+			schemaGraph.createHasDomain(gAttribute, gDomain);
+		}
+	}
+
+	private void createSpecializations() {
+
+		for (Entry<de.uni_koblenz.jgralab.schema.VertexClass, VertexClass> entry : vertexClassMap
+				.entrySet()) {
+			for (de.uni_koblenz.jgralab.schema.AttributedElementClass superClass : entry
+					.getKey().getDirectSuperClasses()) {
+				schemaGraph.createSpecializesVertexClass(entry.getValue(),
+						vertexClassMap.get(superClass));
+			}
+		}
+
+		for (Entry<de.uni_koblenz.jgralab.schema.EdgeClass, EdgeClass> entry : edgeClassMap
+				.entrySet()) {
+			for (de.uni_koblenz.jgralab.schema.AttributedElementClass superClass : entry
+					.getKey().getDirectSuperClasses()) {
+				schemaGraph.createSpecializesEdgeClass(entry.getValue(),
+						edgeClassMap.get(superClass));
+			}
+		}
+	}
+
+	private void createEdgeClasses() {
+
+		for (Entry<de.uni_koblenz.jgralab.schema.Package, Package> entry : packageMap
+				.entrySet()) {
+			createEdgeClasses(entry.getKey(), entry.getValue());
+		}
+	}
+
+	private void createVertexClasses() {
+
+		for (Entry<de.uni_koblenz.jgralab.schema.Package, Package> entry : packageMap
+				.entrySet()) {
+			createVertexClasses(entry.getKey(), entry.getValue());
+		}
+	}
+
+	private void createDomains() {
+
+		for (Entry<de.uni_koblenz.jgralab.schema.Package, Package> entry : packageMap
+				.entrySet()) {
+			createDomains(entry.getKey(), entry.getValue());
+		}
+	}
+
+	private void createPackages() {
+		createDefaultPackage();
+
+		createSubPackages(defaultPackage, gDefaultPackage);
 	}
 
 	private void createDefaultPackage() {
@@ -99,21 +254,51 @@ public class Schema2SchemaGraph {
 		packageMap.put(defaultPackage, gDefaultPackage);
 
 		schemaGraph.createContainsDefaultPackage(gSchema, gDefaultPackage);
-
-		createDomains(defaultPackage, gDefaultPackage);
-
-		createVertexClasses(defaultPackage, gDefaultPackage);
-
-		createEdgeClasses(defaultPackage, gDefaultPackage);
-
-		createSubPackages(defaultPackage, gDefaultPackage);
 	}
 
 	private void createEdgeClasses(
-			de.uni_koblenz.jgralab.schema.Package defaultPackage2,
-			Package defaultPackage3) {
-		// TODO Auto-generated method stub
+			de.uni_koblenz.jgralab.schema.Package Package, Package gPackage) {
 
+		EdgeClass gEdgeClass;
+
+		for (de.uni_koblenz.jgralab.schema.EdgeClass edgeClass : Package
+				.getEdgeClasses().values()) {
+
+			gEdgeClass = createEdgeClass(edgeClass);
+
+			attributedElementClassMap.put(edgeClass, gEdgeClass);
+			edgeClassMap.put(edgeClass, gEdgeClass);
+			schemaGraph.createContainsGraphElementClass(gPackage, gEdgeClass);
+		}
+	}
+
+	private EdgeClass createEdgeClass(
+			de.uni_koblenz.jgralab.schema.EdgeClass edgeClass) {
+
+		EdgeClass gEdgeClass = null;
+
+		if (edgeClass instanceof de.uni_koblenz.jgralab.schema.AggregationClass) {
+			AggregationClass gAggregationClass;
+			if (edgeClass instanceof de.uni_koblenz.jgralab.schema.CompositionClass) {
+				gAggregationClass = schemaGraph.createCompositionClass();
+			} else {
+				gAggregationClass = schemaGraph.createAggregationClass();
+			}
+
+			de.uni_koblenz.jgralab.schema.AggregationClass aggregationClass = (de.uni_koblenz.jgralab.schema.AggregationClass) edgeClass;
+			gAggregationClass.setAggregateFrom(aggregationClass
+					.isAggregateFrom());
+			gEdgeClass = gAggregationClass;
+
+		} else {
+			gEdgeClass = schemaGraph.createEdgeClass();
+		}
+
+		gEdgeClass.setIsAbstract(edgeClass.isAbstract());
+		gEdgeClass.setQualifiedName(edgeClass.getQualifiedName());
+
+		assert (gEdgeClass != null);
+		return gEdgeClass;
 	}
 
 	private void createVertexClasses(
@@ -128,6 +313,7 @@ public class Schema2SchemaGraph {
 			gVertexClass.setQualifiedName(vertexClass.getQualifiedName());
 
 			attributedElementClassMap.put(vertexClass, gVertexClass);
+			vertexClassMap.put(vertexClass, gVertexClass);
 
 			schemaGraph.createContainsGraphElementClass(gPackage, gVertexClass);
 		}
@@ -172,33 +358,27 @@ public class Schema2SchemaGraph {
 
 		if (domain instanceof de.uni_koblenz.jgralab.schema.BooleanDomain) {
 			gDomain = schemaGraph.createBooleanDomain();
-		}
-		if (domain instanceof de.uni_koblenz.jgralab.schema.IntDomain) {
+		} else if (domain instanceof de.uni_koblenz.jgralab.schema.IntDomain) {
 			gDomain = schemaGraph.createIntDomain();
-		}
-		if (domain instanceof de.uni_koblenz.jgralab.schema.LongDomain) {
+		} else if (domain instanceof de.uni_koblenz.jgralab.schema.LongDomain) {
 			gDomain = schemaGraph.createLongDomain();
-		}
-		if (domain instanceof de.uni_koblenz.jgralab.schema.DoubleDomain) {
+		} else if (domain instanceof de.uni_koblenz.jgralab.schema.DoubleDomain) {
 			gDomain = schemaGraph.createDoubleDomain();
-		}
-		if (domain instanceof de.uni_koblenz.jgralab.schema.StringDomain) {
+		} else if (domain instanceof de.uni_koblenz.jgralab.schema.StringDomain) {
 			gDomain = schemaGraph.createStringDomain();
-		}
-		if (domain instanceof de.uni_koblenz.jgralab.schema.RecordDomain) {
+		} else if (domain instanceof de.uni_koblenz.jgralab.schema.RecordDomain) {
 			gDomain = schemaGraph.createRecordDomain();
-		}
-		if (domain instanceof de.uni_koblenz.jgralab.schema.ListDomain) {
+		} else if (domain instanceof de.uni_koblenz.jgralab.schema.ListDomain) {
 			gDomain = schemaGraph.createListDomain();
-		}
-		if (domain instanceof de.uni_koblenz.jgralab.schema.SetDomain) {
+		} else if (domain instanceof de.uni_koblenz.jgralab.schema.SetDomain) {
 			gDomain = schemaGraph.createSetDomain();
-		}
-		if (domain instanceof de.uni_koblenz.jgralab.schema.MapDomain) {
+		} else if (domain instanceof de.uni_koblenz.jgralab.schema.MapDomain) {
 			gDomain = schemaGraph.createMapDomain();
-		}
-		if (domain instanceof de.uni_koblenz.jgralab.schema.EnumDomain) {
+		} else if (domain instanceof de.uni_koblenz.jgralab.schema.EnumDomain) {
 			gDomain = schemaGraph.createEnumDomain();
+		} else {
+			throw new RuntimeException("FIXME: Unforseen domain occured! "
+					+ domain);
 		}
 
 		assert (gDomain != null);
