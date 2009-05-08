dd30dfd648fe88c4cc7f4775cd2a923f0179fff1
Tassilo Horn <horn@uni-koblenz.de>
2009-05-08 13:54:44 +0000

    New GReQL function

---
+++ src/de/uni_koblenz/jgralab/greql2/funlib/Split.java
@@ -0,0 +1,126 @@
+/*
+ * JGraLab - The Java graph laboratory
+ * (c) 2006-2008 Institute for Software Technology
+ *               University of Koblenz-Landau, Germany
+ *
+ *               ist@uni-koblenz.de
+ *
+ * Please report bugs to http://serres.uni-koblenz.de/bugzilla
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package de.uni_koblenz.jgralab.greql2.funlib;
+
+import java.util.ArrayList;
+
+import de.uni_koblenz.jgralab.BooleanGraphMarker;
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.greql2.exception.EvaluateException;
+import de.uni_koblenz.jgralab.greql2.exception.WrongFunctionParameterException;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueList;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueType;
+
+/**
+ *
+ * Splits the given string {@code str} at the positions where {@code regex}
+ * matches and returns a list of the splits.
+ *
+ * <dl>
+ * <dt><b>GReQL-signature</b></dt>
+ * <dd><code>LIST keySet(str:STRING, regex:STRING)</code></dd>
+ * <dd>&nbsp;</dd>
+ * </dl>
+ * <dl>
+ * <dt></dt>
+ * <dd>
+ * <dl>
+ * <dt><b>Parameters:</b></dt>
+ * <dd><code>str</code> - the string to split</dd>
+ * <dd><code>regex</code> - the regular expression matching the split positions</dd>
+ * <dt><b>Returns:</b></dt>
+ * <dd>the LIST of splits.</dd>
+ * </dl>
+ * </dd>
+ * </dl>
+ *
+ * @author Tassilo Horn &lt;horn@uni-koblenz.de&gt;
+ *
+ */
+public class Split extends AbstractGreql2Function {
+	{
+		JValueType[][] x = { { JValueType.STRING, JValueType.STRING } };
+		signatures = x;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * de.uni_koblenz.jgralab.greql2.funlib.Greql2Function#evaluate(de.uni_koblenz
+	 * .jgralab.Graph, de.uni_koblenz.jgralab.BooleanGraphMarker,
+	 * de.uni_koblenz.jgralab.greql2.jvalue.JValue[])
+	 */
+	@Override
+	public JValue evaluate(Graph graph, BooleanGraphMarker subgraph,
+			JValue[] arguments) throws EvaluateException {
+		if (checkArguments(arguments) == -1) {
+			throw new WrongFunctionParameterException(this, null, arguments);
+		}
+		String str = arguments[0].toString();
+		String regex = arguments[1].toString();
+		String[] splits = str.split(regex);
+		JValueList result = new JValueList();
+		for (String split : splits) {
+			result.add(new JValue(split));
+		}
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * de.uni_koblenz.jgralab.greql2.funlib.Greql2Function#getEstimatedCardinality
+	 * (int)
+	 */
+	@Override
+	public long getEstimatedCardinality(int inElements) {
+		return 3;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * de.uni_koblenz.jgralab.greql2.funlib.Greql2Function#getEstimatedCosts
+	 * (java.util.ArrayList)
+	 */
+	@Override
+	public long getEstimatedCosts(ArrayList<Long> inElements) {
+		return 2;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see de.uni_koblenz.jgralab.greql2.funlib.Greql2Function#getSelectivity()
+	 */
+	@Override
+	public double getSelectivity() {
+		return 1;
+	}
+
+}
