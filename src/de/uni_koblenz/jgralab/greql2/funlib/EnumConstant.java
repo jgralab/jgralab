7a694390b5f1bff3dba322ed707c31875d8bc89d
Tassilo Horn <horn@uni-koblenz.de>
2009-05-18 15:34:06 +0000

    New function

---
+++ src/de/uni_koblenz/jgralab/greql2/funlib/EnumConstant.java
@@ -0,0 +1,156 @@
+/*
+ * JGraLab - The Java graph laboratory
+ * (c) 2006-2008 Institute for Software Technology
+ *               University of Koblenz-Landau, Germany
+ *
+ *               ist@uni-koblenz.de
+ *
+ * Please report bugs to http://serres.uni-koblenz.de/bugzilla
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package de.uni_koblenz.jgralab.greql2.funlib;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import de.uni_koblenz.jgralab.BooleanGraphMarker;
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.greql2.exception.EvaluateException;
+import de.uni_koblenz.jgralab.greql2.exception.WrongFunctionParameterException;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueType;
+import de.uni_koblenz.jgralab.schema.EnumDomain;
+
+/**
+ * *
+ * <dl>
+ * <dt><b>GReQL-signature</b></dt>
+ * <dd><code>ENUM enumConstant(enumTypeName : STRING, constName : STRING)</code>
+ * </dd>
+ * <dd>&nbsp;</dd>
+ * </dl>
+ * <dl>
+ * <dt></dt>
+ * <dd>
+ * <dl>
+ * <dt><b>Parameters:</b></dt>
+ * <dd><code>enumTypeName</code> - collection or path or pathsystem to check</dd>
+ * <dd><code>constName</code> - object or attributed element to check</dd>
+ * <dt><b>Returns:</b></dt>
+ * <dd>the enum constant</dd>
+ * <dd><code>Null</code> if one of the given parameters is <code>Null</code></dd>
+ * </dl>
+ * </dd>
+ * </dl>
+ *
+ * @author ist@uni-koblenz.de
+ *
+ */
+public class EnumConstant extends AbstractGreql2Function {
+
+	{
+		JValueType[][] x = { { JValueType.STRING, JValueType.STRING } };
+		signatures = x;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * de.uni_koblenz.jgralab.greql2.funlib.Greql2Function#evaluate(de.uni_koblenz
+	 * .jgralab.Graph, de.uni_koblenz.jgralab.BooleanGraphMarker,
+	 * de.uni_koblenz.jgralab.greql2.jvalue.JValue[])
+	 */
+	@Override
+	public JValue evaluate(Graph graph, BooleanGraphMarker subgraph,
+			JValue[] arguments) throws EvaluateException {
+		if (checkArguments(arguments) < 0) {
+			throw new WrongFunctionParameterException(this, null, arguments);
+		}
+		String enumDomainName = arguments[0].toString();
+		String enumConstantName = arguments[1].toString();
+
+		EnumDomain enumDomain = (EnumDomain) graph.getSchema().getDomain(
+				enumDomainName);
+		JValue result = null;
+
+		String enumClassName = enumDomain.getJavaClassName(graph.getSchema()
+				.getPackagePrefix());
+		try {
+			Class<?> myEnum = Class.forName(enumClassName);
+			Method fromString = myEnum.getMethod("fromString",
+					(Class<?>[]) null);
+			Object constant = fromString.invoke(null,
+					new Object[] { enumConstantName });
+			result = new JValue(constant);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * de.uni_koblenz.jgralab.greql2.funlib.Greql2Function#getEstimatedCardinality
+	 * (int)
+	 */
+	@Override
+	public long getEstimatedCardinality(int inElements) {
+		return 1;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * de.uni_koblenz.jgralab.greql2.funlib.Greql2Function#getEstimatedCosts
+	 * (java.util.ArrayList)
+	 */
+	@Override
+	public long getEstimatedCosts(ArrayList<Long> inElements) {
+		return 1;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see de.uni_koblenz.jgralab.greql2.funlib.Greql2Function#getSelectivity()
+	 */
+	@Override
+	public double getSelectivity() {
+		return 1;
+	}
+
+}
