cf278dd42cdf6dfc4ede09e55183f3d551f95dd5
Tassilo Horn <horn@uni-koblenz.de>
2009-05-26 08:37:00 +0000

    Added top-level test suite.

---
+++ testit/de/uni_koblenz/jgralabtest/RunTests.java
@@ -0,0 +1,21 @@
+package de.uni_koblenz.jgralabtest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import de.uni_koblenz.jgralabtest.codegenerator.RunCodeGeneratorTests;
+import de.uni_koblenz.jgralabtest.graphvalidator.RunGraphValidatorTests;
+import de.uni_koblenz.jgralabtest.greql2.RunGreql2Tests;
+import de.uni_koblenz.jgralabtest.schema.RunSchemaTests;
+
+/**
+ * @author ist@uni-koblenz.de
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { RunCoreTests.class, RunSchemaTests.class,
+		RunGraphValidatorTests.class, RunGreql2Tests.class,
+		RunCodeGeneratorTests.class })
+public class RunTests {
+
+}
