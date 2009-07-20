4eaa9f21f1181c781db50b37ce2064d6ad2fcfa2
Tassilo Horn <horn@uni-koblenz.de>
2009-07-20 09:23:51 +0000

    Implemented GReQL function entrySet(jValueMap)

---
+++ src/de/uni_koblenz/jgralab/greql2/funlib/EntrySet.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package de.uni_koblenz.jgralab.greql2.funlib;
+
+import java.util.ArrayList;
+
+import de.uni_koblenz.jgralab.BooleanGraphMarker;
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.greql2.exception.EvaluateException;
+import de.uni_koblenz.jgralab.greql2.exception.WrongFunctionParameterException;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueType;
+
+/**
+ * Retrieves the entries of the given map as set of key-value tuples.
+ * 
+ * <dl>
+ * <dt><b>GReQL-signature</b></dt>
+ * <dd><code>SET entrySet(m:MAP)</code></dd>
+ * <dd>&nbsp;</dd>
+ * </dl>
+ * <dl>
+ * <dt></dt>
+ * <dd>
+ * <dl>
+ * <dt><b>Parameters:</b></dt>
+ * <dd><code>m</code> - map to be used</dd>
+ * <dt><b>Returns:</b></dt>
+ * <dd>the SET of keys in <code>m</code>.</dd>
+ * </dl>
+ * </dd>
+ * </dl>
+ * 
+ * @author ist@uni-koblenz.de
+ * 
+ */
+public class EntrySet extends AbstractGreql2Function {
+	{
+		JValueType[][] x = { { JValueType.MAP } };
+		signatures = x;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.uni_koblenz.jgralab.greql2.funlib.Greql2Function#evaluate(de.uni_koblenz
+	 * .jgralab.Graph, de.uni_koblenz.jgralab.BooleanGraphMarker,
+	 * de.uni_koblenz.jgralab.greql2.jvalue.JValue[])
+	 */
+	@Override
+	public JValue evaluate(Graph graph, BooleanGraphMarker subgraph,
+			JValue[] arguments) throws EvaluateException {
+		if (checkArguments(arguments) < 0) {
+			throw new WrongFunctionParameterException(this, null, arguments);
+		}
+		return arguments[0].toJValueMap().entrySetAsJValueTupleSet();
+	}
+
+	@Override
+	public long getEstimatedCardinality(int inElements) {
+		return 1;
+	}
+
+	@Override
+	public long getEstimatedCosts(ArrayList<Long> inElements) {
+		return 15;
+	}
+
+	@Override
+	public double getSelectivity() {
+		return 1;
+	}
+
+}
