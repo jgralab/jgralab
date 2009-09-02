c5835acc9ab830ef030fb790161cf95f878bc901
Daniel Bildhauer <dbildh@uni-koblenz.de>
2009-09-02 06:35:15 +0000

    Removed classes of manual parser from trunk head

--- src/de/uni_koblenz/jgralab/greql2/parser/SimpleToken.java
+++
@@ -1,9 +0,0 @@
-package de.uni_koblenz.jgralab.greql2.parser;
-
-public class SimpleToken extends Token {
-	
-	public SimpleToken(TokenTypes type, int position, int length) {
-		super(type, position, length);
-	}
-
-}
