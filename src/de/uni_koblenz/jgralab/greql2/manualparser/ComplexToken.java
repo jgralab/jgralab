54b1c812683dbbdcef4f8ae32261659ff5026f68
Daniel Bildhauer <dbildh@uni-koblenz.de>
2009-09-02 12:49:35 +0000

    Added classes of manual greql parser to separate directory

---
+++ src/de/uni_koblenz/jgralab/greql2/manualparser/ComplexToken.java
@@ -0,0 +1,20 @@
+package de.uni_koblenz.jgralab.greql2.manualparser;
+
+public class ComplexToken extends Token {
+	
+	public String value = null;
+	
+	public ComplexToken(TokenTypes type, int offset, int length, String value) {
+		super(type, offset ,length);
+		this.value=value;
+	}
+
+	public boolean isComplex() {
+		return true;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+}
---
+++ src/de/uni_koblenz/jgralab/greql2/manualparser/Token.java
@@ -0,0 +1,34 @@
+package de.uni_koblenz.jgralab.greql2.manualparser;
+
+public abstract class Token {
+
+	public TokenTypes type;
+	
+	private int offset;
+	
+	private int length;
+	
+	public Token(TokenTypes type, int offset, int length) {
+		this.type = type;
+		this.offset = offset;
+		this.length = offset;
+	}
+	
+	public boolean isComplex() {
+		return false;
+	}
+	
+	public String getValue() {
+		return ManualGreqlLexer.getTokenString(type);
+	}
+
+	
+	public int getOffset() {
+		return offset;
+	}
+	
+	public int getLength() {
+		return length;
+	}
+	
+}
