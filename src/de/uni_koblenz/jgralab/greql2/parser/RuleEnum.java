f7de0943daa29c09899c4440dcda342c31c4e3da
Daniel Bildhauer <dbildh@uni-koblenz.de>
2009-09-03 08:23:45 +0000

    Organized imports, removed olf gramamr file

---
+++ src/de/uni_koblenz/jgralab/greql2/parser/RuleEnum.java
@@ -0,0 +1,9 @@
+package de.uni_koblenz.jgralab.greql2.parser;
+
+public enum RuleEnum {
+	ALTERNATIVE_PATH_DESCRIPTION,
+	PATH_EXPRESSION,
+	RESTRICTED_EXPRESSION,
+	EXPRESSION,
+	EXPRESSION_LIST
+}
