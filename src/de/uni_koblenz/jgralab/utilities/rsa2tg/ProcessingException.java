723ea852faedeca83a8ef9ccd6fd2e0b6f2b715c
Eckhard Großmann <mmce@uni-koblenz.de>
2009-09-15 13:14:24 +0000

    Added a ProcessingException, which automatically generates a message what file have been processed and at what line the problem occurred.

---
+++ src/de/uni_koblenz/jgralab/utilities/rsa2tg/ProcessingException.java
@@ -0,0 +1,20 @@
+package de.uni_koblenz.jgralab.utilities.rsa2tg;
+
+import javax.xml.stream.XMLStreamReader;
+
+public class ProcessingException extends RuntimeException {
+
+	public ProcessingException(XMLStreamReader parser, String file,
+			String message) {
+		this(file, parser.getLocation().getLineNumber(), message);
+	}
+
+	public ProcessingException(String file, String message) {
+		super("Unexpected error occured in file '" + file + "'.\n" + message);
+	}
+
+	public ProcessingException(String file, int lineNumber, String message) {
+		super("Unexpected error occured in file '" + file + "' at line "
+				+ lineNumber + ".\n" + message);
+	}
+}
