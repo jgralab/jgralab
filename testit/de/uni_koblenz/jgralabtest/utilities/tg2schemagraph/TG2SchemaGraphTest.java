415619153510c62b1d25795d80866d002bf03966
Eckhard Großmann <mmce@uni-koblenz.de>
2009-09-27 19:38:54 +0000

    Rsa2TG:
    Removed an unnecessary member variable 'filenameXmi' and replaced all references by 'getFileName()'.
    
    TG2SchemaGraphTest:
    
    Correct some failed test due to a GraphIOException caused by FileNotFoundExceptions.
    Made some improvements to the test, so that every used file has it's own test.

--- testit/de/uni_koblenz/jgralabtest/utilities/tg2schemagraph/TG2SchemaGraphTest.java
+++ testit/de/uni_koblenz/jgralabtest/utilities/tg2schemagraph/TG2SchemaGraphTest.java
@@ -1,5 +1,6 @@
 package de.uni_koblenz.jgralabtest.utilities.tg2schemagraph;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import de.uni_koblenz.jgralab.GraphIO;
@@ -10,28 +11,13 @@ import de.uni_koblenz.jgralab.utilities.tg2schemagraph.Schema2SchemaGraph;
 
 public class TG2SchemaGraphTest {
 
-	@Test
-	public void testSchema2SchemaGraph() throws GraphIOException {
+	public void testSchema2SchemaGraph(String tgFile) {
 
-		// All Schema files, with which the Converter should be tested.
-		String[] tgFiles = {
-				"testit/de/uni_koblenz/jgralabtest/tg2schemagraphtest/OsmSchema.rsa.tg",
-				"testit/de/uni_koblenz/jgralabtest/tg2schemagraphtest/GrumlSchema.rsa.tg",
-				"testit/de/uni_koblenz/jgralabtest/tg2schemagraphtest/Test.rsa.tg",
-				"testit/testschemas/citymapschema.tg",
-				"testit/testschemas/ConstrainedSchema.tg",
-				"testit/testschemas/MinimalSchema.tg",
-				"testit/testschemas/VertexTestSchema.tg",
-				"testit/de/uni_koblenz/jgralabtest/tg2schemagraphtest/testschema0.tg",
-				"testit/de/uni_koblenz/jgralabtest/tg2schemagraphtest/testschema1.tg",
-				"testit/testschemas/sclschema.tg" };
-
-		// Loop over all files
-		for (String filename : tgFiles) {
-			// Loads the Schema
-			System.out.println("Testing with: " + filename);
+		// Loads the Schema
+		try {
+			System.out.println("Testing with: " + tgFile);
 			System.out.print("Loading TG and creating Schema ... ");
-			Schema schema = GraphIO.loadSchemaFromFile(filename);
+			Schema schema = GraphIO.loadSchemaFromFile(tgFile);
 			System.out.println("\tdone");
 
 			// Converts the Schema to a SchemaGraph
@@ -44,7 +30,65 @@ public class TG2SchemaGraphTest {
 			System.out.print("Testing ...");
 			new CompareSchemaWithSchemaGraph().compare(schema, schemaGraph);
 			System.out.println("\t\t\t\tdone");
-			System.out.println();
+			System.out.println("Succesful!");
+		} catch (GraphIOException e) {
+			System.err.print("An ERROR occurred!");
+			System.out.println("");
+			Assert.fail(e.toString());
+		} finally {
+			System.out.println("");
 		}
+
+	}
+
+	@Test
+	public void testOsmSchema() {
+		testSchema2SchemaGraph("testit/de/uni_koblenz/jgralabtest/utilities/tg2schemagraph/OsmSchema.rsa.tg");
+
+	}
+
+	@Test
+	public void testGrumlSchema() {
+		testSchema2SchemaGraph("testit/de/uni_koblenz/jgralabtest/utilities/tg2schemagraph/GrumlSchema.rsa.tg");
+	}
+
+	@Test
+	public void testTest() {
+		testSchema2SchemaGraph("testit/de/uni_koblenz/jgralabtest/utilities/tg2schemagraph/Test.rsa.tg");
+	}
+
+	@Test
+	public void testTestSchema0() {
+		testSchema2SchemaGraph("testit/de/uni_koblenz/jgralabtest/utilities/tg2schemagraph/testschema0.tg");
+	}
+
+	@Test
+	public void testTestSchema1() {
+		testSchema2SchemaGraph("testit/de/uni_koblenz/jgralabtest/utilities/tg2schemagraph/testschema1.tg");
+	}
+
+	@Test
+	public void testCityMapSchema() {
+		testSchema2SchemaGraph("testit/testschemas/citymapschema.tg");
+	}
+
+	@Test
+	public void testConstraintSchema() {
+		testSchema2SchemaGraph("testit/testschemas/ConstrainedSchema.tg");
+	}
+
+	@Test
+	public void testMinimalSchema() {
+		testSchema2SchemaGraph("testit/testschemas/MinimalSchema.tg");
+	}
+
+	@Test
+	public void testVertexTestSchema() {
+		testSchema2SchemaGraph("testit/testschemas/VertexTestSchema.tg");
+	}
+
+	@Test
+	public void testMotorWayMapSchema() {
+		testSchema2SchemaGraph("testit/testschemas/motorwaymapschema.tg");
 	}
 }
