7f075f1874242e04ebf78d4ce94f7e4e02eb80ca
Tassilo Horn <horn@uni-koblenz.de>
2009-09-28 07:19:34 +0000

    Write boolean ops in infix notation. More test cases.

--- testit/de/uni_koblenz/jgralabtest/greql2/GreqlSerializationTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/GreqlSerializationTest.java
@@ -37,7 +37,52 @@ public class GreqlSerializationTest {
 	private String[] queries = {
 			"from i : list(1..10) with i*i < 15 report i end",
 			"forall i : list(1..100), i <> 10, i < 90 @ i * i + 1 % 4 = 17",
-			"from i : from i : list(30..40) with i < 35 reportSet i end, x : list(1, 3, 17, 19) with isPrime(x+i) report i, x end" };
+			"from i : from i : list(30..40) with i < 35 reportSet i end, x : list(1, 3, 17, 19) with isPrime(x+i) report i, x end",
+			"from w : list(2..10), x : list(2..10), y : list(2..10), z : list(1..2) "
+					+ "     with isPrime(x + z) and x * x > y and z > x * x "
+					+ "     reportBag w, x, y, z end",
+			"from x : list(1..5),                  "
+					+ "          y : list(21..100)   "
+					+ "     with isPrime(x) and isPrime(y)       "
+					+ "          and isPrime(theElement(  "
+					+ "                        from x : list(12..13),"
+					+ "                             y : list(10..11)"
+					+ "                        with isPrime(y) and isPrime(x+y)"
+					+ "                        reportSet x+y end))"
+					+ "     report x, y, from y : list(21..100),"
+					+ "                       z : from a : list(10..20),"
+					+ "                                b : list(30..60)"
+					+ "                           with a * 3 = b and isPrime(a)"
+					+ "                           reportSet a + b end"
+					+ "                  with isPrime(y+x) and isPrime(z+x)"
+					+ "                  reportSet y, z end                "
+					+ "     end",
+			"from class      : V,          "
+					+ "          superClass : V           "
+					+ "     with count(children(superClass)) > 1                "
+					+ "          and superClass -->+ class "
+					+ "          and (exists mid, mid2 : V,                                  "
+					+ "                      mid -->+ class, "
+					+ "                      mid2 -->+ class "
+					+ "                      @ mid <> mid2) "
+					+ "     reportSet class                              "
+					+ "     end",
+			"from a : V{Variable},                              "
+					+ "              b : V{BagComprehension}                       "
+					+ "         with a -->* b                                      "
+					+ "         reportBag from x : list(1..10),                    "
+					+ "                        y : list(11..20)                    "
+					+ "                   with isPrime(x * x + y - 1)              "
+					+ "                   reportSet x * x + y - 1, x, y, a, b end, "
+					+ "                   a, b end",
+			"from x:list(1..10), y:list(x..13), z:list(1..x), a : set(1, 2, 3), b : z "
+					+ "           with x <> 0 and y <> 0 and z <> null and (b <> z)"
+					+ "           report isPrime(z), isPrime(z*z), isPrime(z+z*z-1), b end",
+			"from u, v, w, x, y, z : set(true, false, null)     "
+					+ "     with ((u xor v) or (w and x and (y or z)) or (y and z) or (u and z) and ((u and x) or (y and w) and (u or v)))     "
+					+ "          or ((u xor v) or (w and x and (y or z)) or (y and z) or (u and z) and ((u and x) or (y and w) and (u or v)))  "
+					+ "          and ((u xor v) or (w and x and (y or z)) or (y and z) or (u and z) and ((u and x) or (y and w) and (u or v))) "
+					+ "     reportSet u, v, w, x, y, z end          " };
 
 	@Test
 	public void testAll() {
