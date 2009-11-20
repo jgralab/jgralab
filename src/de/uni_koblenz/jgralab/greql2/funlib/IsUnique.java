b7be19cdaf24e588ed5b6757926580dbf440899f
Daniel Bildhauer <dbildh@uni-koblenz.de>
2009-11-20 08:49:48 +0000

    added isunique function

---
+++ src/de/uni_koblenz/jgralab/greql2/funlib/IsUnique.java
@@ -0,0 +1,99 @@
+/*
+ * JGraLab - The Java graph laboratory
+ * (c) 2006-2009 Institute for Software Technology
+ *               University of Koblenz-Landau, Germany
+ *
+ *               ist@uni-koblenz.de
+ *
+ * Please report bugs to http://serres.uni-koblenz.de/bugzilla
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package de.uni_koblenz.jgralab.greql2.funlib;
+
+import java.util.ArrayList;
+
+import de.uni_koblenz.jgralab.BooleanGraphMarker;
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.greql2.exception.EvaluateException;
+import de.uni_koblenz.jgralab.greql2.exception.WrongFunctionParameterException;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueCollection;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueSet;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueType;
+
+/**
+ * Checks if all elements in a given collection are unique. 
+ * 
+ * <dl>
+ * <dt><b>GReQL-signature</b></dt>
+ * <dd><code>BOOLEAN isUnique(c:COLLECTION)</code></dd>
+ * <dd>&nbsp;</dd>
+ * </dl>
+ * <dl>
+ * <dt></dt>
+ * <dd>
+ * <dl>
+ * <dt><b>Parameters:</b></dt>
+ * <dd><code>c</code> - collection to check</dd>
+ * <dt><b>Returns:</b></dt>
+ * <dd><code>true</code> if there is no element twice in the collection
+ * </dd>
+ * <dd><code>Null</code> if the collection is <code>Null</code></dd>
+ * <dd><code>false</code> otherwise</dd>
+ * </dl>
+ * </dd>
+ * </dl>
+ * 
+ * @author ist@uni-koblenz.de
+ * 
+ */
+
+public class IsUnique extends AbstractGreql2Function {
+
+	{
+		JValueType[][] x = { { JValueType.COLLECTION } };
+		signatures = x;
+	}
+
+	public JValue evaluate(Graph graph, BooleanGraphMarker subgraph,
+			JValue[] arguments) throws EvaluateException {
+
+		switch (checkArguments(arguments)) {
+		case 0:
+			JValueCollection col = arguments[0].toCollection();
+			if (col.isJValueSet())
+				return new JValue(true);
+			JValueSet set = col.toJValueSet();
+			return new JValue(set.size() == col.size());
+		default:
+			throw new WrongFunctionParameterException(this, arguments);
+		}
+	}
+
+	public long getEstimatedCosts(ArrayList<Long> inElements) {
+		return inElements.size();
+	}
+
+	public double getSelectivity() {
+		return 0.2;
+	}
+
+	public long getEstimatedCardinality(int inElements) {
+		return 1;
+	}
+
+}
