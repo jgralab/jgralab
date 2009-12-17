6499e1736bbe1ace923009bb0a1480a8190e915f
Sascha Strauß <strauss@uni-koblenz.de>
2009-12-17 17:56:21 +0000

    Added generic array graph marker for vertices and edges.

---
+++ src/de/uni_koblenz/jgralab/graphmarker/ArrayGenericEdgeGraphMarker.java
@@ -0,0 +1,35 @@
+package de.uni_koblenz.jgralab.graphmarker;
+
+import de.uni_koblenz.jgralab.Edge;
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.Vertex;
+
+public class ArrayGenericEdgeGraphMarker extends ArrayGenericGraphMarker<Edge> {
+
+	protected ArrayGenericEdgeGraphMarker(Graph graph) {
+		super(graph, graph.getMaxECount());
+	}
+
+	@Override
+	public void edgeDeleted(Edge e) {
+		unmark(e);
+	}
+
+	@Override
+	public void maxEdgeCountIncreased(int newValue) {
+		if (newValue > temporaryAttributes.length) {
+			expand(newValue);
+		}
+	}
+
+	@Override
+	public void maxVertexCountIncreased(int newValue) {
+		// do nothing
+	}
+
+	@Override
+	public void vertexDeleted(Vertex v) {
+		// do nothing
+	}
+
+}
