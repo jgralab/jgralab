3ce7deddb7b73624fbc8385b875798d3845bf168
Sascha Strauß <strauss@uni-koblenz.de>
2009-12-17 14:52:31 +0000

    Added abstract class "AbstractGraphMarker" as superclass for all (future) graph markers. Added abstract class "BitSetGraphMarker" Added "BitSetVertexGraphMarker" Added "BitSetEdgeGraphMarker"

---
+++ src/de/uni_koblenz/jgralab/graphmarker/BitSetEdgeGraphMarker.java
@@ -0,0 +1,23 @@
+package de.uni_koblenz.jgralab.graphmarker;
+
+import de.uni_koblenz.jgralab.Edge;
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.Vertex;
+
+public class BitSetEdgeGraphMarker extends BitSetGraphMarker<Edge> {
+
+	protected BitSetEdgeGraphMarker(Graph graph) {
+		super(graph);
+	}
+
+	@Override
+	public void edgeDeleted(Edge e) {
+		unmark(e);
+	}
+
+	@Override
+	public void vertexDeleted(Vertex v) {
+		// do nothing
+	}
+
+}
