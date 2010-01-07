92872af9bc45b6e4cc0836838e29ca67f05c87bd
Sascha Strauß <strauss@uni-koblenz.de>
2010-01-07 17:03:49 +0000

    added IntegerGraphMarker

---
+++ src/de/uni_koblenz/jgralab/graphmarker/IntegerGraphMarker.java
@@ -0,0 +1,112 @@
+package de.uni_koblenz.jgralab.graphmarker;
+
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.GraphElement;
+import de.uni_koblenz.jgralab.Vertex;
+
+public abstract class IntegerGraphMarker<T extends GraphElement> extends
+		AbstractGraphMarker<T> {
+
+	private static final int DEFAULT_UNMARKED_VALUE = Integer.MIN_VALUE;
+
+	protected int[] temporaryAttributes;
+	protected int marked;
+	protected int unmarkedValue;
+
+	protected IntegerGraphMarker(Graph graph, int size) {
+		super(graph);
+		unmarkedValue = DEFAULT_UNMARKED_VALUE;
+		temporaryAttributes = createNewArray(size);
+	}
+
+	private int[] createNewArray(int size) {
+		int[] newArray = new int[size];
+		for (int i = 0; i < size; i++) {
+			newArray[i] = unmarkedValue;
+		}
+		return newArray;
+	}
+
+	@Override
+	public void clear() {
+		for (int i = 0; i < temporaryAttributes.length; i++) {
+			temporaryAttributes[i] = unmarkedValue;
+		}
+		marked = 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return marked == 0;
+	}
+
+	@Override
+	public boolean isMarked(T graphElement) {
+		assert (graphElement.getGraph() == graph);
+		assert (graphElement.getId() <= (graphElement instanceof Vertex ? graph
+				.getMaxVCount() : graph.getMaxECount()));
+		return temporaryAttributes[graphElement.getId()] != unmarkedValue;
+	}
+
+	/**
+	 * marks the given element with the given value
+	 * 
+	 * @param elem
+	 *            the graph element to mark
+	 * @param value
+	 *            the object that should be used as marking
+	 * @return The previous element the given graph element has been marked
+	 *         with, <code>null</code> if the given element has not been marked.
+	 */
+	public int mark(T graphElement, int value) {
+		assert (graphElement.getGraph() == graph);
+		assert (graphElement.getId() <= (graphElement instanceof Vertex ? graph
+				.getMaxVCount() : graph.getMaxECount()));
+		int out = temporaryAttributes[graphElement.getId()];
+		temporaryAttributes[graphElement.getId()] = value;
+		marked += 1;
+		return out;
+	}
+
+	public int getMark(T graphElement) {
+		assert (graphElement.getGraph() == graph);
+		assert (graphElement.getId() <= (graphElement instanceof Vertex ? graph
+				.getMaxVCount() : graph.getMaxECount()));
+		int out = temporaryAttributes[graphElement.getId()];
+		return out;
+	}
+
+	@Override
+	public boolean removeMark(T graphElement) {
+		assert (graphElement.getGraph() == graph);
+		assert (graphElement.getId() <= (graphElement instanceof Vertex ? graph
+				.getMaxVCount() : graph.getMaxECount()));
+		if (temporaryAttributes[graphElement.getId()] == unmarkedValue) {
+			return false;
+		}
+		temporaryAttributes[graphElement.getId()] = unmarkedValue;
+		marked -= 1;
+		return true;
+	}
+
+	@Override
+	public int size() {
+		return marked;
+	}
+
+	public int maxSize() {
+		return temporaryAttributes.length;
+	}
+
+	protected void expand(int newSize) {
+		assert (newSize > temporaryAttributes.length);
+		int[] newTemporaryAttributes = createNewArray(newSize);
+		System.arraycopy(temporaryAttributes, 0, newTemporaryAttributes, 0,
+				temporaryAttributes.length);
+		// for (int i = 0; i < temporaryAttributes.length; i++) {
+		// newTemporaryAttributes[i] = temporaryAttributes[i];
+		// }
+		temporaryAttributes = newTemporaryAttributes;
+	}
+
+}
