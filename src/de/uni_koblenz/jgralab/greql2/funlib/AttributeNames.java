7734cc6b8e758ee8e27d8a187c4432a528e32272
Daniel Bildhauer <dbildh@uni-koblenz.de>
2010-01-11 09:15:40 +0000

    Added a new GReQL function to determine the names of all attributed of an attributed element or an attributed element class

---
+++ src/de/uni_koblenz/jgralab/greql2/funlib/AttributeNames.java
@@ -0,0 +1,114 @@
+/*
+ * JGraLab - The Java graph laboratory
+ * (c) 2006-2009 Institute for Software Technology
+ *               University of Koblenz-Landau, Germany
+ *
+ *               ist@uni-koblenz.de
+ *
+ * Please report bugs to http://serres.uni-koblenz.de/bugzilla
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package de.uni_koblenz.jgralab.greql2.funlib;
+
+import java.util.ArrayList;
+
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.graphmarker.BooleanGraphMarker;
+import de.uni_koblenz.jgralab.greql2.exception.EvaluateException;
+import de.uni_koblenz.jgralab.greql2.exception.WrongFunctionParameterException;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueSet;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueType;
+import de.uni_koblenz.jgralab.schema.Attribute;
+import de.uni_koblenz.jgralab.schema.AttributedElementClass;
+
+/**
+ * Returns the set of names of the attributes of an attributed element or 
+ * an attributed element class.
+ * 
+ * <dl>
+ * <dt><b>GReQL-signature</b></dt>
+ * <dd><code>SET attributeNames(ae:ATTRIBUTEDELEMENT)</code></dd>
+ * <dd><code>BOOLEAN hasAttribute(ae:ATTRIBUTEDELEMENTCLASS)</code>
+ * </dd>
+ * <dd>&nbsp;</dd>
+ * </dl>
+ * <dl>
+ * <dt></dt>
+ * <dd>
+ * <dl>
+ * <dt><b>Parameters:</b></dt>
+ * <dd><code>ae</code> - attributed element or attributed element clazz to check
+ * </dd>
+ * <dt><b>Returns:</b></dt>
+ * <dd>The set of attribute names valid for the given element</dd>
+ * </dl>
+ * </dd>
+ * </dl>
+ * 
+ * @author ist@uni-koblenz.de
+ * 
+ */
+
+public class AttributeNames extends Greql2Function {
+
+	{
+		JValueType[][] x = {
+				{ JValueType.ATTRIBUTEDELEMENT},
+				{ JValueType.ATTRIBUTEDELEMENTCLASS } };
+		signatures = x;
+
+		description = "Return true, iff the given AttrElem has an attribute with the given name.";
+	}
+
+	@Override
+	public JValue evaluate(Graph graph, BooleanGraphMarker subgraph,
+			JValue[] arguments) throws EvaluateException {
+		AttributedElementClass clazz = null;
+		switch (checkArguments(arguments)) {
+		case 0:
+			clazz = arguments[0].toAttributedElement()
+					.getAttributedElementClass();
+			break;
+		case 1:
+			clazz = arguments[0].toAttributedElementClass();
+			break;
+		default:
+			throw new WrongFunctionParameterException(this, arguments);
+		}
+		JValueSet set = new JValueSet();
+		for (Attribute a : clazz.getAttributeList()) {
+			set.add(new JValue(a.getName()));
+		}
+		return set;
+	}
+
+	@Override
+	public long getEstimatedCosts(ArrayList<Long> inElements) {
+		return 5;
+	}
+
+	@Override
+	public double getSelectivity() {
+		return 1;
+	}
+
+	@Override
+	public long getEstimatedCardinality(int inElements) {
+		return 5;
+	}
+}
