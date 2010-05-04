007a2687cda5c68ff9acbca18a43258763d9727d
Volker Riediger <riediger@uni-koblenz.de>
2010-05-04 05:17:33 +0000

    Added - progress bar - small borders - scrollpane for query editor - worker threads to GReQL "Gui"
    Re-Activated optimizer

--- src/de/uni_koblenz/jgralab/utilities/greqlinterface/GreqlGui.java
+++ src/de/uni_koblenz/jgralab/utilities/greqlinterface/GreqlGui.java
@@ -1,33 +1,38 @@
 package de.uni_koblenz.jgralab.utilities.greqlinterface;
 
-import java.awt.Color;
-import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.regex.Pattern;
 
 import javax.swing.AbstractAction;
-import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
+import javax.swing.BoundedRangeModel;
+import javax.swing.DefaultBoundedRangeModel;
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileFilter;
 
 import de.uni_koblenz.jgralab.Graph;
 import de.uni_koblenz.jgralab.GraphIO;
+import de.uni_koblenz.jgralab.ProgressFunction;
 import de.uni_koblenz.jgralab.WorkInProgress;
 import de.uni_koblenz.jgralab.codegenerator.CodeGeneratorConfiguration;
 import de.uni_koblenz.jgralab.greql2.evaluator.GreqlEvaluator;
 import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
 import de.uni_koblenz.jgralab.greql2.jvalue.JValueHTMLOutputVisitor;
-import de.uni_koblenz.jgralab.impl.ProgressFunctionImpl;
-import de.uni_koblenz.jgralab.utilities.tg2dot.Tg2Dot;
 
 @WorkInProgress(description = "insufficcient result presentation, simplistic GUI, no optimizer control, no load/save functionality, ...", responsibleDevelopers = "horn")
 public class GreqlGui extends JFrame {
@@ -37,23 +42,191 @@ public class GreqlGui extends JFrame {
 
 	private JFileChooser fileChooser;
 
-	private JPanel panel;
+	private JPanel queryPanel, resultPanel;
 	private JTextArea queryArea;
 	private JEditorPane resultPane;
 	private JButton fileSelectionButton, evalQueryButton;
+	private JProgressBar progressBar;
+	private BoundedRangeModel brm;
+
+	class Worker extends Thread implements ProgressFunction {
+		BoundedRangeModel brm;
+		private long totalElements;
+		Exception ex;
+
+		Worker(BoundedRangeModel brm) {
+			this.brm = brm;
+		}
+
+		@Override
+		public void finished() {
+			try {
+				SwingUtilities.invokeAndWait(new Runnable() {
+					@Override
+					public void run() {
+						brm.setValue(brm.getMaximum());
+					}
+				});
+			} catch (InterruptedException e) {
+			} catch (InvocationTargetException e) {
+			}
+		}
+
+		@Override
+		public long getUpdateInterval() {
+			return brm.getMaximum() > totalElements ? 1 : totalElements
+					/ brm.getMaximum();
+		}
+
+		@Override
+		public void init(long totalElements) {
+			this.totalElements = totalElements;
+			try {
+				SwingUtilities.invokeAndWait(new Runnable() {
+					@Override
+					public void run() {
+						brm.setValue(brm.getMinimum());
+					}
+				});
+			} catch (InterruptedException e) {
+			} catch (InvocationTargetException e) {
+			}
+		}
+
+		@Override
+		public void progress(long processedElements) {
+			try {
+				SwingUtilities.invokeAndWait(new Runnable() {
+					@Override
+					public void run() {
+						if (brm.getValue() < brm.getMaximum()) {
+							brm.setValue(brm.getValue() + 1);
+						}
+					}
+				});
+			} catch (InterruptedException e) {
+			} catch (InvocationTargetException e) {
+			}
+		}
+	}
+
+	class GraphLoader extends Worker {
+		private File file;
+
+		GraphLoader(BoundedRangeModel brm, File file) {
+			super(brm);
+			this.file = file;
+		}
+
+		@Override
+		public void run() {
+			try {
+				graph = GraphIO.loadSchemaAndGraphFromFile(file
+						.getCanonicalPath(),
+						CodeGeneratorConfiguration.WITHOUT_TRANSACTIONS, this);
+			} catch (Exception e1) {
+				graph = null;
+				ex = e1;
+			}
+			try {
+				SwingUtilities.invokeAndWait(new Runnable() {
+					@Override
+					public void run() {
+						if (graph == null) {
+							JOptionPane.showMessageDialog(null,
+									ex.getMessage(), ex.getClass()
+											.getSimpleName(),
+									JOptionPane.ERROR_MESSAGE);
+
+						}
+						fileSelectionButton.setEnabled(true);
+						evalQueryButton.setEnabled(graph != null);
+
+					}
+				});
+			} catch (InterruptedException e) {
+			} catch (InvocationTargetException e) {
+			}
+		}
+	}
+
+	class Evaluator extends Worker {
+		private String query;
+
+		Evaluator(BoundedRangeModel brm, String query) {
+			super(brm);
+			this.query = query;
+		}
+
+		@Override
+		public void run() {
+			final GreqlEvaluator eval = new GreqlEvaluator(query, graph, null,
+					this);
+			eval.setOptimize(true);
+			try {
+				// if (eval.parseQuery()) {
+				// assert eval.getSyntaxGraph() != null;
+				// Tg2Dot t2d = new Tg2Dot();
+				// t2d.setGraph(eval.getSyntaxGraph());
+				// t2d.setPrintEdgeAttributes(true);
+				// t2d.setPrintReversedEdges(true);
+				// t2d.setOutputFile("query.greql.dot");
+				// t2d.printGraph();
+				// }
+				eval.startEvaluation();
+			} catch (Exception e1) {
+				ex = e1;
+			}
+			try {
+				SwingUtilities.invokeAndWait(new Runnable() {
+					@Override
+					public void run() {
+						if (ex == null) {
+							JValue result = eval.getEvaluationResult();
+							File resultFile;
+							try {
+								resultFile = File.createTempFile(
+										"greqlQueryResult", ".html");
+								new JValueHTMLOutputVisitor(result, resultFile
+										.getCanonicalPath(), graph);
+								resultPane.setPage(new URL("file", "localhost",
+										resultFile.getCanonicalPath()));
+							} catch (IOException e) {
+							}
+						} else {
+							String msg = ex.getMessage();
+							if (msg == null) {
+								msg = "An exception occured!";
+							}
+							JOptionPane.showMessageDialog(null, msg, ex
+									.getClass().getSimpleName(),
+									JOptionPane.ERROR_MESSAGE);
+
+						}
+						fileSelectionButton.setEnabled(true);
+						evalQueryButton.setEnabled(true);
+					}
+				});
+			} catch (InterruptedException e) {
+			} catch (InvocationTargetException e) {
+			}
+		}
+	}
 
 	public GreqlGui() {
 		super("GReQL GUI");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		panel = new JPanel();
-		getContentPane().add(panel);
-
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-		queryArea = new JTextArea(20, 40);
+		getContentPane().setLayout(new BorderLayout());
+		queryArea = new JTextArea(15, 50);
 		queryArea.setEditable(true);
 		queryArea.setText("/*\n * Please enter your query here!\n */\n\n");
+		JScrollPane queryScrollPane = new JScrollPane(queryArea);
+
+		queryPanel = new JPanel();
+		queryPanel.setLayout(new BorderLayout(4, 4));
+		queryPanel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+		queryPanel.add(queryScrollPane, BorderLayout.CENTER);
 
 		resultPane = new JEditorPane(
 				"text/html; charset=UTF-8",
@@ -61,8 +234,21 @@ public class GreqlGui extends JFrame {
 						+ "Simply select a graph, type a query and press the evaluation button."
 						+ "</html>");
 		resultPane.setEditable(false);
-		resultPane.setBackground(Color.LIGHT_GRAY);
+		resultPane.setMinimumSize(new Dimension(200, 200));
+		resultPane.setPreferredSize(resultPane.getMinimumSize());
 		JScrollPane resultScrollPane = new JScrollPane(resultPane);
+
+		brm = new DefaultBoundedRangeModel();
+		progressBar = new JProgressBar();
+		progressBar.setModel(brm);
+
+		resultPanel = new JPanel();
+		resultPanel.setLayout(new BorderLayout(4, 4));
+		resultPanel.setBorder(BorderFactory.createEmptyBorder(0, 4, 16, 4));
+		resultPanel.add(progressBar, BorderLayout.NORTH);
+		resultPanel.add(resultScrollPane, BorderLayout.CENTER);
+
+		getContentPane().add(resultPanel, BorderLayout.CENTER);
 
 		fileChooser = new JFileChooser();
 		fileChooser.setAcceptAllFileFilterUsed(false);
@@ -93,73 +279,32 @@ public class GreqlGui extends JFrame {
 				int returnVal = fileChooser.showOpenDialog(fileSelectionButton);
 
 				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					final File file = fileChooser.getSelectedFile();
-
-					try {
-						graph = GraphIO.loadSchemaAndGraphFromFile(file
-								.getCanonicalPath(), CodeGeneratorConfiguration.WITHOUT_TRANSACTIONS,
-								new ProgressFunctionImpl());
-					} catch (Exception e1) {
-						e1.printStackTrace();
-						JOptionPane.showMessageDialog(null, e1.getMessage(), e1
-								.getClass().getSimpleName(),
-								JOptionPane.ERROR_MESSAGE);
-					}
-
+					fileSelectionButton.setEnabled(false);
+					evalQueryButton.setEnabled(false);
+					new GraphLoader(brm, fileChooser.getSelectedFile()).start();
 				}
 			}
 		});
+
 		evalQueryButton = new JButton(new AbstractAction("Evaluate Query") {
 
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String query = queryArea.getText();
-				GreqlEvaluator eval = new GreqlEvaluator(query, graph, null);
-				eval.setOptimize(false);
-				try {
-					if (eval.parseQuery()) {
-						assert eval.getSyntaxGraph() != null;
-						Tg2Dot t2d = new Tg2Dot();
-						t2d.setGraph(eval.getSyntaxGraph());
-						t2d.setPrintEdgeAttributes(true);
-						t2d.setPrintReversedEdges(true);
-						t2d.setOutputFile("query.greql.dot");
-						t2d.printGraph();
-					}
-					eval.startEvaluation();
-					JValue result = eval.getEvaluationResult();
-					File resultFile = File.createTempFile("greqlQueryResult",
-							".html");
-					new JValueHTMLOutputVisitor(result, resultFile
-							.getCanonicalPath(), graph);
-					resultPane.setPage(new URL("file", "localhost", resultFile
-							.getCanonicalPath()));
-				} catch (Exception e1) {
-					String msg = e1.getMessage();
-					if (msg == null) {
-						msg = "An Exception occured!";
-					}
-					JOptionPane.showMessageDialog(null, msg, e1.getClass()
-							.getSimpleName(), JOptionPane.ERROR_MESSAGE);
-					e1.printStackTrace();
-				}
+				fileSelectionButton.setEnabled(false);
+				evalQueryButton.setEnabled(false);
+				new Evaluator(brm, queryArea.getText()).start();
 			}
 
 		});
-
+		evalQueryButton.setEnabled(false);
 		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new GridLayout(1, 2));
 		buttonPanel.add(fileSelectionButton);
 		buttonPanel.add(evalQueryButton);
-		panel.add(buttonPanel);
+		queryPanel.add(buttonPanel, BorderLayout.SOUTH);
 
-		JPanel textPanel = new JPanel(new GridLayout(2, 1));
-		textPanel.add(queryArea);
-		textPanel.add(resultScrollPane);
-		panel.add(textPanel);
-
+		getContentPane().add(queryPanel, BorderLayout.NORTH);
 		pack();
 		setVisible(true);
 	}
