58aced74c7ab1bff8d648f910c868fb956a53af2
Tassilo Horn <horn@uni-koblenz.de>
2010-05-19 15:39:27 +0000

    Reverted my last commit

--- src/de/uni_koblenz/jgralab/greql2/parser/ManualGreqlLexer.java
+++ src/de/uni_koblenz/jgralab/greql2/parser/ManualGreqlLexer.java
@@ -53,7 +53,6 @@ public class ManualGreqlLexer {
 		fixedTokens.put(TokenTypes.WITH, "with");
 		fixedTokens.put(TokenTypes.QUESTION, "?");
 		fixedTokens.put(TokenTypes.EXCL, "!");
-		fixedTokens.put(TokenTypes.COND, "cond");
 		fixedTokens.put(TokenTypes.COLON, ":");
 		fixedTokens.put(TokenTypes.COMMA, ",");
 		fixedTokens.put(TokenTypes.DOT, ".");
@@ -321,7 +320,7 @@ public class ManualGreqlLexer {
 					position++;
 				}
 			}
-			//skip multiline comments
+			//skip multiline comments 
 			if ((position < query.length() - 4)
 					&& (query.substring(position, position + 2).equals("/*"))) {
 				position++;
@@ -334,7 +333,7 @@ public class ManualGreqlLexer {
 				}
 			}
 		} while (
-				   ((position < query.length()) && (isWs(query.charAt(position))))
+				   ((position < query.length()) && (isWs(query.charAt(position)))) 
 			    || ((position < query.length() - 2) && (query.substring(position, position + 2).equals("//")))
 			    || ((position < query.length() - 4) && (query.substring(position, position + 2).equals("/*")))
 				);
