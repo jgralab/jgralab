062ddb8976c5234d2cfa6f0d3a4f87ea410b33de
Eckhard Großmann <mmce@uni-koblenz.de>
2010-06-26 14:16:23 +0000

    New GReQL-function which returns a unique type name.

---
+++ src/de/uni_koblenz/jgralab/greql2/funlib/UniqueTypeName.java
@@ -0,0 +1,111 @@
+/*
+ * JGraLab - The Java graph laboratory
+ * (c) 2006-2010 Institute for Software Technology
+ *               University of Koblenz-Landau, Germany
+ *
+ *               ist@uni-koblenz.de
+ *
+ * Please report bugs to http://serres.uni-koblenz.de/bugzilla
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package de.uni_koblenz.jgralab.greql2.funlib;
+
+import java.util.ArrayList;
+
+import de.uni_koblenz.jgralab.AttributedElement;
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.graphmarker.BooleanGraphMarker;
+import de.uni_koblenz.jgralab.greql2.exception.EvaluateException;
+import de.uni_koblenz.jgralab.greql2.exception.WrongFunctionParameterException;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueImpl;
+import de.uni_koblenz.jgralab.greql2.jvalue.JValueType;
+import de.uni_koblenz.jgralab.schema.AttributedElementClass;
+
+/**
+ * Returns the unique type of the given attributed element.
+ * 
+ * <dl>
+ * <dt><b>GReQL-signature</b></dt>
+ * <dd><code>STRING UniqueTypeName(ae:ATTRELEM)</code></dd>
+ * <dd>&nbsp;</dd>
+ * </dl>
+ * <dl>
+ * <dt></dt>
+ * <dd>
+ * <dl>
+ * <dt><b>Parameters:</b></dt>
+ * <dd><code>ae</code> - attributed element to return type for</dd>
+ * <dt><b>Returns:</b></dt>
+ * <dd>the unique type name of the given attributed element as string</dd>
+ * <dd><code>Null</code> if one of the parameters is <code>Null</code></dd>
+ * </dl>
+ * </dd>
+ * </dl>
+ * 
+ * @author ist@uni-koblenz.de
+ * 
+ */
+public class UniqueTypeName extends Greql2Function {
+
+	{
+		JValueType[][] x = { { JValueType.ATTRELEM, JValueType.STRING },
+				{ JValueType.ATTRELEMCLASS, JValueType.STRING } };
+		signatures = x;
+
+		description = "Returns the unique type name of the given AttrElem.";
+
+		Category[] c = { Category.SCHEMA_ACCESS };
+		categories = c;
+	}
+
+	@Override
+	public JValue evaluate(Graph graph, BooleanGraphMarker subgraph,
+			JValue[] arguments) throws EvaluateException {
+		AttributedElementClass aec = null;
+		AttributedElement elem = null;
+		switch (checkArguments(arguments)) {
+		case 0:
+			elem = arguments[0].toAttributedElement();
+			aec = elem.getAttributedElementClass();
+			break;
+		case 1:
+			aec = arguments[0].toAttributedElementClass();
+			break;
+		default:
+			throw new WrongFunctionParameterException(this, arguments);
+		}
+
+		return new JValueImpl(aec.getUniqueName(), elem);
+	}
+
+	@Override
+	public long getEstimatedCosts(ArrayList<Long> inElements) {
+		return 2;
+	}
+
+	@Override
+	public double getSelectivity() {
+		return 1;
+	}
+
+	@Override
+	public long getEstimatedCardinality(int inElements) {
+		return 1;
+	}
+
+}
