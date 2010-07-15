7e16477ef741b6cfe79ca1f108cb46151bef8c5f
Sascha Strauß <strauss@uni-koblenz.de>
2010-07-15 11:50:52 +0000

    added several blank problem interfaces

---
+++ src/de/uni_koblenz/jgralab/algolib/problems/directed/AcyclicitySolver.java
@@ -0,0 +1,9 @@
+package de.uni_koblenz.jgralab.algolib.problems.directed;
+
+//TODO write problem specification
+public interface AcyclicitySolver {
+	
+	public void solveAcyclicity();
+
+	public boolean isAcyclic();
+}
