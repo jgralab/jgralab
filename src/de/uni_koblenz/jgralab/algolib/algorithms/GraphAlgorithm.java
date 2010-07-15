73800e1970939e79f8f3e06e2518e1dc70aa1346
Sascha Strauß <strauss@uni-koblenz.de>
2010-07-15 13:57:39 +0000

    created package structure for graph algorithms created base graph algorithm implementation created base implementation for graph algorithms that can operate on directed and undirected graphs

---
+++ src/de/uni_koblenz/jgralab/algolib/algorithms/GraphAlgorithm.java
@@ -0,0 +1,115 @@
+package de.uni_koblenz.jgralab.algolib.algorithms;
+
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.GraphElement;
+import de.uni_koblenz.jgralab.algolib.functions.BooleanFunction;
+
+public abstract class GraphAlgorithm {
+
+	public static final BooleanFunction<GraphElement> DEFAULT_SUBGRAPH = new BooleanFunction<GraphElement>() {
+
+		@Override
+		public boolean get(GraphElement parameter) {
+			return true;
+		}
+
+		@Override
+		public boolean isDefined(GraphElement parameter) {
+			return true;
+		}
+
+		@Override
+		public void set(GraphElement parameter, boolean value) {
+			throw new UnsupportedOperationException(
+					"This function is immutable.");
+		}
+
+	};
+
+	protected Graph graph;
+	protected BooleanFunction<GraphElement> subgraph;
+	protected AlgorithmStates state;
+	protected boolean terminated;
+
+	public GraphAlgorithm(Graph graph) {
+		super();
+		this.graph = graph;
+		reset();
+		resetParameters();
+	}
+
+	public void reset() {
+		if (getState() != AlgorithmStates.RUNNING) {
+			this.terminated = false;
+			this.state = AlgorithmStates.INITIALIZED;
+		} else {
+			throw new IllegalStateException(
+					"The algorithm may not be reseted while it is running.");
+		}
+	}
+
+	public void resetParameters() {
+		if (getState() == AlgorithmStates.INITIALIZED) {
+			this.subgraph = DEFAULT_SUBGRAPH;
+		} else {
+			throw new IllegalStateException(
+					"The parameters may only be reseted to their default values when in state "
+							+ AlgorithmStates.INITIALIZED);
+		}
+	}
+
+	public GraphAlgorithm(Graph graph, BooleanFunction<GraphElement> subgraph) {
+		this(graph);
+		this.subgraph = subgraph;
+	}
+
+	public Graph getGraph() {
+		return graph;
+	}
+
+	public void setGraph(Graph graph) {
+		if (getState() == AlgorithmStates.INITIALIZED) {
+			this.graph = graph;
+		} else {
+			throw new IllegalStateException(
+					"The graph may only be changed when in state "
+							+ AlgorithmStates.INITIALIZED);
+		}
+	}
+
+	public BooleanFunction<GraphElement> getSubgraph() {
+		return subgraph;
+	}
+
+	public void setSubgraph(BooleanFunction<GraphElement> subgraph) {
+		if (getState() == AlgorithmStates.INITIALIZED) {
+			this.subgraph = subgraph;
+		} else {
+			throw new IllegalStateException(
+					"The subgraph may only be changed when in state "
+							+ AlgorithmStates.INITIALIZED);
+		}
+	}
+
+	public synchronized AlgorithmStates getState() {
+		return state;
+	}
+
+	public void terminate() {
+		if (getState() == AlgorithmStates.RUNNING) {
+			terminated = true;
+		} else {
+			throw new IllegalStateException(
+					"The algorithm may only be terminated, when in state "
+							+ AlgorithmStates.RUNNING);
+		}
+	}
+
+	protected synchronized void cancelIfInterrupted() {
+		if (Thread.interrupted()) {
+			state = AlgorithmStates.CANCELED;
+		}
+	}
+
+	public abstract boolean isDirected();
+}
---
+++ src/de/uni_koblenz/jgralab/algolib/algorithms/HybridGraphAlgorithm.java
@@ -0,0 +1,36 @@
+package de.uni_koblenz.jgralab.algolib.algorithms;
+
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.GraphElement;
+import de.uni_koblenz.jgralab.algolib.functions.BooleanFunction;
+
+public abstract class HybridGraphAlgorithm extends GraphAlgorithm {
+	public static final boolean DEFAULT_DIRECTED = true;
+
+	protected boolean directed;
+
+	public HybridGraphAlgorithm(Graph graph) {
+		super(graph);
+	}
+
+	public HybridGraphAlgorithm(Graph graph,
+			BooleanFunction<GraphElement> subgraph, boolean directed) {
+		super(graph, subgraph);
+	}
+
+	@Override
+	public boolean isDirected() {
+		return directed;
+	}
+
+	public void setDirected(boolean directed) {
+		this.directed = directed;
+	}
+
+	@Override
+	public void resetParameters() {
+		super.resetParameters();
+		directed = DEFAULT_DIRECTED;
+	}
+
+}
