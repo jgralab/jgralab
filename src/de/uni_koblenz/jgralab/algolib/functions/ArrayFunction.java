bec5af372816d5b909c9d42fcec2566c15567f94
Sascha Strauß <strauss@uni-koblenz.de>
2010-07-16 13:25:12 +0000

    created encapsulation for intdomain functions

---
+++ src/de/uni_koblenz/jgralab/algolib/functions/ArrayFunction.java
@@ -0,0 +1,27 @@
+package de.uni_koblenz.jgralab.algolib.functions;
+
+public class ArrayFunction<RANGE> implements IntDomainFunction<RANGE> {
+
+	private RANGE[] values;
+	
+	public ArrayFunction(RANGE[] values) {
+		this.values = values;
+	}
+
+	@Override
+	public RANGE get(int parameter) {
+		return values[parameter];
+	}
+
+	@Override
+	public boolean isDefined(int parameter) {
+		return parameter >= 0 && parameter < values.length
+				&& values[parameter] != null;
+	}
+
+	@Override
+	public void set(int parameter, RANGE value) {
+		values[parameter] = value;
+	}
+
+}
