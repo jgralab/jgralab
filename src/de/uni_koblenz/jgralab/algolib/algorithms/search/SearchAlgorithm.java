7e641008d11b175429e028450bb305c91c526c4a
Sascha Strauß <strauss@uni-koblenz.de>
2010-07-16 13:08:12 +0000

    removed the boolean flags from "SearchAlgorithm" added abstract methods to GraphAlgorithm and SearchAlgorithm for adding visitors

--- src/de/uni_koblenz/jgralab/algolib/algorithms/search/SearchAlgorithm.java
+++ src/de/uni_koblenz/jgralab/algolib/algorithms/search/SearchAlgorithm.java
@@ -6,10 +6,7 @@ import de.uni_koblenz.jgralab.GraphElement;
 import de.uni_koblenz.jgralab.Vertex;
 import de.uni_koblenz.jgralab.algolib.algorithms.HybridGraphAlgorithm;
 import de.uni_koblenz.jgralab.algolib.functions.BooleanFunction;
-import de.uni_koblenz.jgralab.algolib.functions.Function;
-import de.uni_koblenz.jgralab.algolib.functions.IntFunction;
-import de.uni_koblenz.jgralab.graphmarker.ArrayVertexMarker;
-import de.uni_koblenz.jgralab.graphmarker.IntegerVertexMarker;
+import de.uni_koblenz.jgralab.algolib.visitors.SearchVisitor;
 
 public abstract class SearchAlgorithm extends HybridGraphAlgorithm {
 
@@ -32,21 +29,11 @@ public abstract class SearchAlgorithm extends HybridGraphAlgorithm {
 		}
 
 	};
-	public static final boolean DEFAULT_COMPUTE_VERTEX_ORDER = true;
-	public static final boolean DEFAULT_COMPUTE_EDGE_ORDER = true;
-	public static final boolean DEFAULT_COMPUTE_LEVEL = false;
-	public static final boolean DEFAULT_COMPUTE_PARENT = false;
 
 	protected BooleanFunction<Edge> navigable;
-	protected boolean computeVertexOrder;
-	protected boolean computeEdgeOrder;
-	protected boolean computeLevel;
-	protected boolean computeParent;
 
 	protected Vertex[] vertexOrder;
 	protected Edge[] edgeOrder;
-	protected IntFunction<Vertex> level;
-	protected Function<Vertex, Edge> parent;
 
 	public SearchAlgorithm(Graph graph, BooleanFunction<GraphElement> subgraph,
 			boolean directed, BooleanFunction<Edge> navigable) {
@@ -60,53 +47,16 @@ public abstract class SearchAlgorithm extends HybridGraphAlgorithm {
 
 	@Override
 	public void reset() {
-		vertexOrder = computeVertexOrder ? new Vertex[graph.getVCount() + 1]
-				: null;
-		edgeOrder = computeEdgeOrder ? new Edge[graph.getECount() + 1] : null;
-		level = computeLevel ? new IntegerVertexMarker(graph) : null;
-		parent = computeParent ? new ArrayVertexMarker<Edge>(graph) : null;
+		vertexOrder =  new Vertex[graph.getVCount() + 1];
+		edgeOrder = new Edge[graph.getECount() + 1];
+		// reset visitors
 	}
 
 	@Override
 	public void resetParameters() {
 		super.resetParameters();
 		this.navigable = DEFAULT_NAVIGABLE;
-		this.computeVertexOrder = DEFAULT_COMPUTE_VERTEX_ORDER;
-		this.computeEdgeOrder = DEFAULT_COMPUTE_EDGE_ORDER;
-		this.computeLevel = DEFAULT_COMPUTE_LEVEL;
-		this.computeParent = DEFAULT_COMPUTE_PARENT;
 	}
-
-	public boolean isComputeVertexOrder() {
-		return computeVertexOrder;
-	}
-
-	public void setComputeVertexOrder(boolean computeVertexOrder) {
-		this.computeVertexOrder = computeVertexOrder;
-	}
-
-	public boolean isComputeEdgeOrder() {
-		return computeEdgeOrder;
-	}
-
-	public void setComputeEdgeOrder(boolean computeEdgeOrder) {
-		this.computeEdgeOrder = computeEdgeOrder;
-	}
-
-	public boolean isComputeLevel() {
-		return computeLevel;
-	}
-
-	public void setComputeLevel(boolean computeLevel) {
-		this.computeLevel = computeLevel;
-	}
-
-	public boolean isComputeParent() {
-		return computeParent;
-	}
-
-	public void setComputeParent(boolean computeParent) {
-		this.computeParent = computeParent;
-	}
-
+	
+	public abstract void addSearchVisitor(SearchVisitor visitor);
 }
