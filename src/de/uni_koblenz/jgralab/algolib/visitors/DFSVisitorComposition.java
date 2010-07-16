bef81b38eaef6fa86e6fbbeaa61767c45fd047e4
Sascha Strauß <strauss@uni-koblenz.de>
2010-07-16 13:01:02 +0000

    improved the visitors to allow setting the algortihm also the compositions have been improved. They are now specializations from each other. They don't need an initial visitor when created and they now have methods for adding and removing visitors.

--- src/de/uni_koblenz/jgralab/algolib/visitors/DFSVisitorComposition.java
+++ src/de/uni_koblenz/jgralab/algolib/visitors/DFSVisitorComposition.java
@@ -1,86 +1,111 @@
 package de.uni_koblenz.jgralab.algolib.visitors;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import de.uni_koblenz.jgralab.Edge;
 import de.uni_koblenz.jgralab.Vertex;
 
-public class DFSVisitorComposition extends DFSVisitorAdapter {
-	private List<DFSVisitor> visitors;
+public class DFSVisitorComposition extends SearchVisitorComposition implements
+		DFSVisitor {
+	private Set<DFSVisitor> dfsVisitors;
 
-	public DFSVisitorComposition(DFSVisitor initialVisitor) {
-		visitors = new LinkedList<DFSVisitor>();
-		visitors.add(initialVisitor);
+	public DFSVisitorComposition() {
+		dfsVisitors = new HashSet<DFSVisitor>();
+	}
+
+	public void addDFSVisitor(DFSVisitor newVisitor) {
+		dfsVisitors.add(newVisitor);
+	}
+
+	public void removeDFSVisitor(DFSVisitor toRemove) {
+		dfsVisitors.remove(dfsVisitors);
+	}
+
+	public void clearDFSVisitors() {
+		dfsVisitors.clear();
 	}
 
 	@Override
 	public void leaveTreeEdge(Edge e) {
-		for (DFSVisitor currentVisitor : visitors) {
+		for (DFSVisitor currentVisitor : dfsVisitors) {
 			currentVisitor.leaveTreeEdge(e);
 		}
 	}
 
 	@Override
 	public void leaveVertex(Vertex v) {
-		for (DFSVisitor currentVisitor : visitors) {
+		for (DFSVisitor currentVisitor : dfsVisitors) {
 			currentVisitor.leaveVertex(v);
 		}
 	}
 
 	@Override
 	public void visitBackwardArc(Edge e) {
-		for (DFSVisitor currentVisitor : visitors) {
+		for (DFSVisitor currentVisitor : dfsVisitors) {
 			currentVisitor.visitBackwardArc(e);
 		}
 	}
 
 	@Override
 	public void visitCrosslink(Edge e) {
-		for (DFSVisitor currentVisitor : visitors) {
+		for (DFSVisitor currentVisitor : dfsVisitors) {
 			currentVisitor.visitForwardArc(e);
 		}
 	}
 
 	@Override
 	public void visitForwardArc(Edge e) {
-		for (DFSVisitor currentVisitor : visitors) {
+		for (DFSVisitor currentVisitor : dfsVisitors) {
 			currentVisitor.visitForwardArc(e);
 		}
 	}
 
 	@Override
 	public void visitFrond(Edge e) {
-		for (DFSVisitor currentVisitor : visitors) {
+		super.visitFrond(e);
+		for (DFSVisitor currentVisitor : dfsVisitors) {
 			currentVisitor.visitFrond(e);
 		}
 	}
 
 	@Override
 	public void visitRoot(Vertex v) {
-		for (DFSVisitor currentVisitor : visitors) {
+		super.visitRoot(v);
+		for (DFSVisitor currentVisitor : dfsVisitors) {
 			currentVisitor.visitRoot(v);
 		}
 	}
 
 	@Override
 	public void visitTreeEdge(Edge e) {
-		for (DFSVisitor currentVisitor : visitors) {
+		super.visitTreeEdge(e);
+		for (DFSVisitor currentVisitor : dfsVisitors) {
 			currentVisitor.visitTreeEdge(e);
 		}
 	}
 
 	@Override
 	public void visitEdge(Edge e) {
-		for (DFSVisitor currentVisitor : visitors) {
+		super.visitEdge(e);
+		for (DFSVisitor currentVisitor : dfsVisitors) {
 			currentVisitor.visitEdge(e);
 		}
 	}
 
 	@Override
 	public void visitVertex(Vertex v) {
-		for (DFSVisitor currentVisitor : visitors) {
+		super.visitVertex(v);
+		for (DFSVisitor currentVisitor : dfsVisitors) {
 			currentVisitor.visitVertex(v);
+		}
+	}
+
+	@Override
+	public void reset() {
+		super.reset();
+		for (DFSVisitor currentVisitor : dfsVisitors) {
+			currentVisitor.reset();
 		}
 	}
 
--- src/de/uni_koblenz/jgralab/algolib/visitors/SimpleVisitorAdapter.java
+++ src/de/uni_koblenz/jgralab/algolib/visitors/SimpleVisitorAdapter.java
@@ -2,8 +2,9 @@ package de.uni_koblenz.jgralab.algolib.visitors;
 
 import de.uni_koblenz.jgralab.Edge;
 import de.uni_koblenz.jgralab.Vertex;
+import de.uni_koblenz.jgralab.algolib.algorithms.GraphAlgorithm;
 
-public abstract class SimpleVisitorAdapter implements SimpleVisitor {
+public class SimpleVisitorAdapter implements SimpleVisitor {
 
 	@Override
 	public void visitEdge(Edge e) {
@@ -12,6 +13,16 @@ public abstract class SimpleVisitorAdapter implements SimpleVisitor {
 
 	@Override
 	public void visitVertex(Vertex v) {
+		
+	}
+
+	@Override
+	public void reset() {
+		
+	}
+
+	@Override
+	public void setAlgorithm(GraphAlgorithm alg) {
 		
 	}
 
