ae3174ac23aeffdba0753fda82b228bf0235a899
Daniel Bildhauer <dbildh@uni-koblenz.de>
2010-07-20 10:19:15 +0000

    Fixed bug that GreQL doesn't care about subgraph marker

--- src/de/uni_koblenz/jgralab/greql2/evaluator/GreqlEvaluator.java
+++ src/de/uni_koblenz/jgralab/greql2/evaluator/GreqlEvaluator.java
@@ -1054,7 +1054,7 @@ public class GreqlEvaluator {
 
 		long plainEvaluationStartTime = System.currentTimeMillis();
 		result = vertexEvalGraphMarker.getMark(
-				queryGraph.getFirstGreql2Expression()).getResult(null);
+				queryGraph.getFirstGreql2Expression()).getResult(subgraphMarker);
 
 		// last, remove all added tempAttributes, currently, this are only
 		// subgraphAttributes
