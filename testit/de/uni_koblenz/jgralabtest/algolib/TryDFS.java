80e511db5c7297ef1d1274ba3840ad6f912716d6
Sascha Strauß <strauss@uni-koblenz.de>
2010-08-10 14:40:51 +0000

    Added iterative version of DFS that does not use recursion but behaves the same way as the recursive DFS.

--- testit/de/uni_koblenz/jgralabtest/algolib/TryDFS.java
+++ testit/de/uni_koblenz/jgralabtest/algolib/TryDFS.java
@@ -1,6 +1,7 @@
 package de.uni_koblenz.jgralabtest.algolib;
 
 import de.uni_koblenz.jgralab.algolib.algorithms.search.DepthFirstSearch;
+import de.uni_koblenz.jgralab.algolib.algorithms.search.IterativeDepthFirstSearch;
 import de.uni_koblenz.jgralab.algolib.algorithms.search.RecursiveDepthFirstSearch;
 import de.uni_koblenz.jgralabtest.schemas.algolib.simple.SimpleGraph;
 import de.uni_koblenz.jgralabtest.schemas.algolib.simple.SimpleSchema;
@@ -8,34 +9,48 @@ import de.uni_koblenz.jgralabtest.schemas.algolib.simple.SimpleVertex;
 
 public class TryDFS {
 	public static void main(String[] args) {
-		SimpleGraph graph = SimpleSchema.instance().createSimpleGraph();
-		SimpleVertex v1 = graph.createSimpleVertex();
-		SimpleVertex v2 = graph.createSimpleVertex();
-		SimpleVertex v3 = graph.createSimpleVertex();
-		SimpleVertex v4 = graph.createSimpleVertex();
-		SimpleVertex v5 = graph.createSimpleVertex();
-		graph.createSimpleVertex();
-		graph.createSimpleEdge(v1, v2);
-		graph.createSimpleEdge(v1, v4);
-		graph.createSimpleEdge(v2, v1);
-		graph.createSimpleEdge(v1, v3);
-		graph.createSimpleEdge(v1, v5);
-		graph.createSimpleEdge(v3, v2);
-		graph.createSimpleEdge(v2, v4);
-		graph.createSimpleEdge(v3, v4);
-		graph.createSimpleEdge(v4, v5);
-		graph.createSimpleEdge(v3, v5);
-		DepthFirstSearch dfs = new RecursiveDepthFirstSearch(graph).withLevel().withParent().withRorder();
-		dfs.addVisitor(new DebugSearchVisitor());
+		// SimpleGraph graph = SimpleSchema.instance().createSimpleGraph();
+		// SimpleVertex v1 = graph.createSimpleVertex();
+		// SimpleVertex v2 = graph.createSimpleVertex();
+		// SimpleVertex v3 = graph.createSimpleVertex();
+		// SimpleVertex v4 = graph.createSimpleVertex();
+		// SimpleVertex v5 = graph.createSimpleVertex();
+		// graph.createSimpleVertex();
+		// graph.createSimpleEdge(v1, v2);
+		// graph.createSimpleEdge(v1, v4);
+		// graph.createSimpleEdge(v2, v1);
+		// graph.createSimpleEdge(v1, v3);
+		// graph.createSimpleEdge(v1, v5);
+		// graph.createSimpleEdge(v3, v2);
+		// graph.createSimpleEdge(v2, v4);
+		// graph.createSimpleEdge(v3, v4);
+		// graph.createSimpleEdge(v4, v5);
+		// graph.createSimpleEdge(v3, v5);
 
-		dfs.execute(v1);
+		SimpleGraph graph = RandomGraph.createEmptyGraph();
+		RandomGraph.addWeakComponent(0, graph, 200000, 200000);
 
-		printResults(dfs);
-		
-		dfs.reset();
+		DepthFirstSearch dfs = new RecursiveDepthFirstSearch(graph);
+		// dfs.addVisitor(new DebugSearchVisitor());
+
+		System.out.println("Starting recursive:");
+		try {
+			dfs.execute();
+			System.out.println(dfs.getVertexOrder().length());
+			// printResults(dfs);
+		} catch (StackOverflowError e) {
+			System.err.println("Fail!");
+		}
+
+		dfs = new IterativeDepthFirstSearch(graph);
+		// dfs.addVisitor(new DebugSearchVisitor());
+
+		System.out.println("Starting iterative:");
 		dfs.execute();
-		printResults(dfs);
-		
+		System.out.println(dfs.getVertexOrder().length());
+
+		// printResults(dfs);
+
 		System.out.println("Fini");
 	}
 
