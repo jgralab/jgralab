5004d481b9704bad6f4618eb5cd14ecdbcab251a
Sascha Strauß <strauss@uni-koblenz.de>
2010-08-13 14:00:29 +0000

    added algorithm for solving the shortest paths problem that uses BFS fixed a bug in BFS

---
+++ src/de/uni_koblenz/jgralab/algolib/algorithms/shortest_paths/ShortestPathsWithBFS.java
@@ -0,0 +1,88 @@
+package de.uni_koblenz.jgralab.algolib.algorithms.shortest_paths;
+
+import de.uni_koblenz.jgralab.Edge;
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.GraphElement;
+import de.uni_koblenz.jgralab.Vertex;
+import de.uni_koblenz.jgralab.algolib.algorithms.AlgorithmStates;
+import de.uni_koblenz.jgralab.algolib.algorithms.GraphAlgorithm;
+import de.uni_koblenz.jgralab.algolib.algorithms.search.BreadthFirstSearch;
+import de.uni_koblenz.jgralab.algolib.functions.BooleanFunction;
+import de.uni_koblenz.jgralab.algolib.functions.Function;
+import de.uni_koblenz.jgralab.algolib.functions.IntFunction;
+import de.uni_koblenz.jgralab.algolib.problems.DistanceFromVertexSolver;
+import de.uni_koblenz.jgralab.algolib.problems.ShortestPathsFromVertexSolver;
+import de.uni_koblenz.jgralab.algolib.visitors.Visitor;
+
+public class ShortestPathsWithBFS extends GraphAlgorithm implements
+		DistanceFromVertexSolver, ShortestPathsFromVertexSolver {
+
+	private BreadthFirstSearch bfs;
+
+	public ShortestPathsWithBFS(Graph graph, BreadthFirstSearch bfs) {
+		this(graph, null, bfs);
+	}
+
+	public ShortestPathsWithBFS(Graph graph,
+			BooleanFunction<GraphElement> subgraph, BreadthFirstSearch bfs) {
+		super(graph, subgraph);
+		this.bfs = bfs;
+	}
+
+	@Override
+	public void addVisitor(Visitor visitor) {
+		throw new UnsupportedOperationException(
+				"This algorithm does not support visitors.");
+	}
+
+	@Override
+	public void disableOptionalResults() {
+	}
+
+	@Override
+	protected void done() {
+		state = AlgorithmStates.FINISHED;
+	}
+
+	@Override
+	public boolean isDirected() {
+		return bfs.isDirected();
+	}
+
+	@Override
+	public boolean isHybrid() {
+		return bfs.isHybrid();
+	}
+
+	@Override
+	public void removeVisitor(Visitor visitor) {
+		throw new UnsupportedOperationException(
+				"This algorithm does not support visitors.");
+	}
+
+	@Override
+	public void setDirected(boolean directed) {
+		bfs.setDirected(directed);
+	}
+
+	@Override
+	public ShortestPathsWithBFS execute(Vertex start) {
+		bfs.reset();
+		startRunning();
+		bfs.withLevel().withParent().execute(start);
+		done();
+		return this;
+	}
+
+	@Override
+	public IntFunction<Vertex> getDistance() {
+		checkStateForResult();
+		return bfs.getLevel();
+	}
+
+	@Override
+	public Function<Vertex, Edge> getParent() {
+		return bfs.getParent();
+	}
+
+}
