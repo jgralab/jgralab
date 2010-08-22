4f799582f4312ecdb117655224a87e57399c04aa
Sascha Strauß <strauss@uni-koblenz.de>
2010-08-22 10:38:53 +0000

    added the floyd algorithm

--- src/de/uni_koblenz/jgralab/algolib/problems/WeightedDistancesSolver.java
+++ src/de/uni_koblenz/jgralab/algolib/problems/WeightedDistancesSolver.java
@@ -1,15 +1,34 @@
 package de.uni_koblenz.jgralab.algolib.problems;
 
+import de.uni_koblenz.jgralab.Vertex;
+import de.uni_koblenz.jgralab.algolib.functions.BinaryDoubleFunction;
+
 /**
  * The problem <b>weighted distances</b> can be defined for directed and
  * undirected graphs. The graph may not have negative cycles. Algorithms solving
  * this problem are not required to check this precondition. There are no
- * further parameters.
+ * further parameters. The result is a binary function <i>weighted distance</i>
+ * that assigns each vertex pair the weighted distance from the first vertex to
+ * the second vertex using a shortest path.
  * 
  * 
  * @author strauss@uni-koblenz.de
  * 
  */
 public interface WeightedDistancesSolver extends WeightedProblemSolver {
+
+	/**
+	 * Solves the problem <b>weighted distances</b>.
+	 * 
+	 * @return this algorithm object.
+	 */
+	public WeightedDistancesSolver execute();
+
+	/**
+	 * Retrieves the result <code>weightedDistance</code>.
+	 * 
+	 * @return the result <code>weightedDistance</code>.
+	 */
+	public BinaryDoubleFunction<Vertex, Vertex> getWeightedDistance();
 
 }
--- src/de/uni_koblenz/jgralab/algolib/problems/WeightedShortestPathsSolver.java
+++ src/de/uni_koblenz/jgralab/algolib/problems/WeightedShortestPathsSolver.java
@@ -1,14 +1,33 @@
 package de.uni_koblenz.jgralab.algolib.problems;
 
 import de.uni_koblenz.jgralab.Edge;
-import de.uni_koblenz.jgralab.algolib.VertexPair;
-import de.uni_koblenz.jgralab.algolib.functions.Function;
+import de.uni_koblenz.jgralab.Vertex;
+import de.uni_koblenz.jgralab.algolib.functions.BinaryFunction;
 
-//TODO write problem specification
-//can be defined for directed and undirected graphs
+/**
+ * The problem <b>weighted shortest paths</b> can be defined for directed and
+ * undirected graphs. The graph may not have negative cycles. Algorithms solving
+ * this problem are not required to check this precondition. There are no
+ * further parameters. The result is a binary function <i>successor</i> that
+ * assigns each vertex pair an edge, which is the next edge from the first
+ * vertex to follow for reaching the second vertex using a shortest path.
+ * 
+ * @author strauss@uni-koblenz.de
+ * 
+ */
 public interface WeightedShortestPathsSolver extends ProblemSolver {
 
+	/**
+	 * Solves the problem <b>weighted shortest paths</b>.
+	 * 
+	 * @return this algorithm object.
+	 */
 	public WeightedShortestPathsSolver execute();
 
-	public Function<VertexPair, Edge> getSuccessorRelation();
+	/**
+	 * Retrieves the result <code>successor</code>.
+	 * 
+	 * @return the result <code>successor</code>.
+	 */
+	public BinaryFunction<Vertex, Vertex, Edge> getSuccessor();
 }
