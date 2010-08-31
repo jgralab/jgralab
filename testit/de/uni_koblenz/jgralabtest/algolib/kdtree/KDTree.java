4edc22c295c69b9d8aa4a0d159482ca1b6b97fa3
Sascha Strauß <strauss@uni-koblenz.de>
2010-08-31 11:50:27 +0000

    Cleaned KDTree code of unused methods. Extracted class TryKDTree. The Stopwatch now measures the time in nanos. TryAStarSearch now (again) applies Ford-Moore, but only once.

--- testit/de/uni_koblenz/jgralabtest/algolib/kdtree/KDTree.java
+++ testit/de/uni_koblenz/jgralabtest/algolib/kdtree/KDTree.java
@@ -4,21 +4,16 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
-
-import de.uni_koblenz.jgralabtest.algolib.Stopwatch;
 
 public class KDTree<P extends Point> {
 
 	private static final int DELTA = 4;
 
 	private abstract class TreeNode {
-		private Key parent;
 		protected int level;
 
-		public TreeNode(Key parent, int level) {
+		public TreeNode(int level) {
 			super();
-			this.parent = parent;
 			this.level = level;
 		}
 
@@ -32,8 +27,8 @@ public class KDTree<P extends Point> {
 		private TreeNode left;
 		private TreeNode right;
 
-		public Key(Key parent, int level, int position, double value) {
-			super(parent, level);
+		public Key(int level, int position, double value) {
+			super(level);
 			this.position = position;
 			this.value = value;
 		}
@@ -65,8 +60,8 @@ public class KDTree<P extends Point> {
 	private class Leaf extends TreeNode {
 		private LinkedList<P> values;
 
-		public Leaf(Key parent, int level, LinkedList<P> values) {
-			super(parent, level);
+		public Leaf(int level, LinkedList<P> values) {
+			super(level);
 			this.values = values;
 		}
 
@@ -100,18 +95,17 @@ public class KDTree<P extends Point> {
 		dimension = rawData.get(0).getDimension();
 		this.rawData = rawData;
 
-		root = createTree(null, 0, rawData, 0);
+		root = createTree(0, rawData, 0);
 	}
 
 	public int getDimension() {
 		return dimension;
 	}
 
-	private TreeNode createTree(Key parent, int level, LinkedList<P> segment,
-			int position) {
+	private TreeNode createTree(int level, LinkedList<P> segment, int position) {
 		int segmentSize = segment.size();
 		if (segmentSize <= maxSegmentSize) {
-			return new Leaf(parent, level, segment);
+			return new Leaf(level, segment);
 		}
 		double mean = 0;
 		for (P current : segment) {
@@ -134,12 +128,12 @@ public class KDTree<P extends Point> {
 
 		assert (segmentSize == left.size() + right.size());
 
-		Key out = new Key(parent, level, position, mean);
+		Key out = new Key(level, position, mean);
 
 		int newPosition = (position + 1) % dimension;
 		int newLevel = level + 1;
-		TreeNode leftTree = createTree(out, newLevel, left, newPosition);
-		TreeNode rightTree = createTree(out, newLevel, right, newPosition);
+		TreeNode leftTree = createTree(newLevel, left, newPosition);
+		TreeNode rightTree = createTree(newLevel, right, newPosition);
 
 		out.left = leftTree;
 		out.right = rightTree;
@@ -222,73 +216,6 @@ public class KDTree<P extends Point> {
 		}
 	}
 
-	private Leaf getMatchingLeaf(TreeNode node, Point point) {
-		if (node.isLeaf()) {
-			return ((Leaf) node);
-		}
-		Key keyNode = (Key) node;
-		int position = keyNode.position;
-		double key = keyNode.value;
-		if (point.get(position) > key) {
-			return getMatchingLeaf(keyNode.right, point);
-		} else {
-			return getMatchingLeaf(keyNode.left, point);
-		}
-	}
-
-	private void findNearestNeighborsInSubtree(P point, int count,
-			List<P> nearestNeighbors, double[] minimalDistances,
-			TreeNode startAt) {
-		if (!startAt.isLeaf()) {
-			System.out.println("looking for nearest neighbors at\n"
-					+ ((Key) startAt).value
-					+ (startAt == root ? " ROOT " : " level: "
-							+ ((Key) startAt).level));
-		}
-		// search segment of given point
-		Leaf leaf = getMatchingLeaf(startAt, point);
-		LinkedList<P> segment = leaf.values;
-
-		// compute best nearest neighbor candidates from segment
-		findNearestNeighborsInSegment(point, count, nearestNeighbors,
-				minimalDistances, segment);
-
-		// go back up the tree and probe for better neighbors in other segments
-		TreeNode currentTreeNode = leaf;
-		while (currentTreeNode != startAt) {
-			TreeNode lastTreeNode = currentTreeNode;
-			currentTreeNode = currentTreeNode.parent;
-
-			Key currentKey = (Key) currentTreeNode;
-			// compute distance from hyperplane that is described by the current
-			// tree node
-			double squaredDistanceFromCurrentHyperplane = ((Point) point)
-					.get(currentKey.position)
-					- currentKey.value;
-			squaredDistanceFromCurrentHyperplane *= squaredDistanceFromCurrentHyperplane;
-
-			// if this distance is smaller than the worst nearest neighbor found
-			// so far, better neighbors can be found.
-			if (squaredDistanceFromCurrentHyperplane < minimalDistances[count - 1]) {
-				if (lastTreeNode == currentKey.left) {
-					findNearestNeighborsInSubtree(point, count,
-							nearestNeighbors, minimalDistances,
-							currentKey.right);
-					// leaf = getMatchingLeaf(currentKey.right, point);
-				} else {
-					assert (lastTreeNode == currentKey.right);
-					findNearestNeighborsInSubtree(point, count,
-							nearestNeighbors, minimalDistances, currentKey.left);
-					// leaf = getMatchingLeaf(currentKey.left, point);
-				}
-				segment = leaf.values;
-				// try to find better nearest neighbors in new segment
-				findNearestNeighborsInSegment(point, count, nearestNeighbors,
-						minimalDistances, segment);
-			}
-		}
-	}
-
 	private void findNearestNeighborsInSegment(P point, int count,
 			List<P> nearestNeighbors, double[] minimalDistances,
 			LinkedList<P> segment) {
@@ -328,82 +255,5 @@ public class KDTree<P extends Point> {
 				}
 			}
 		}
-	}
-
-	public static void main(String[] args) {
-		// common parameters
-		int count = 3;
-		int segmentSize = 100;
-		int length = 110;
-		double min = -100;
-		double max = 100;
-		Random rng = new Random();
-
-		Stopwatch sw = new Stopwatch();
-		System.out.println("Creating random list with " + length + " points.");
-		sw.start();
-		LinkedList<Point2> list = createRandomList(rng, min, max, length);
-		sw.stop();
-		System.out.println("List created in " + sw.getDuration() + "ms.");
-		Point2 point = createRandomPoint(min, max, rng);
-
-		System.out
-				.println("Searching for " + count + " nearest neighbors of "
-						+ point + "\nusing KD-tree with segment size of "
-						+ segmentSize);
-
-		sw.reset();
-		sw.start();
-		KDTree<Point2> tree = new KDTree<Point2>(list, segmentSize);
-		sw.stop();
-
-		System.out.println("KD-tree built in " + sw.getDuration() + "ms.");
-
-		sw.reset();
-		sw.start();
-		List<Point2> nearestNeighbors1 = tree.getNearestNeighborsExhaustively(
-				point, count);
-		sw.stop();
-		System.out.println(nearestNeighbors1 + "\n   by exhaustive search in "
-				+ sw.getDuration() + "ms.");
-		sw.reset();
-		sw.start();
-		List<Point2> nearestNeighbors2 = tree.getNearestNeighbors(point, count);
-		sw.stop();
-		System.out.println(nearestNeighbors2 + "\n   by KD-tree search    in "
-				+ sw.getDuration() + "ms.");
-		// System.out.println(tree);
-	}
-
-	private static LinkedList<Point2> createRandomList(Random rng, double min,
-			double max, int length) {
-		LinkedList<Point2> list = new LinkedList<Point2>();
-		for (int i = 0; i < length; i++) {
-			Point2 newPoint = createRandomPoint(min, max, rng);
-			// System.out.println(newPoint);
-			list.add(newPoint);
-		}
-		return list;
-	}
-
-	private static Point2 createRandomPoint(double min, double max, Random rng) {
-		double x = rng.nextDouble() * (max - min) + min;
-		double y = rng.nextDouble() * (max - min) + min;
-		Point2 newPoint = new Point2(x, y);
-		return newPoint;
-	}
-
-	private static LinkedList<Point2> createSmallList() {
-		LinkedList<Point2> list = new LinkedList<Point2>();
-		Point2 e = new Point2(1, 1);
-		list.add(e);
-		list.add(new Point2(3, 3.5));
-		list.add(new Point2(4.5, 4));
-		list.add(new Point2(4, 2));
-		list.add(new Point2(2, 1.5));
-		list.add(new Point2(6, 5));
-		list.add(new Point2(1, 3));
-		list.add(new Point2(5, 0.5));
-		return list;
 	}
 }
