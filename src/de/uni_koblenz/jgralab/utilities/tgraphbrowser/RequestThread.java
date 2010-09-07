a3ed962bcb727f2ea11e119de7aec4bfa9f737dd
Sascha Strauß <strauss@uni-koblenz.de>
2010-09-07 13:49:19 +0000

    Updated GPL header for all java files. Added GPL header to java files that didn't contain one yet.

--- src/de/uni_koblenz/jgralab/utilities/tgraphbrowser/RequestThread.java
+++ src/de/uni_koblenz/jgralab/utilities/tgraphbrowser/RequestThread.java
@@ -1,17 +1,25 @@
-/* 
-Copyright Paul James Mutton, 2001-2004, http://www.jibble.org/
-
-This file is part of Mini Wegb Server / SimpleWebServer.
-
-This software is dual-licensed, allowing you to choose between the GNU
-General Public License (GPL) and the www.jibble.org Commercial License.
-Since the GPL may be too restrictive for use in a proprietary application,
-a commercial license is also provided. Full license information can be
-found at http://www.jibble.org/licenses/
-
-$Author: pjm2 $
-$Id: ServerSideScriptEngine.java,v 1.4 2004/02/01 13:37:35 pjm2 Exp $
-
+/*
+ * JGraLab - The Java graph laboratory
+ * (c) 2006-2010 Institute for Software Technology
+ *               University of Koblenz-Landau, Germany
+ * 
+ *               ist@uni-koblenz.de
+ * 
+ * Please report bugs to http://serres.uni-koblenz.de/bugzilla
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 package de.uni_koblenz.jgralab.utilities.tgraphbrowser;
