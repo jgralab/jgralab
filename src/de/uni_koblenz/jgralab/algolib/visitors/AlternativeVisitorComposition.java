ed07f905e768a30d448c557b171807f98d215934
Sascha Strauß <strauss@uni-koblenz.de>
2010-09-15 12:44:13 +0000

    added new alternative implementations improved benchmark

---
+++ src/de/uni_koblenz/jgralab/algolib/visitors/AlternativeVisitorComposition.java
@@ -0,0 +1,85 @@
+/*
+ * JGraLab - The Java graph laboratory
+ * (c) 2006-2010 Institute for Software Technology
+ *               University of Koblenz-Landau, Germany
+ * 
+ *               ist@uni-koblenz.de
+ * 
+ * Please report bugs to http://serres.uni-koblenz.de/bugzilla
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package de.uni_koblenz.jgralab.algolib.visitors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uni_koblenz.jgralab.algolib.algorithms.GraphAlgorithm;
+
+public abstract class AlternativeVisitorComposition implements Visitor {
+
+	private List<Visitor> visitors;
+
+	{
+		createVisitorsLazily();
+	}
+
+	protected void createVisitorsLazily() {
+		if (visitors == null) {
+			visitors = new ArrayList<Visitor>();
+		}
+	}
+
+	public void addVisitor(Visitor visitor) {
+		// createVisitorsLazily();
+		if (!visitors.contains(visitor)) {
+			visitors.add(visitor);
+		}
+	}
+
+	public void removeVisitor(Visitor visitor) {
+		// if (visitors != null) {
+		visitors.remove(visitor);
+		// if (visitors.size() == 0) {
+		// visitors = null;
+		// }
+		// }
+	}
+
+	public void clearVisitors() {
+		// visitors = null;
+		visitors.clear();
+	}
+
+	@Override
+	public void reset() {
+		// if (visitors != null) {
+		int n = visitors.size();
+		for (int i = 0; i < n; i++) {
+			visitors.get(i).reset();
+		}
+		// }
+	}
+
+	@Override
+	public void setAlgorithm(GraphAlgorithm alg) {
+		// if (visitors != null) {
+		int n = visitors.size();
+		for (int i = 0; i < n; i++) {
+			visitors.get(i).setAlgorithm(alg);
+		}
+		// }
+	}
+}
