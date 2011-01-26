bd9018f5dd39395b6720793980711d8d4432f3dd
mmce <devnull@localhost>
2011-01-26 20:22:29 +0100

    Removed a warning.

--- src/de/uni_koblenz/jgralab/utilities/tg2dot/graph_layout/definition/AbstractDefinition.java
+++ src/de/uni_koblenz/jgralab/utilities/tg2dot/graph_layout/definition/AbstractDefinition.java
@@ -4,8 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import de.uni_koblenz.jgralab.utilities.dot.DotWriter;
-
 /**
  * Provides the general implementation for {@link Definition}.
  * 
