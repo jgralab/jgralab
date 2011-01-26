fcf32c747a89321c3acf4775b4b2524dc7a360fa
mmce <devnull@localhost>
2011-01-26 11:28:05 +0100

    Fixed test for GReQL method nodeTrace(). All Tests of PathSystemTest are working.
    TODO: More tests in order to get full coverage.

--- testit/de/uni_koblenz/jgralabtest/greql2/PathSystemTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/PathSystemTest.java
@@ -423,21 +423,22 @@ public class PathSystemTest extends GenericTests {
 
 	@Test
 	public void testNodeTrace() throws Exception {
-		// TODO: Broken, because the GReQL parser removes all WhereExpressions
-		// and LetExpressions!
-		String queryString = "from v: V{WhereExpression}, w: V{Variable} report nodeTrace(extractPath(v  :-) <--{IsDefinitionOf} <--{IsVarOf}, w)) end";
-		JValue result = evalTestQuery("PathLength", queryString);
+		String queryString = "from c: V{localities.County}, r:V{junctions.Crossroad}"
+				+ "report nodeTrace(extractPath(pathSystem(c, -->{localities.ContainsLocality} -->{localities.ContainsCrossroad}), r)) "
+				+ "end";
+		JValue result = evalTestQuery("PathLength", queryString,
+				TestVersion.CITY_MAP_GRAPH);
 		JValueBag bag = result.toCollection().toJValueBag();
-		assertEquals(5, bag.size());
+		assertEquals(countyCount * crossroadCount, bag.size());
 		int emptyTraces = 0;
 		for (JValue v : bag) {
-			if (v.toCollection().size() == 0) {
+			if (v.toCollection().isEmpty()) {
 				emptyTraces++;
 			} else {
 				assertEquals(3, v.toCollection().size());
 			}
 		}
-		assertEquals(1, emptyTraces);
+		assertEquals(crossroadCount + uncontainedCrossroadCount, emptyTraces);
 	}
 
 	@Test
