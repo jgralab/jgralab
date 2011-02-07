15eb42a2a711c33dec0256df3ee54f87d59abe8e
mmce <devnull@localhost>
2011-02-07 14:06:03 +0100

    Fixed testAnd().

--- testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
@@ -17,11 +17,12 @@ public class LogicFunctionTest extends GenericTests {
 	 */
 	@Test
 	public void testAnd() throws Exception {
-		// TODO: Broken, because the GReQL parser removes all WhereExpressions
-		// and LetExpressions!
-		String queryString = "from var: V{Variable}, def: V{WhereExpression} with var  -->{IsVarOf} -->{IsDefinitionOf} def and 1=1 report var end";
-		JValue result = evalTestQuery("and", queryString);
-		assertEquals(4, result.toCollection().size());
+		String queryString = "from c: V{localities.County}, a:V{junctions.Airport} "
+				+ "with c -->{localities.ContainsLocality} -->{connections.AirRoute} a "
+				+ "and 1=1 report a end";
+		JValue result = evalTestQuery("and", queryString,
+				TestVersion.CITY_MAP_GRAPH);
+		assertEquals(3, result.toCollection().size());
 	}
 
 	/*
