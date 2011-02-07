742c8b4556e679496a30d47a04b13e9bc62e470f
mmce <devnull@localhost>
2011-02-07 14:12:46 +0100

    Fixed testOr().

--- testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
@@ -32,11 +32,12 @@ public class LogicFunctionTest extends GenericTests {
 	 */
 	@Test
 	public void testOr() throws Exception {
-		// TODO: Broken, because the GReQL parser removes all WhereExpressions
-		// and LetExpressions!
-		String queryString = "from var: V{Variable}, def: V{WhereExpression} with var  -->{IsVarOf} -->{IsDefinitionOf} def or 1=2 report var end";
-		JValue result = evalTestQuery("Or", queryString);
-		assertEquals(4, result.toCollection().size());
+		String queryString = "from c: V{localities.County}, a:V{junctions.Airport} "
+				+ "with c -->{localities.ContainsLocality} -->{connections.AirRoute} a "
+				+ "or 1=2 report a end";
+		JValue result = evalTestQuery("Or", queryString,
+				TestVersion.CITY_MAP_GRAPH);
+		assertEquals(3, result.toCollection().size());
 	}
 
 }
