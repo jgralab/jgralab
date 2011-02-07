c3bcd64699da266fa80637e934aca2121a364dd1
mmce <devnull@localhost>
2011-02-07 15:08:24 +0100

    Added the same tests but this time for functions and not for their infix or prefix usage.

--- testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
@@ -15,6 +15,17 @@ public class LogicFunctionTest extends GenericTests {
 	@Test
 	public void testAnd() throws Exception {
 		String queryString = "from el:list(1..100) "
+				+ "with and(true, false) report el end";
+		JValue result = evalTestQuery("and", queryString);
+		assertEquals(16, result.toCollection().size());
+	}
+
+	/*
+	 * Test method for the GReQL function 'and' as infix.
+	 */
+	@Test
+	public void testAndInfix() throws Exception {
+		String queryString = "from el:list(1..100) "
 				+ "with el % 2 = 0 and el % 3 = 0 report el end";
 		JValue result = evalTestQuery("and", queryString);
 		assertEquals(16, result.toCollection().size());
@@ -32,6 +43,17 @@ public class LogicFunctionTest extends GenericTests {
 	}
 
 	/*
+	 * Test method for the GReQL function 'not' as prefix.
+	 */
+	@Test
+	public void testNotPrefix() throws Exception {
+		String queryString = "from el:list(1..100) "
+				+ "with not(el % 2 = 0) report el end";
+		JValue result = evalTestQuery("or", queryString);
+		assertEquals(50, result.toCollection().size());
+	}
+
+	/*
 	 * Test method for the GReQL function 'or'.
 	 */
 	@Test
@@ -43,12 +65,34 @@ public class LogicFunctionTest extends GenericTests {
 	}
 
 	/*
+	 * Test method for the GReQL function 'or'.
+	 */
+	@Test
+	public void testOrInfix() throws Exception {
+		String queryString = "from el:list(1..100) "
+				+ "with or(el % 2 = 0, el % 3 = 0) report el end";
+		JValue result = evalTestQuery("or", queryString);
+		assertEquals(50 + 17, result.toCollection().size());
+	}
+
+	/*
 	 * Test method for the GReQL function 'xor'.
 	 */
 	@Test
 	public void testXor() throws Exception {
 		String queryString = "from el:list(1..100) "
 				+ "with el % 2 = 0 xor el % 3 = 0 report el end";
+		JValue result = evalTestQuery("or", queryString);
+		assertEquals(50 + 17 - 16, result.toCollection().size());
+	}
+
+	/*
+	 * Test method for the GReQL function 'xor'.
+	 */
+	@Test
+	public void testXorInfix() throws Exception {
+		String queryString = "from el:list(1..100) "
+				+ "with xor(el % 2 = 0, el % 3 = 0) report el end";
 		JValue result = evalTestQuery("or", queryString);
 		assertEquals(50 + 17 - 16, result.toCollection().size());
 	}
