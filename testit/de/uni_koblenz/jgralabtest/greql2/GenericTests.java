ed63ef03835eb8c27d3e02a7b406140389b348af
mmce <devnull@localhost>
2011-02-07 10:25:05 +0100

    Changed every static method printGraphAsDot to convertGraph.

--- testit/de/uni_koblenz/jgralabtest/greql2/GenericTests.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/GenericTests.java
@@ -280,7 +280,7 @@ public class GenericTests {
 			}
 			System.out.println(((SerializableGreql2) eval.getSyntaxGraph())
 					.serialize());
-			Tg2Dot.printGraphAsDot(eval.getSyntaxGraph(), true, dotFileName);
+			Tg2Dot.convertGraph(eval.getSyntaxGraph(), true, dotFileName);
 		}
 
 		printTestFunctionFooter(functionName);
