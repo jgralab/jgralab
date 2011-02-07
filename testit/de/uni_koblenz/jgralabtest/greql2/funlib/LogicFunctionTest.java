e968034600e2d4a3e9184c8d5d5a9a634f55ba6c
mmce <devnull@localhost>
2011-02-07 14:34:43 +0100

    Added another test method testNot.

--- testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
@@ -7,13 +7,11 @@ import org.junit.Test;
 import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
 import de.uni_koblenz.jgralabtest.greql2.GenericTests;
 
-// TODO the functions "not" and "xor" haven't been tested.
+// TODO the functions "not" haven't been tested.
 public class LogicFunctionTest extends GenericTests {
 
 	/*
-	 * Test method for
-	 * 'greql2.evaluator.GreqlEvaluator.evaluateSequentialPathDescription(SequentialPathDescription,
-	 * Graph)'
+	 * Test method for the GReQL function 'and'.
 	 */
 	@Test
 	public void testAnd() throws Exception {
@@ -25,17 +23,26 @@ public class LogicFunctionTest extends GenericTests {
 	}
 
 	/*
-	 * Test method for
-	 * 'greql2.evaluator.GreqlEvaluator.evaluateSequentialPathDescription(SequentialPathDescription,
-	 * Graph)'
+	 * Test method for the GReQL function 'not'.
+	 */
+	@Test
+	public void testNot() throws Exception {
+		String queryString = "from el:list(1..100) "
+				+ "with not (el % 2 = 0) report el end";
+		JValue result = evalTestQuery("or", queryString,
+				TestVersion.CITY_MAP_GRAPH);
+		assertEquals(50, result.toCollection().size());
+	}
+
+	/*
+	 * Test method for the GReQL function 'or'.
 	 */
 	@Test
 	public void testOr() throws Exception {
 		String queryString = "from el:list(1..100) "
 				+ "with el % 2 = 0 or el % 3 = 0 report el end";
-		JValue result = evalTestQuery("Or", queryString,
+		JValue result = evalTestQuery("or", queryString,
 				TestVersion.CITY_MAP_GRAPH);
 		assertEquals(50 + 17, result.toCollection().size());
 	}
-
 }
