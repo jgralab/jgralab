bd8b8e98b42a3c759d541295aa0f809bdfbf4719
mmce <devnull@localhost>
2011-02-07 14:37:04 +0100

    Added another test method testXor.
    Removed the TODO completely.

--- testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
 import de.uni_koblenz.jgralabtest.greql2.GenericTests;
 
-// TODO the functions "not" haven't been tested.
 public class LogicFunctionTest extends GenericTests {
 
 	/*
@@ -17,8 +16,7 @@ public class LogicFunctionTest extends GenericTests {
 	public void testAnd() throws Exception {
 		String queryString = "from el:list(1..100) "
 				+ "with el % 2 = 0 and el % 3 = 0 report el end";
-		JValue result = evalTestQuery("and", queryString,
-				TestVersion.CITY_MAP_GRAPH);
+		JValue result = evalTestQuery("and", queryString);
 		assertEquals(16, result.toCollection().size());
 	}
 
@@ -29,8 +27,7 @@ public class LogicFunctionTest extends GenericTests {
 	public void testNot() throws Exception {
 		String queryString = "from el:list(1..100) "
 				+ "with not (el % 2 = 0) report el end";
-		JValue result = evalTestQuery("or", queryString,
-				TestVersion.CITY_MAP_GRAPH);
+		JValue result = evalTestQuery("or", queryString);
 		assertEquals(50, result.toCollection().size());
 	}
 
@@ -41,8 +38,18 @@ public class LogicFunctionTest extends GenericTests {
 	public void testOr() throws Exception {
 		String queryString = "from el:list(1..100) "
 				+ "with el % 2 = 0 or el % 3 = 0 report el end";
-		JValue result = evalTestQuery("or", queryString,
-				TestVersion.CITY_MAP_GRAPH);
+		JValue result = evalTestQuery("or", queryString);
 		assertEquals(50 + 17, result.toCollection().size());
+	}
+
+	/*
+	 * Test method for the GReQL function 'xor'.
+	 */
+	@Test
+	public void testXor() throws Exception {
+		String queryString = "from el:list(1..100) "
+				+ "with el % 2 = 0 xor el % 3 = 0 report el end";
+		JValue result = evalTestQuery("or", queryString);
+		assertEquals(50 + 17 - 16, result.toCollection().size());
 	}
 }
