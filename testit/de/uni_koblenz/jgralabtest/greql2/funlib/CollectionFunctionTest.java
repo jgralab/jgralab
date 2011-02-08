f0ee2045a96d4dbc0f7565b5cc53d20d174a6833
mmce <devnull@localhost>
2011-02-08 15:01:37 +0100

    Moved the concat test to CollectionFunctionTest, because this test case concerns collections.

--- testit/de/uni_koblenz/jgralabtest/greql2/funlib/CollectionFunctionTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/funlib/CollectionFunctionTest.java
@@ -28,6 +28,20 @@ public class CollectionFunctionTest extends GenericTests {
 	}
 
 	@Test
+	public void testConcat2() throws Exception {
+		String queryString = "list(1..3) ++ list(4..6)";
+		JValue result = evalTestQuery("Concat", queryString);
+		JValueList l = new JValueList();
+		l.add(new JValueImpl(1));
+		l.add(new JValueImpl(2));
+		l.add(new JValueImpl(3));
+		l.add(new JValueImpl(4));
+		l.add(new JValueImpl(5));
+		l.add(new JValueImpl(6));
+		assertEquals(l, result);
+	}
+
+	@Test
 	public void testContainsKey1() throws Exception {
 		JValueMap map = new JValueMap();
 		map.put(new JValueImpl(1), new JValueImpl("a string"));
