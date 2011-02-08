9b6739eaa48a0090994066d4965abecb83c685c8
mmce <devnull@localhost>
2011-02-08 10:49:38 +0100

    Added a lot of special cases for "add".

--- testit/de/uni_koblenz/jgralabtest/greql2/funlib/ArithmeticFunctionTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/funlib/ArithmeticFunctionTest.java
@@ -1,6 +1,9 @@
 package de.uni_koblenz.jgralabtest.greql2.funlib;
 
 import static org.junit.Assert.assertEquals;
+import static java.lang.Double.NaN;
+import static java.lang.Double.NEGATIVE_INFINITY;
+import static java.lang.Double.POSITIVE_INFINITY;
 
 import org.junit.Test;
 
@@ -43,18 +46,114 @@ public class ArithmeticFunctionTest extends GenericTests {
 	public void testAdd6() throws Exception {
 		assertQueryEquals("Add", "0.025 + 0.975", 1.0);
 		assertQueryEquals("Add", "add(0.025, 0.975)", 1.0);
+
+		System.out.println("Add");
+		System.out.println(NaN + NaN);
+		System.out.println(NaN + POSITIVE_INFINITY);
+		System.out.println(NaN + NEGATIVE_INFINITY);
+		System.out.println(POSITIVE_INFINITY + POSITIVE_INFINITY);
+		System.out.println(POSITIVE_INFINITY + NEGATIVE_INFINITY);
+		System.out.println(NEGATIVE_INFINITY + NEGATIVE_INFINITY);
+
+		System.out.println("Sub");
+		System.out.println(NaN - NaN);
+		System.out.println(NaN - POSITIVE_INFINITY);
+		System.out.println(NaN - NEGATIVE_INFINITY);
+		System.out.println(POSITIVE_INFINITY - POSITIVE_INFINITY);
+		System.out.println(POSITIVE_INFINITY - NEGATIVE_INFINITY);
+		System.out.println(NEGATIVE_INFINITY - NEGATIVE_INFINITY);
+
+		System.out.println("Mul");
+		System.out.println(NaN * NaN);
+		System.out.println(NaN * POSITIVE_INFINITY);
+		System.out.println(NaN * NEGATIVE_INFINITY);
+		System.out.println(POSITIVE_INFINITY * POSITIVE_INFINITY);
+		System.out.println(POSITIVE_INFINITY * NEGATIVE_INFINITY);
+		System.out.println(NEGATIVE_INFINITY * NEGATIVE_INFINITY);
+
+		System.out.println("Div");
+		System.out.println(NaN / NaN);
+		System.out.println(NaN / POSITIVE_INFINITY);
+		System.out.println(NaN / NEGATIVE_INFINITY);
+		System.out.println(POSITIVE_INFINITY / POSITIVE_INFINITY);
+		System.out.println(POSITIVE_INFINITY / NEGATIVE_INFINITY);
+		System.out.println(NEGATIVE_INFINITY / NEGATIVE_INFINITY);
+
+		System.out.println("Mod");
+		System.out.println(NaN % NaN);
+		System.out.println(NaN % POSITIVE_INFINITY);
+		System.out.println(NaN % NEGATIVE_INFINITY);
+		System.out.println(POSITIVE_INFINITY % POSITIVE_INFINITY);
+		System.out.println(POSITIVE_INFINITY % NEGATIVE_INFINITY);
+		System.out.println(NEGATIVE_INFINITY % NEGATIVE_INFINITY);
+	}
+
+	@Test
+	public void testAddSpecialCase1() throws Exception {
+		assertQueryEquals("Add", "Infinity + 1.5", POSITIVE_INFINITY);
+		assertQueryEquals("Add", "add(Infinity, 1.5)", POSITIVE_INFINITY);
+	}
+
+	@Test
+	public void testAddSpecialCase2() throws Exception {
+		assertQueryEquals("Add", "6 + Infinity", POSITIVE_INFINITY);
+		assertQueryEquals("Add", "add(6, Infinity)", POSITIVE_INFINITY);
+	}
+
+	@Test
+	public void testAddSpecialCase3() throws Exception {
+		assertQueryEquals("Add", "-Infinity + 1.5", NEGATIVE_INFINITY);
+		assertQueryEquals("Add", "add(-Infinity, 1.5)", NEGATIVE_INFINITY);
+	}
+
+	@Test
+	public void testAddSpecialCase4() throws Exception {
+		assertQueryEquals("Add", "6 + -Infinity", NEGATIVE_INFINITY);
+		assertQueryEquals("Add", "add(6, -Infinity)", NEGATIVE_INFINITY);
+	}
+
+	@Test
+	public void testAddSpecialCase5() throws Exception {
+		assertQueryEquals("Add", "NaN + 1.5", NaN);
+		assertQueryEquals("Add", "add(-NaN, 1.5)", NEGATIVE_INFINITY);
+	}
+
+	@Test
+	public void testAddSpecialCase6() throws Exception {
+		assertQueryEquals("Add", "6 + -NaN", NaN);
+		assertQueryEquals("Add", "add(6, -NaN)", NaN);
+
+	}
+
+	@Test
+	public void testAddSpecialCases() throws Exception {
+
+		assertQueryEquals("Add", "Infinity + Infinity", POSITIVE_INFINITY);
+		assertQueryEquals("Add", "-Infinity + -Infinity", NEGATIVE_INFINITY);
+		assertQueryEquals("Add", "-NaN + -NaN", NaN);
+		assertQueryEquals("Add", "Infinity + -Infinity", NaN);
+		assertQueryEquals("Add", "Infinity + NaN", NaN);
+		assertQueryEquals("Add", "-Infinity + NaN", NaN);
+
+		// As functions
+		assertQueryEquals("Add", "add(Infinity, Infinity)", POSITIVE_INFINITY);
+		assertQueryEquals("Add", "add(-Infinity, -Infinity)", NEGATIVE_INFINITY);
+		assertQueryEquals("Add", "add(-NaN, -NaN)", NaN);
+		assertQueryEquals("Add", "add(Infinity, -Infinity)", NaN);
+		assertQueryEquals("Add", "add(Infinity, NaN)", NaN);
+		assertQueryEquals("Add", "add(-Infinity, NaN)", NaN);
 	}
 
 	@Test
 	public void testDiv1() throws Exception {
-		assertQueryEquals("Div", "3 / 0", Double.POSITIVE_INFINITY);
-		assertQueryEquals("Div", "div(3, 0)", Double.POSITIVE_INFINITY);
+		assertQueryEquals("Div", "3 / 0", POSITIVE_INFINITY);
+		assertQueryEquals("Div", "div(3, 0)", POSITIVE_INFINITY);
 	}
 
 	@Test
 	public void testDiv2() throws Exception {
-		assertQueryEquals("Div", "-3 / 0", Double.NEGATIVE_INFINITY);
-		assertQueryEquals("Div", "div(-3, 0)", Double.NEGATIVE_INFINITY);
+		assertQueryEquals("Div", "-3 / 0", NEGATIVE_INFINITY);
+		assertQueryEquals("Div", "div(-3, 0)", NEGATIVE_INFINITY);
 	}
 
 	@Test
@@ -79,6 +178,60 @@ public class ArithmeticFunctionTest extends GenericTests {
 	public void testDiv6() throws Exception {
 		assertQueryEquals("Div", "3 / 7", 3 / 7.0);
 		assertQueryEquals("Div", "div(3, 7)", 3 / 7.0);
+	}
+
+	@Test
+	public void testDiv7() throws Exception {
+		assertQueryEquals("Div", "Infinity / 7", POSITIVE_INFINITY);
+		assertQueryEquals("Div", "div(Infinity, 7)", POSITIVE_INFINITY);
+	}
+
+	@Test
+	public void testDiv9() throws Exception {
+		assertQueryEquals("Div", "3 / Infinity", POSITIVE_INFINITY);
+		assertQueryEquals("Div", "div(3, Infinity)", POSITIVE_INFINITY);
+	}
+
+	@Test
+	public void testDiv10() throws Exception {
+		assertQueryEquals("Div", "Infinity / Infinity", POSITIVE_INFINITY);
+		assertQueryEquals("Div", "div(Infinity, Infinity)", POSITIVE_INFINITY);
+	}
+
+	@Test
+	public void testDiv11() throws Exception {
+		assertQueryEquals("Div", "-Infinity / 7", NaN);
+		assertQueryEquals("Div", "div(-Infinity, 7)", NaN);
+	}
+
+	@Test
+	public void testDiv12() throws Exception {
+		assertQueryEquals("Div", "3 / -Infinity", NaN);
+		assertQueryEquals("Div", "div(3, -Infinity)", NaN);
+	}
+
+	@Test
+	public void testDiv13() throws Exception {
+		assertQueryEquals("Div", "-Infinity / -Infinity", NaN);
+		assertQueryEquals("Div", "div(-Infinity, -Infinity)", NaN);
+	}
+
+	@Test
+	public void testDiv14() throws Exception {
+		assertQueryEquals("Div", "NaN / 7", NaN);
+		assertQueryEquals("Div", "div(Nan, 7)", NaN);
+	}
+
+	@Test
+	public void testDiv15() throws Exception {
+		assertQueryEquals("Div", "3 / NaN", NaN);
+		assertQueryEquals("Div", "div(3, NaN)", NaN);
+	}
+
+	@Test
+	public void testDiv16() throws Exception {
+		assertQueryEquals("Div", "NaN / NaN", NaN);
+		assertQueryEquals("Div", "div(Nan, NaN)", NaN);
 	}
 
 	@Test
