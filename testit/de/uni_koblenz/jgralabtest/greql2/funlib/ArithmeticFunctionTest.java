81c2c427aa9d10524d2942c2b7a14943a1951a00
mmce <devnull@localhost>
2011-02-08 09:55:53 +0100

    Major restructuring using the new assertQueryEquals methods.

--- testit/de/uni_koblenz/jgralabtest/greql2/funlib/ArithmeticFunctionTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/funlib/ArithmeticFunctionTest.java
@@ -5,110 +5,87 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
-import de.uni_koblenz.jgralab.greql2.jvalue.JValueImpl;
 import de.uni_koblenz.jgralabtest.greql2.GenericTests;
 
 public class ArithmeticFunctionTest extends GenericTests {
 
-	private static final double DELTA = 0.00000001;
-
-	public void testArithmeticOperationAsDouble(String operationSign,
-			double arg1, double arg2, double expected) throws Exception {
-
-		String queryString = arg1 + " " + operationSign + " " + arg2;
-		JValue result = evalTestQuery(operationSign, queryString);
-		assertEquals(expected, result.toDouble().doubleValue(), DELTA);
-	}
-
-	public void testArithmeticOperationAsLong(String operationSign, long arg1,
-			long arg2, long expected) throws Exception {
-		String queryString = arg1 + " " + operationSign + " " + arg2;
-		JValue result = evalTestQuery(operationSign, queryString);
-		assertEquals(expected, result.toLong().longValue());
-	}
-
-	public void testArithmeticOperationAsInteger(String operationSign,
-			int arg1, int arg2, int expected) throws Exception {
-		String queryString = arg1 + " " + operationSign + " " + arg2;
-		JValue result = evalTestQuery(operationSign, queryString);
-		assertEquals(expected, result.toInteger().intValue());
-	}
+	// TODO create some methods named assertEqual receiving a greql string and
+	// the expected value. This should make it easier to understand and create
+	// new tests.
 
 	@Test
 	public void testAdd1() throws Exception {
-		testArithmeticOperationAsDouble("+", 6, 1.5, 7.5);
+		assertQueryEquals("Add", "6 + 1.5", 7.5);
 	}
 
 	@Test
 	public void testAdd2() throws Exception {
-		testArithmeticOperationAsDouble("+", 6, -1.5, 4.5);
+		assertQueryEquals("Add", "6 + -1.5", 4.5);
 	}
 
 	@Test
 	public void testAdd3() throws Exception {
-		testArithmeticOperationAsDouble("+", 0.025, 0.975, 1);
+		assertQueryEquals("Add", "0.025 + 0.975", 1.0);
 	}
 
 	@Test
 	public void testDiv1() throws Exception {
-		testArithmeticOperationAsDouble("/", 3, 3, 1);
+		assertQueryEquals("Div", "3 / 3", 1.0);
 	}
 
 	@Test
 	public void testDiv2() throws Exception {
-		testArithmeticOperationAsDouble("/", 3, 1, 3);
+		assertQueryEquals("Div", "3 / 1", 3.0);
 	}
 
 	@Test
 	public void testDiv3() throws Exception {
-		testArithmeticOperationAsDouble("/", 3, 7, 3 / 7.0);
+		assertQueryEquals("Div", "3 / 7", 3 / 7.0);
 	}
 
 	@Test
 	public void testSub1() throws Exception {
-		testArithmeticOperationAsDouble("-", 6, -1.5, 7.5);
+		assertQueryEquals("Sub", "6 - -1.5", 7.5);
 	}
 
 	@Test
 	public void testSub2() throws Exception {
-		testArithmeticOperationAsLong("-", 6, 3, 3);
+		assertQueryEquals("Sub", "6 - 3", 3L);
 	}
 
 	@Test
 	public void testSub3() throws Exception {
-		testArithmeticOperationAsLong("-", 16, 323, -307);
+		assertQueryEquals("Sub", "16 - 323", -307L);
 	}
 
 	@Test
 	public void testSub4() throws Exception {
-		testArithmeticOperationAsDouble("-", 1.5, 6, -4.5);
+		assertQueryEquals("Sub", "1.5 - 6", -4.5);
 	}
 
 	@Test
 	public void testSub5() throws Exception {
-		String queryString = "10 - 4 - 3 - 2";
-		JValue result = evalTestQuery("Sub5", queryString);
-		assertEquals(Integer.valueOf(1), result.toInteger());
+		assertQueryEquals("Sub", "10 - 4 - 3 - 2", 1);
 	}
 
 	@Test
 	public void testMod1() throws Exception {
-		testArithmeticOperationAsInteger("%", 9, 2, 1);
+		assertQueryEquals("Sub", "9 % 2", 1);
 	}
 
 	@Test
 	public void testMod2() throws Exception {
-		testArithmeticOperationAsInteger("%", -9, 2, -1);
+		assertQueryEquals("Mod", "-9 % 2", -1);
 	}
 
 	@Test
 	public void testMod3() throws Exception {
-		testArithmeticOperationAsInteger("%", 9, 3, 0);
+		assertQueryEquals("Mod", "9 % 3", 0);
 	}
 
 	@Test
 	public void testMul() throws Exception {
-		testArithmeticOperationAsDouble("*", 6, 1.5, 9);
+		assertQueryEquals("Mul", "6 * 1.5", 9.0);
 	}
 
 	@Test
@@ -125,9 +102,7 @@ public class ArithmeticFunctionTest extends GenericTests {
 
 	@Test
 	public void testMultiplicative() throws Exception {
-		String queryString = "100 / 10 / 5 * 2";
-		JValue result = evalTestQuery("Mod", queryString);
-		assertEquals(Double.valueOf(4), result.toDouble(), 0.01);
+		assertQueryEquals("Several Operations", "100 / 10 / 5 * 2", 4.0);
 	}
 
 }
