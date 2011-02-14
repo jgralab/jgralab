7e0a09356cca703ffe9e848857daaa87541aaa6f
mmce <devnull@localhost>
2011-02-14 15:22:08 +0100

    Added a WorkInProgress Annotation.

--- testit/de/uni_koblenz/jgralabtest/utilities/tg2dot/Tg2DotTest.java
+++ testit/de/uni_koblenz/jgralabtest/utilities/tg2dot/Tg2DotTest.java
@@ -6,8 +6,10 @@ import de.uni_koblenz.jgralab.Graph;
 import de.uni_koblenz.jgralab.GraphIO;
 import de.uni_koblenz.jgralab.GraphIOException;
 import de.uni_koblenz.jgralab.ProgressFunction;
+import de.uni_koblenz.jgralab.WorkInProgress;
 import de.uni_koblenz.jgralab.utilities.tg2dot.Tg2Dot;
 
+@WorkInProgress(responsibleDevelopers = "mmce@uni-koblenz.de", description = "More test have to be included. Every static method should be tested. Additionally the class itself should be tested.")
 public class Tg2DotTest {
 
 	@Test
