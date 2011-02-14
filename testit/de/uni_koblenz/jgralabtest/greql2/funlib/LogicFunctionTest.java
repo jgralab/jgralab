27c2a53b664267369b2470a2c1ec7b782e293082
mmce <devnull@localhost>
2011-02-14 15:25:51 +0100

    Removed several obsolete "or" test methods.

--- testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/funlib/LogicFunctionTest.java
@@ -92,50 +92,6 @@ public class LogicFunctionTest extends GenericTests {
 	}
 
 	/*
-	 * Test method for the GReQL function 'and' as infix.
-	 */
-	@Test
-	public void testAndInfix() throws Exception {
-		String queryString = "from el:list(1..100) "
-				+ "with el % 2 = 0 and el % 3 = 0 report el end";
-		JValue result = evalTestQuery("and", queryString);
-		assertEquals(16, result.toCollection().size());
-	}
-
-	/*
-	 * Test method for the GReQL function 'not'.
-	 */
-	@Test
-	public void testNot() throws Exception {
-		String queryString = "from el:list(1..100) "
-				+ "with not (el % 2 = 0) report el end";
-		JValue result = evalTestQuery("or", queryString);
-		assertEquals(50, result.toCollection().size());
-	}
-
-	/*
-	 * Test method for the GReQL function 'not' as prefix.
-	 */
-	@Test
-	public void testNotPrefix() throws Exception {
-		String queryString = "from el:list(1..100) "
-				+ "with not(el % 2 = 0) report el end";
-		JValue result = evalTestQuery("or", queryString);
-		assertEquals(50, result.toCollection().size());
-	}
-
-	/*
-	 * Test method for the GReQL function 'or' as infix.
-	 */
-	@Test
-	public void testOrInfix() throws Exception {
-		String queryString = "from el:list(1..100) "
-				+ "with el % 2 = 0 or el % 3 = 0 report el end";
-		JValue result = evalTestQuery("or", queryString);
-		assertEquals(50 + 17, result.toCollection().size());
-	}
-
-	/*
 	 * Test method for the GReQL function 'or'.
 	 */
 	@Test
