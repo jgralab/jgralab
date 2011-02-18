de5bf263cc5040f039e91bb2fb5bf5e016550156
Tassilo Horn <horn@uni-koblenz.de>
2011-02-18 15:52:49 +0100

    Backed out changeset 651c7019b687

--- src/de/uni_koblenz/jgralab/SSSSListeningState.java
+++
@@ -1,5 +0,0 @@
-package de.uni_koblenz.jgralab;
-
-public abstract class SSSSListeningState extends SSSListeningState {
-
-}
--- src/de/uni_koblenz/jgralab/SSSSSListeningState.java
+++
@@ -1,5 +0,0 @@
-package de.uni_koblenz.jgralab;
-
-public abstract class SSSSSListeningState extends SSSSListeningState {
-
-}
