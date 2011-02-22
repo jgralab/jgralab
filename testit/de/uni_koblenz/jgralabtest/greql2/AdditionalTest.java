235cff26bbb18da4fb88555682396dc30f781397
Eckhard Großmann <mmce@uni-koblenz.de>
2011-02-22 10:21:46 +0100

    Added a class to test for the record access.

---
+++ testit/de/uni_koblenz/jgralabtest/greql2/AdditionalTest.java
@@ -0,0 +1,19 @@
+package de.uni_koblenz.jgralabtest.greql2;
+
+import org.junit.Test;
+
+public class AdditionalTest extends GenericTests {
+
+	@Test
+	public void testRecordAccess() throws Exception {
+		evalTestQuery("rec (menue1:'bratwurst', menue2:'currywurst', menue3:'steak', menue4:'kaenguruhfleisch', menue5:'spiessbraten') store as x");
+
+		assertQueryEquals("using x: x.menue4", "kaenguruhfleisch");
+		assertQueryEquals("using x: x.menue1", "bratwurst");
+		assertQueryEquals("using x: x.menue5", "spiessbraten");
+		assertQueryEquals("using x: x.menue3", "steak");
+
+		assertQueryEqualsNull("using x: x.menue0");
+		assertQueryEqualsNull("using x: x.menue6");
+	}
+}
