ac007e5d3a9e728475d975d4c8d76c54d6d0f483
Tassilo Horn <horn@uni-koblenz.de>
2011-02-28 09:43:10 +0100

    fix misspelled name

--- src/de/uni_koblenz/jgralab/graphmarker/AbstractGraphMarker.java
+++ src/de/uni_koblenz/jgralab/graphmarker/AbstractGraphMarker.java
@@ -37,11 +37,11 @@ package de.uni_koblenz.jgralab.graphmarker;
 import de.uni_koblenz.jgralab.AttributedElement;
 import de.uni_koblenz.jgralab.Edge;
 import de.uni_koblenz.jgralab.Graph;
-import de.uni_koblenz.jgralab.GraphStructureChangedAdapterWithAutoRevome;
+import de.uni_koblenz.jgralab.GraphStructureChangedAdapterWithAutoRemove;
 import de.uni_koblenz.jgralab.Vertex;
 
 public abstract class AbstractGraphMarker<T extends AttributedElement> extends
-		GraphStructureChangedAdapterWithAutoRevome {
+		GraphStructureChangedAdapterWithAutoRemove {
 	protected final Graph graph;
 
 	protected AbstractGraphMarker(Graph graph) {
