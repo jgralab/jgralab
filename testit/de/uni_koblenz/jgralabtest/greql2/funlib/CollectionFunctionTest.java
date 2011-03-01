99265fbdc01fcbdcf0de181be6ff367c0c551f29
Eckhard Großmann <mmce@uni-koblenz.de>
2011-03-01 09:36:24 +0100

    Minimal changes.

--- testit/de/uni_koblenz/jgralabtest/greql2/funlib/CollectionFunctionTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/funlib/CollectionFunctionTest.java
@@ -42,10 +42,6 @@ import org.junit.Test;
 
 import de.uni_koblenz.jgralab.greql2.exception.EvaluateException;
 import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
-import de.uni_koblenz.jgralab.greql2.jvalue.JValueBag;
-import de.uni_koblenz.jgralab.greql2.jvalue.JValueList;
-import de.uni_koblenz.jgralab.greql2.jvalue.JValueMap;
-import de.uni_koblenz.jgralab.greql2.jvalue.JValueSet;
 import de.uni_koblenz.jgralabtest.greql2.GenericTests;
 
 public class CollectionFunctionTest extends GenericTests {
