781da7adb393e9b21154e4667dcffcfe947d2725
Eckhard Großmann <mmce@uni-koblenz.de>
2011-04-20 19:51:23 +0200

    Minimal Change.

--- testit/de/uni_koblenz/jgralabtest/greql2/evaluator/GreqlEvaluatorTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/evaluator/GreqlEvaluatorTest.java
@@ -1233,21 +1233,10 @@ public class GreqlEvaluatorTest extends GenericTest {
 	}
 
 	@Test
-	public void testEvaluateStartRestriction1() throws Exception {
-		String queryString = "from county: V{localities.County}, airport: V{junctions.Airport} with county -->{localities.ContainsLocality} {localities.Locality} & -->{connections.AirRoute} crossroad report county end";
-		JValue result = evalTestQuery(queryString);
-		assertEquals(3, result.toCollection().size());
-		JValue resultWO = evalQueryWithOptimizer(queryString);
-		assertEquals(result, resultWO);
-	}
-
-	@Test
 	public void testEvaluateStartRestriction5() throws Exception {
-		// TODO: Broken, because the GReQL parser removes all WhereExpressions
-		// and LetExpressions!
-		String queryString = "from var: V{Variable}, def: V{WhereExpression} with var -->{IsVarOf} {Definition} & -->{IsDefinitionOf} def report var end";
+		String queryString1 = "from var: V{Variable}, def: V{WhereExpression} with var -->{IsVarOf} {Definition} & -->{IsDefinitionOf} def report var end";
 		String queryString2 = "from var: V{Variable}, def: V{WhereExpression} with contains(-->{IsVarOf} {Definition} & -->{IsDefinitionOf} def, var) report var end";
-		JValue result = evalTestQuery("StartRestriction5", queryString);
+		JValue result = evalTestQuery("StartRestriction5", queryString1);
 		assertEquals(4, result.toCollection().size());
 		JValue resultWO = evalTestQuery("StartRestriction5 (wo)", queryString2);
 		assertEquals(result, resultWO);
