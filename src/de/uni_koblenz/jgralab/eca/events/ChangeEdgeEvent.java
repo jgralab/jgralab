a4e2de5c707066e62e20fbd4eb662d42ced72e77
kheckelmann <devnull@localhost>
2011-05-25 14:47:18 +0200

    create and delete vertex, create edge and change edge now tested for rule without condition - seems to work

--- src/de/uni_koblenz/jgralab/eca/events/ChangeEdgeEvent.java
+++ src/de/uni_koblenz/jgralab/eca/events/ChangeEdgeEvent.java
@@ -1,11 +1,20 @@
 package de.uni_koblenz.jgralab.eca.events;
 
+import de.uni_koblenz.jgralab.AttributedElement;
 import de.uni_koblenz.jgralab.eca.EventManager;
 
 public class ChangeEdgeEvent extends Event {
 
-	public ChangeEdgeEvent(EventManager manager, EventTime time) {
-		super(manager, time);
+	public ChangeEdgeEvent(EventManager manager, EventTime time,Class <? extends AttributedElement> type) {
+		super(manager, time,type);
+		if(time.equals(EventTime.BEFORE)){
+			manager.getBeforeChangeEdgeEvents().add(this);
+		}else{
+			manager.getAfterChangeEdgeEvents().add(this);
+		}
+	}
+	public ChangeEdgeEvent(EventManager manager, EventTime time,String contextExpr) {
+		super(manager, time,contextExpr);
 		if(time.equals(EventTime.BEFORE)){
 			manager.getBeforeChangeEdgeEvents().add(this);
 		}else{
--- src/de/uni_koblenz/jgralab/eca/events/CreateEdgeEvent.java
+++ src/de/uni_koblenz/jgralab/eca/events/CreateEdgeEvent.java
@@ -1,20 +1,29 @@
 package de.uni_koblenz.jgralab.eca.events;
 
+import de.uni_koblenz.jgralab.AttributedElement;
 import de.uni_koblenz.jgralab.Edge;
 import de.uni_koblenz.jgralab.eca.ECARule;
 import de.uni_koblenz.jgralab.eca.EventManager;
 
 public class CreateEdgeEvent extends Event{
 
-	public CreateEdgeEvent(EventManager manager, EventTime time) {
-		super(manager,time);
+	public CreateEdgeEvent(EventManager manager, EventTime time, Class <? extends AttributedElement> type) {
+		super(manager,time,type);
 		if(time.equals(EventTime.BEFORE)){
 			manager.getBeforeCreateEdgeEvents().add(this);
 		}else{
 			manager.getAfterCreateEdgeEvents().add(this);
 		}
 	}
-
+	public CreateEdgeEvent(EventManager manager, EventTime time, String contextExpr) {
+		super(manager,time,contextExpr);
+		if(time.equals(EventTime.BEFORE)){
+			manager.getBeforeCreateEdgeEvents().add(this);
+		}else{
+			manager.getAfterCreateEdgeEvents().add(this);
+		}
+	}
+	
 	public void fire(Class<? extends Edge> edgeClass){
 		for(ECARule rule : rules){
 			if(this.getType().equals(edgeClass)){
