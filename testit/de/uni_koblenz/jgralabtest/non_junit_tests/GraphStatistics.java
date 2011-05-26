54a4721b044cbe2db92efbd751bb1f0f4c069f64
riediger <devnull@localhost>
2011-05-26 16:30:30 +0200

    Added simple graph counter

---
+++ testit/de/uni_koblenz/jgralabtest/non_junit_tests/GraphStatistics.java
@@ -0,0 +1,51 @@
+package de.uni_koblenz.jgralabtest.non_junit_tests;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.uni_koblenz.jgralab.Edge;
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.GraphIO;
+import de.uni_koblenz.jgralab.GraphIOException;
+import de.uni_koblenz.jgralab.Vertex;
+import de.uni_koblenz.jgralab.codegenerator.CodeGeneratorConfiguration;
+import de.uni_koblenz.jgralab.impl.ConsoleProgressFunction;
+import de.uni_koblenz.jgralab.schema.NamedElement;
+
+public class GraphStatistics {
+	static Map<NamedElement, Integer> counters = new HashMap<NamedElement, Integer>();
+
+	public static void main(String[] args) {
+		try {
+			Graph g = GraphIO.loadSchemaAndGraphFromFile(args[0],
+					CodeGeneratorConfiguration.MINIMAL,
+					new ConsoleProgressFunction());
+
+			for (Vertex v : g.vertices()) {
+				count(v.getAttributedElementClass());
+				count(v.getAttributedElementClass().getPackage());
+			}
+
+			for (Edge e : g.edges()) {
+				count(e.getAttributedElementClass());
+				count(e.getAttributedElementClass().getPackage());
+			}
+			for (NamedElement e : counters.keySet()) {
+				System.out.println(e.getClass().getSimpleName() + ";"
+						+ e.getQualifiedName() + ";" + counters.get(e));
+			}
+		} catch (GraphIOException e) {
+
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static void count(NamedElement e) {
+		if (counters.containsKey(e)) {
+			counters.put(e, counters.get(e) + 1);
+		} else {
+			counters.put(e, 1);
+		}
+	}
+}
