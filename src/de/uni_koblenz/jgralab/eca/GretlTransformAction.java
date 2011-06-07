70fb7738748c51cffc0437a0123304614569b4ed
kheckelmann <devnull@localhost>
2011-06-07 10:04:07 +0200

    gretl transformation as action added

--- src/de/uni_koblenz/jgralab/eca/GretlTransformAction.java
+++ src/de/uni_koblenz/jgralab/eca/GretlTransformAction.java
@@ -1,15 +1,19 @@
 package de.uni_koblenz.jgralab.eca;
 
 
-// import de.uni_koblenz.jgralab.gretl.Context;
-// import de.uni_koblenz.jgralab.gretl.Transformation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.gretl.Context;
+import de.uni_koblenz.jgralab.gretl.Transformation;
 
 public class GretlTransformAction extends Action {
 
 	/**
 	 * Class of Transformation
 	 */
-	//Class<? extends Transformation<Graph>> transformationClass;
+	Class<? extends Transformation<Graph>> transformationClass;
 	
 	/**
 	 * Creates a new GretlTransformAction with the given Transformation Class
@@ -17,8 +21,8 @@ public class GretlTransformAction extends Action {
 	 * @param t
 	 *            the Transformation Class
 	 */
-	public GretlTransformAction(/* Class<? extends Transformation<Graph>> t */) {
-		/* this.transformationClass = t; */
+	public GretlTransformAction(Class<? extends Transformation<Graph>> t) {
+		this.transformationClass = t;
 	}
 
 
@@ -27,31 +31,40 @@ public class GretlTransformAction extends Action {
 	 */
 	@Override
 	public void doAction(){
-		/*
-		 * try { Graph graph = this.getRule().getECARuleManager().getGraph();
-		 * 
-		 * Context context = new Context("", ""); context.setSourceGraph(graph);
-		 * 
-		 * Constructor<? extends Transformation<Graph>> constr =
-		 * transformationClass .getConstructor(Context.class);
-		 * Transformation<Graph> transform = constr.newInstance(context);
-		 * 
-		 * transform.execute();
-		 * 
-		 * } catch (SecurityException e) {
-		 * System.err.println("Gretl transformation failed!");
-		 * e.printStackTrace(); } catch (NoSuchMethodException e) {
-		 * System.err.println("Gretl transformation failed!");
-		 * e.printStackTrace(); } catch (IllegalArgumentException e) {
-		 * System.err.println("Gretl transformation failed!");
-		 * e.printStackTrace(); } catch (InstantiationException e) {
-		 * System.err.println("Gretl transformation failed!");
-		 * e.printStackTrace(); } catch (IllegalAccessException e) {
-		 * System.err.println("Gretl transformation failed!");
-		 * e.printStackTrace(); } catch (InvocationTargetException e) {
-		 * System.err.println("Gretl transformation failed!");
-		 * e.printStackTrace(); }
-		 */
+
+		try {
+			Graph graph = this.getRule().getECARuleManager().getGraph();
+
+			Context context = new Context(graph.getSchema());
+			context.setSourceGraph(graph);
+			context.setTargetGraph(graph);
+
+			Constructor<? extends Transformation<Graph>> constr = transformationClass
+					.getConstructor(Context.class);
+			Transformation<Graph> transform = constr.newInstance(context);
+
+			transform.execute();
+
+		} catch (SecurityException e) {
+			System.err.println("Gretl transformation "
+					+ this.transformationClass.getName() + " failed!");
+		} catch (NoSuchMethodException e) {
+			System.err.println("Gretl transformation "
+					+ this.transformationClass.getName() + " failed!");
+		} catch (IllegalArgumentException e) {
+			System.err.println("Gretl transformation "
+					+ this.transformationClass.getName() + " failed!");
+		} catch (InstantiationException e) {
+			System.err.println("Gretl transformation "
+					+ this.transformationClass.getName() + " failed!");
+		} catch (IllegalAccessException e) {
+			System.err.println("Gretl transformation "
+					+ this.transformationClass.getName() + " failed!");
+		} catch (InvocationTargetException e) {
+			System.err.println("Gretl transformation "
+					+ this.transformationClass.getName() + " failed!");
+		}
+
 	}
 
 }
