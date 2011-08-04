5b870083feb7029267214e0deb0668df81f0af3e
kheckelmann <devnull@localhost>
2011-08-04 10:39:09 +0200

    start a testcase for IO

---
+++ testit/de/uni_koblenz/jgralabtest/eca/ECATestIO.java
@@ -0,0 +1,122 @@
+package de.uni_koblenz.jgralabtest.eca;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.uni_koblenz.jgralab.JGraLab;
+import de.uni_koblenz.jgralab.eca.Action;
+import de.uni_koblenz.jgralab.eca.Condition;
+import de.uni_koblenz.jgralab.eca.ECAIO;
+import de.uni_koblenz.jgralab.eca.ECAIOException;
+import de.uni_koblenz.jgralab.eca.ECARule;
+import de.uni_koblenz.jgralab.eca.PrintAction;
+import de.uni_koblenz.jgralab.eca.events.CreateVertexEventDescription;
+import de.uni_koblenz.jgralab.eca.events.DeleteVertexEventDescription;
+import de.uni_koblenz.jgralab.eca.events.EventDescription;
+import de.uni_koblenz.jgralabtest.eca.schemas.simplelibrary.Book;
+import de.uni_koblenz.jgralabtest.eca.schemas.simplelibrary.Library;
+import de.uni_koblenz.jgralabtest.eca.schemas.simplelibrary.Loans;
+import de.uni_koblenz.jgralabtest.eca.schemas.simplelibrary.Magazin;
+import de.uni_koblenz.jgralabtest.eca.schemas.simplelibrary.MediaType;
+import de.uni_koblenz.jgralabtest.eca.schemas.simplelibrary.NewMedia;
+import de.uni_koblenz.jgralabtest.eca.schemas.simplelibrary.SimpleLibraryGraph;
+import de.uni_koblenz.jgralabtest.eca.schemas.simplelibrary.SimpleLibrarySchema;
+import de.uni_koblenz.jgralabtest.eca.schemas.simplelibrary.User;
+
+public class ECATestIO {
+
+	private static SimpleLibraryGraph simlibgraph;
+
+	@BeforeClass
+	public static void setUp() {
+		System.out.println("Start ECA IO Test.\n");
+		JGraLab.setLogLevel(Level.OFF);
+		initGraph();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		System.out.println("Finish ECA IO Test.\n");
+	}
+
+	@Test
+	public void testSaveRule() {
+		EventDescription bef_ev = new DeleteVertexEventDescription(
+				EventDescription.EventTime.BEFORE, Book.class);
+		Action bef_act = new PrintAction(
+				"ECA Test Message: Book Vertex will become deleted.");
+		ECARule bef_rule = new ECARule(bef_ev, bef_act);
+
+		EventDescription aft_ev = new CreateVertexEventDescription(
+				EventDescription.EventTime.AFTER, NewMedia.class);
+		Condition aft_cond = new Condition("count( V{NewMedia} ) = 2");
+		Action aft_act = new PrintAction(
+				"ECA Test Message: New Medium after Condition Test created. "
+						+ "This message should appear only once.");
+		ECARule aft_rule = new ECARule(aft_ev, aft_cond, aft_act);
+
+		ArrayList<ECARule> rules = new ArrayList<ECARule>();
+		rules.add(bef_rule);
+		rules.add(aft_rule);
+		try {
+			ECAIO.saveECArules(
+					simlibgraph.getSchema(),
+					"testit/de/uni_koblenz/jgralabtest/eca/io/testSaveRules1.eca",
+					rules);
+		} catch (ECAIOException e) {
+			e.printStackTrace();
+			assert false;
+		}
+	}
+
+
+	public static void initGraph() {
+		SimpleLibraryGraph graph = SimpleLibrarySchema.instance()
+				.createSimpleLibraryGraph();
+		graph.set_version("v1.0");
+
+		// Library
+		Library lib = graph.createLibrary();
+		lib.set_name("Bibliothequa");
+
+		// Users
+		User user1 = graph.createUser();
+		user1.set_name("Martin King");
+
+		User user2 = graph.createUser();
+		user2.set_name("Stephanie Plum");
+
+		// Media
+		Book book1 = graph.createBook();
+		book1.set_title("Lord of the Rings");
+		book1.set_author("J.R.R. Tokien");
+
+		Magazin magazin1 = graph.createMagazin();
+		magazin1.set_title("CT");
+		magazin1.set_publisher("Blub");
+		magazin1.set_year(2011);
+
+		NewMedia newmedia1 = graph.createNewMedia();
+		newmedia1.set_title("Rush Hour 1");
+		newmedia1.set_type(MediaType.DVD);
+
+		// Media are in Library
+		lib.add_media(book1);
+		lib.add_media(magazin1);
+		lib.add_media(newmedia1);
+
+		// user1 loans book1
+		Loans loans_u1_b1 = graph.createLoans(user1, book1);
+		loans_u1_b1.set_date(graph.createDate(1, 1, 2011));
+
+		// user 2 loans magazin1
+		Loans loans_u2_m1 = graph.createLoans(user2, magazin1);
+		loans_u2_m1.set_date(graph.createDate(5, 5, 2011));
+
+		simlibgraph = graph;
+	}
+}
