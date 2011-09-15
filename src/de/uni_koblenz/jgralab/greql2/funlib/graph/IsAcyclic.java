18be4cb90a30a4237a5130145ca7f9826910a549
riediger <devnull@localhost>
2011-09-15 14:16:58 +0200

    Replaced KahnKnuth by DFS.

--- src/de/uni_koblenz/jgralab/greql2/funlib/graph/IsAcyclic.java
+++ src/de/uni_koblenz/jgralab/greql2/funlib/graph/IsAcyclic.java
@@ -2,19 +2,20 @@ package de.uni_koblenz.jgralab.greql2.funlib.graph;
 
 import de.uni_koblenz.jgralab.Graph;
 import de.uni_koblenz.jgralab.algolib.algorithms.AlgorithmTerminatedException;
-import de.uni_koblenz.jgralab.algolib.algorithms.topological_order.KahnKnuthAlgorithm;
+import de.uni_koblenz.jgralab.algolib.algorithms.search.DepthFirstSearch;
+import de.uni_koblenz.jgralab.algolib.algorithms.search.IterativeDepthFirstSearch;
+import de.uni_koblenz.jgralab.algolib.algorithms.topological_order.TopologicalOrderWithDFS;
 import de.uni_koblenz.jgralab.greql2.funlib.Function;
 
 public class IsAcyclic extends Function {
 	public IsAcyclic() {
-		super(
-				"Returns true iff the current graph or the given subgraph is cycle-free.\n"
-						+ "See also: topologicalSort().", 100, 1, 0.1,
+		super("Returns true iff the graph $g$ is acyclic.", 100, 1, 0.1,
 				Category.GRAPH);
 	}
 
-	public Boolean evaluate(Graph graph) {
-		KahnKnuthAlgorithm a = new KahnKnuthAlgorithm(graph, null, null);
+	public Boolean evaluate(Graph g) {
+		DepthFirstSearch dfs = new IterativeDepthFirstSearch(g);
+		TopologicalOrderWithDFS a = new TopologicalOrderWithDFS(g, dfs);
 		try {
 			a.execute();
 		} catch (AlgorithmTerminatedException e) {
--- src/de/uni_koblenz/jgralab/greql2/funlib/graph/TopologicalSort.java
+++ src/de/uni_koblenz/jgralab/greql2/funlib/graph/TopologicalSort.java
@@ -1,21 +1,42 @@
 package de.uni_koblenz.jgralab.greql2.funlib.graph;
 
+import org.pcollections.ArrayPVector;
 import org.pcollections.PVector;
 
 import de.uni_koblenz.jgralab.Graph;
 import de.uni_koblenz.jgralab.Vertex;
+import de.uni_koblenz.jgralab.algolib.algorithms.AlgorithmTerminatedException;
+import de.uni_koblenz.jgralab.algolib.algorithms.search.DepthFirstSearch;
+import de.uni_koblenz.jgralab.algolib.algorithms.search.IterativeDepthFirstSearch;
+import de.uni_koblenz.jgralab.algolib.algorithms.topological_order.TopologicalOrderWithDFS;
+import de.uni_koblenz.jgralab.algolib.functions.Permutation;
 import de.uni_koblenz.jgralab.greql2.funlib.Function;
 
 public class TopologicalSort extends Function {
 	public TopologicalSort() {
 		super(
-				"Returns a list of vertices in topological order if the graph is acyclic. Otherwise, the result is undefined.\n"
-						+ "The topological sort can be restricted to a subgraph.\n"
-						+ "See also: isAcyclic().", 100, 1, 0.1, Category.GRAPH);
+				"Returns a list of vertices in topological order if the graph $g$is acyclic."
+						+ " Otherwise, the result is undefined.", 100, 1, 0.1,
+				Category.GRAPH);
 	}
 
-	public PVector<? extends Vertex> evaluate(Graph graph) {
-		return evaluate(graph);
+	public PVector<? extends Vertex> evaluate(Graph g) {
+		DepthFirstSearch dfs = new IterativeDepthFirstSearch(g);
+		TopologicalOrderWithDFS a = new TopologicalOrderWithDFS(g, dfs);
+		try {
+			a.execute();
+		} catch (AlgorithmTerminatedException e) {
+			throw new RuntimeException(e.getMessage(), e.getCause());
+		}
+		if (!a.isAcyclic()) {
+			return null;
+		}
+		PVector<Vertex> result = ArrayPVector.empty();
+		Permutation<Vertex> t = a.getTopologicalOrder();
+		for (Vertex v : t.getRangeElements()) {
+			result = result.plus(v);
+		}
+		return result;
 	}
 
 	@Override
