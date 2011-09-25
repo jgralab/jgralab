481a574bd3978cec205841b4a47b81ce0e726c69
riediger <devnull@localhost>
2011-09-25 10:34:01 +0200

    Added 2 GReQL functions

---
+++ src/de/uni_koblenz/jgralab/greql2/funlib/graph/EdgesConnected.java
@@ -0,0 +1,32 @@
+package de.uni_koblenz.jgralab.greql2.funlib.graph;
+
+import org.pcollections.ArrayPVector;
+import org.pcollections.PVector;
+
+import de.uni_koblenz.jgralab.Edge;
+import de.uni_koblenz.jgralab.EdgeDirection;
+import de.uni_koblenz.jgralab.Vertex;
+import de.uni_koblenz.jgralab.greql2.funlib.Function;
+import de.uni_koblenz.jgralab.greql2.types.TypeCollection;
+
+public class EdgesConnected extends Function {
+
+	public EdgesConnected() {
+		super("Returns the list of edges of vertex $v$", 2, 5, 1.0,
+				Category.GRAPH);
+	}
+
+	public PVector<Edge> evaluate(Vertex v) {
+		return evaluate(v, null);
+	}
+
+	public PVector<Edge> evaluate(Vertex v, TypeCollection tc) {
+		PVector<Edge> result = ArrayPVector.empty();
+		for (Edge e : v.incidences(EdgeDirection.INOUT)) {
+			if (tc == null || tc.acceptsType(e.getAttributedElementClass())) {
+				result = result.plus(e);
+			}
+		}
+		return result;
+	}
+}
