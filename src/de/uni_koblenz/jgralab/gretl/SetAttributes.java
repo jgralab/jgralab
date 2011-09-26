031cf705da79b1031cce3f45baa612a1b20cd954
Tassilo Horn <horn@uni-koblenz.de>
2011-09-26 11:55:27 +0200

    adapted attribute ops

--- src/de/uni_koblenz/jgralab/gretl/SetAttributes.java
+++ src/de/uni_koblenz/jgralab/gretl/SetAttributes.java
@@ -1,36 +1,22 @@
 package de.uni_koblenz.jgralab.gretl;
 
-import java.lang.reflect.Constructor;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-
-import org.pcollections.ArrayPMap;
-import org.pcollections.PMap;
 
 import de.uni_koblenz.jgralab.AttributedElement;
-import de.uni_koblenz.jgralab.Record;
-import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
-import de.uni_koblenz.jgralab.greql2.jvalue.JValueMap;
-import de.uni_koblenz.jgralab.greql2.jvalue.JValueRecord;
 import de.uni_koblenz.jgralab.gretl.Context.GReTLVariableType;
 import de.uni_koblenz.jgralab.gretl.Context.TransformationPhase;
 import de.uni_koblenz.jgralab.schema.Attribute;
-import de.uni_koblenz.jgralab.schema.CollectionDomain;
-import de.uni_koblenz.jgralab.schema.Domain;
-import de.uni_koblenz.jgralab.schema.MapDomain;
-import de.uni_koblenz.jgralab.schema.RecordDomain;
 
 public class SetAttributes extends
 		Transformation<Map<AttributedElement, Object>> {
 
 	private Attribute attribute = null;
-	private JValueMap archetype2valueMap = null;
+	private Map<Object, Object> archetype2valueMap = null;
 	private String semanticExpression = null;
 
 	public SetAttributes(final Context c, final Attribute attr,
-			final JValueMap archetypeValueMap) {
+			final Map<Object, Object> archetypeValueMap) {
 		super(c);
 		attribute = attr;
 		archetype2valueMap = archetypeValueMap;
@@ -57,98 +43,42 @@ public class SetAttributes extends
 		}
 
 		if (archetype2valueMap == null) {
-			archetype2valueMap = context.evaluateGReQLQuery(semanticExpression)
-					.toJValueMap();
+			archetype2valueMap = context.evaluateGReQLQuery(semanticExpression);
 		}
 
 		HashMap<AttributedElement, Object> resultMap = new HashMap<AttributedElement, Object>(
 				archetype2valueMap.size());
-		for (JValue sourceElement : archetype2valueMap.keySet()) {
+		for (Object archetype : archetype2valueMap.keySet()) {
 			// System.out.println("sourceElement = " + sourceElement);
 			// context.printMappings();
-			JValue targetElemValue = context.getImg(
-					attribute.getAttributedElementClass()).get(sourceElement);
-			if (targetElemValue == null) {
+			AttributedElement image = context.getImg(
+					attribute.getAttributedElementClass()).get(archetype);
+			if (image == null) {
 				String qname = attribute.getAttributedElementClass()
 						.getQualifiedName();
 				throw new GReTLException(context, "The source graph element '"
-						+ sourceElement
+						+ archetype
 						+ "' has no image in "
 						+ Context.toGReTLVarNotation(qname,
 								GReTLVariableType.IMG)
 						+ " yet, so no attribute '" + attribute.getName()
 						+ "' can be created!");
 			}
-			AttributedElement targetElem = targetElemValue
-					.toAttributedElement();
-			JValue val = archetype2valueMap.get(sourceElement);
-			resultMap.put(targetElem, val);
+			Object val = archetype2valueMap.get(archetype);
+			resultMap.put(image, val);
 			if (val != null) {
-				Object o = null;
-
-				o = convertJValueToAttributeValue(val);
-				targetElem.setAttribute(attribute.getName(), o);
+				Object o = convertToAttributeValue(val);
+				image.setAttribute(attribute.getName(), o);
 			}
 		}
 
 		return resultMap;
 	}
 
-	private Object convertJValueToAttributeValue(JValue val) {
-		Domain attrDom = attribute.getDomain();
-		if (attrDom instanceof RecordDomain) {
-			return convertJValueRecordToRecord(val.toJValueRecord());
-		} else if ((attrDom instanceof CollectionDomain)
-				&& (((CollectionDomain) attrDom).getBaseDomain() instanceof RecordDomain)) {
-			@SuppressWarnings("unchecked")
-			Collection<Object> coll = (Collection<Object>) val.toCollection()
-					.toObject();
-			coll.clear();
-			for (JValue i : val.toCollection()) {
-				coll.add(convertJValueRecordToRecord(i.toJValueRecord()));
-			}
-			return coll;
-		} else if (attrDom instanceof MapDomain) {
-			MapDomain md = (MapDomain) attrDom;
-			Domain kd = md.getKeyDomain();
-			Domain vd = md.getValueDomain();
-			if ((kd instanceof RecordDomain) || (vd instanceof RecordDomain)) {
-				JValueMap jmap = val.toJValueMap();
-				PMap<Object, Object> map = ArrayPMap.empty();
-				Object k = null, v = null;
-				for (Entry<JValue, JValue> e : jmap.entrySet()) {
-					if (kd instanceof RecordDomain) {
-						k = convertJValueRecordToRecord(e.getKey()
-								.toJValueRecord());
-					} else {
-						k = e.getKey().toObject();
-					}
-					if (vd instanceof RecordDomain) {
-						v = convertJValueRecordToRecord(e.getValue()
-								.toJValueRecord());
-					} else {
-						v = e.getValue().toObject();
-					}
-					map = map.plus(k, v);
-				}
-				return map;
-			}
-		}
-		return val.toObject();
+	private Object convertToAttributeValue(Object val) {
+		// TODO: Implement proper conversion from GReQL result to domain
+		// (records, enums,...)
+		return val;
 	}
 
-	@SuppressWarnings("unchecked")
-	private Record convertJValueRecordToRecord(JValueRecord jrec) {
-		// TODO (ido) implement construction via reflection
-		RecordDomain rd = (RecordDomain) attribute.getDomain();
-		Constructor<? extends Record> c;
-		try {
-			c = (Constructor<? extends Record>) rd.getM1Class().getConstructor(
-					Map.class);
-			Map<String, Object> compVals = jrec.toObject();
-			return c.newInstance(compVals);
-		} catch (Exception e) {
-			throw new RuntimeException("Can't convert JValue to Record", e);
-		}
-	}
 }
