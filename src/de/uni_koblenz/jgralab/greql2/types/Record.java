1052f4e7742b68102e2ab9f2b4bee0ecd11187d1
Volker Riediger <riediger@uni-koblenz.de>
2011-11-03 13:02:55 +0100

    Added hashCode and equals methods to Record.

--- src/de/uni_koblenz/jgralab/greql2/types/Record.java
+++ src/de/uni_koblenz/jgralab/greql2/types/Record.java
@@ -1,6 +1,7 @@
 package de.uni_koblenz.jgralab.greql2.types;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 import org.pcollections.ArrayPSet;
@@ -48,8 +49,55 @@ public class Record implements de.uni_koblenz.jgralab.Record {
 	}
 
 	@Override
+	public boolean hasComponent(String name) {
+		return entries.containsKey(name);
+	}
+
+	@Override
 	public List<String> getComponentNames() {
 		return ((ArrayPSet<String>) entries.keySet()).toPVector();
+	}
+
+	@Override
+	public int size() {
+		return entries.size();
+	}
+
+	@Override
+	public int hashCode() {
+		int h = 0;
+		for (Object v : entries.values()) {
+			h += v.hashCode();
+		}
+		return h;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (obj instanceof Record) {
+			return entries.equals(((Record) obj).entries);
+		}
+		if (obj instanceof de.uni_koblenz.jgralab.Record) {
+			de.uni_koblenz.jgralab.Record r = (de.uni_koblenz.jgralab.Record) obj;
+			if (size() != r.size()) {
+				return false;
+			}
+			try {
+				Iterator<Object> v = entries.values().iterator();
+				for (String k : entries.keySet()) {
+					if (!r.getComponent(k).equals(v.next())) {
+						return false;
+					}
+				}
+				return true;
+			} catch (NoSuchAttributeException e) {
+				return false;
+			}
+		}
+		return false;
 	}
 
 	@Override
