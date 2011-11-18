0005b48d7343e2d789f7134e23dbd323b43f9487
Tassilo Horn <horn@uni-koblenz.de>
2011-11-18 09:54:03 +0100

    Fix some type parameter issues resulting in compile errors with certain javacs

--- src/de/uni_koblenz/jgralab/impl/ReversedEdgeBaseImpl.java
+++ src/de/uni_koblenz/jgralab/impl/ReversedEdgeBaseImpl.java
@@ -1,29 +1,29 @@
 /*
  * JGraLab - The Java Graph Laboratory
- * 
+ *
  * Copyright (C) 2006-2011 Institute for Software Technology
  *                         University of Koblenz-Landau, Germany
  *                         ist@uni-koblenz.de
- * 
+ *
  * For bug reports, documentation and further information, visit
- * 
+ *
  *                         http://jgralab.uni-koblenz.de
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see <http://www.gnu.org/licenses>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7
- * 
+ *
  * If you modify this Program, or any covered work, by linking or combining
  * it with Eclipse (or a modified version of that program or an Eclipse
  * plugin), containing parts covered by the terms of the Eclipse Public
@@ -44,7 +44,7 @@ import de.uni_koblenz.jgralab.schema.EdgeClass;
 
 /**
  * TODO add comment
- * 
+ *
  * @author ist@uni-koblenz.de
  */
 public abstract class ReversedEdgeBaseImpl extends IncidenceImpl implements
@@ -93,7 +93,7 @@ public abstract class ReversedEdgeBaseImpl extends IncidenceImpl implements
 
 	@Override
 	public <T> T getAttribute(String name) {
-		return normalEdge.getAttribute(name);
+		return normalEdge.<T>getAttribute(name);
 	}
 
 	@Override
