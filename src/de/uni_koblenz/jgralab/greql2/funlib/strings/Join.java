fdb99269a281ca85ba7a373e332419d41e72c779
Volker Riediger <riediger@uni-koblenz.de>
2011-11-18 13:23:36 +0100

    Added GReQL function "join".

---
+++ src/de/uni_koblenz/jgralab/greql2/funlib/strings/Join.java
@@ -0,0 +1,23 @@
+package de.uni_koblenz.jgralab.greql2.funlib.strings;
+
+import java.util.List;
+
+import de.uni_koblenz.jgralab.greql2.funlib.Function;
+
+public class Join extends Function {
+	public Join() {
+		super(
+				"Joins the given strings in $l$ by interleaving with $delimiter$.",
+				10, 3, 0.1, Category.STRINGS);
+	}
+
+	public String evaluate(List<String> l, String delimiter) {
+		StringBuilder sb = new StringBuilder();
+		String d = "";
+		for (String s : l) {
+			sb.append(d).append(s);
+			d = delimiter;
+		}
+		return sb.toString();
+	}
+}
