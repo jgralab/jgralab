140747e88362ca0ed1059a066a8a148df57abd46
dbildh <devnull@localhost>
2011-11-30 09:24:45 +0100

    Fixed bugs caused by unregistered functions, some minor updates and cleanups

--- src/de/uni_koblenz/jgralab/greql2/funlib/graph/EdgeSetSubgraph.java
+++ src/de/uni_koblenz/jgralab/greql2/funlib/graph/EdgeSetSubgraph.java
@@ -1,36 +1,33 @@
 package de.uni_koblenz.jgralab.greql2.funlib.graph;
 
-
 import org.pcollections.PCollection;
 
 import de.uni_koblenz.jgralab.Edge;
 import de.uni_koblenz.jgralab.Graph;
-import de.uni_koblenz.jgralab.Vertex;
 import de.uni_koblenz.jgralab.graphmarker.SubGraphMarker;
 import de.uni_koblenz.jgralab.greql2.funlib.Function;
 import de.uni_koblenz.jgralab.greql2.funlib.NeedsGraphArgument;
 
 @NeedsGraphArgument
-public class ElementSetSubgraph extends Function {
+public class EdgeSetSubgraph extends Function {
 
-	public ElementSetSubgraph() {
+	public EdgeSetSubgraph() {
 		super(
-				"Returns the subgraph induced by the vertex type given.",
+				"Returns the subgraph induced by the edge set given.",
 				7, 1, 1.0, Category.GRAPH);
 	}
 
-	public SubGraphMarker evaluate(Graph graph, PCollection<Vertex> vertexSet, PCollection<Edge> edgeSet) {
+	public SubGraphMarker evaluate(Graph graph, PCollection<Edge> edgeSet) {
 		SubGraphMarker subgraphMarker = new SubGraphMarker(graph);
-		for (Vertex currentVertex : vertexSet) {
-			subgraphMarker.mark(currentVertex);
-		}
-		// add all edges
-		for (Edge currentEdge : edgeSet) {
-			if ((subgraphMarker.isMarked(currentEdge.getAlpha()) && (subgraphMarker.isMarked(currentEdge.getOmega())))) {
+		Edge currentEdge = graph.getFirstEdge();
+		while (currentEdge != null) {
+			if (edgeSet.contains(currentEdge)) {
 				subgraphMarker.mark(currentEdge);
+				subgraphMarker.mark(currentEdge.getAlpha());
+				subgraphMarker.mark(currentEdge.getOmega());
 			}
+			currentEdge = currentEdge.getNextEdge();
 		}
-		
 		return subgraphMarker;
 	}
 	
