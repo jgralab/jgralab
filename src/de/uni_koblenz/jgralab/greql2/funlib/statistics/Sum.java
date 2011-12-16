f5a25b1e052488f264778dc38e1dde799762d1a9
Volker Riediger <riediger@uni-koblenz.de>
2011-12-16 18:38:13 +0100

    Fixed GReQLs sum function to return the best fitting type.

--- src/de/uni_koblenz/jgralab/greql2/funlib/statistics/Sum.java
+++ src/de/uni_koblenz/jgralab/greql2/funlib/statistics/Sum.java
@@ -12,11 +12,44 @@ public class Sum extends Function {
 				Category.STATISTICS);
 	}
 
-	public Double evaluate(Collection<Number> l) {
+	public Number evaluate(Collection<Number> l) {
 		if (l.isEmpty()) {
-			return 0.0;
+			return 0;
 		} else {
-			double sum = 0.0;
+			Class<? extends Number> resultType = Integer.class;
+			// determine best fitting result type
+			for (Number n : l) {
+				if (n instanceof Integer) {
+					continue;
+				}
+				if (n instanceof Long) {
+					if (resultType == Integer.class) {
+						resultType = Long.class;
+					}
+					continue;
+				}
+				if (n instanceof Double) {
+					resultType = Double.class;
+					break;
+				}
+				throw new IllegalArgumentException(
+						"sum can't handle numbers of type " + n.getClass());
+			}
+			if (resultType == Integer.class) {
+				int sum = 0;
+				for (Number n : l) {
+					sum += n.intValue();
+				}
+				return sum;
+			}
+			if (resultType == Long.class) {
+				long sum = 0;
+				for (Number n : l) {
+					sum += n.longValue();
+				}
+				return sum;
+			}
+			double sum = 0;
 			for (Number n : l) {
 				sum += n.doubleValue();
 			}
