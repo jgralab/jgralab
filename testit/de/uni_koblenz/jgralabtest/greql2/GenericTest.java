74be928eb6f98219e8734a406465085186f023c7
Tassilo Horn <horn@uni-koblenz.de>
2011-12-23 12:59:01 +0100

    make isPrime working for ints

--- testit/de/uni_koblenz/jgralabtest/greql2/GenericTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/GenericTest.java
@@ -1,29 +1,29 @@
 /*
  * JGraLab - The Java Graph Laboratory
- * 
+ *
  * Copyright (C) 2006-2011 Institute for Software Technology
  *                         University of Koblenz-Landau, Germany
  *                         ist@uni-koblenz.de
- * 
+ *
  * For bug reports, documentation and further information, visit
- * 
+ *
  *                         http://jgralab.uni-koblenz.de
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see <http://www.gnu.org/licenses>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7
- * 
+ *
  * If you modify this Program, or any covered work, by linking or combining
  * it with Eclipse (or a modified version of that program or an Eclipse
  * plugin), containing parts covered by the terms of the Eclipse Public
@@ -102,7 +102,7 @@ public class GenericTest {
 		Object result = test.evalTestQuery(queryString);
 
 		uncontainedCrossroadCount = crossroadCount
-				- ((Double) result).intValue();
+				- ((Number) result).intValue();
 	}
 
 	protected void assertQueryIsUndefined(String query) throws Exception {
@@ -161,8 +161,8 @@ public class GenericTest {
 			assertEquals(expectedResult, result);
 
 		} catch (AssertionError ex) {
-			if (result instanceof PCollection
-					&& expectedResult instanceof PCollection) {
+			if ((result instanceof PCollection)
+					&& (expectedResult instanceof PCollection)) {
 				PCollection<?> col = (PCollection<?>) result;
 				PCollection<?> col2 = (PCollection<?>) expectedResult;
 				col = col.minusAll(col2);
@@ -222,7 +222,7 @@ public class GenericTest {
 
 	private boolean doesExceptionTypesEqual(
 			Class<? extends Exception> exceptionClass, Throwable exception) {
-		return exception != null
+		return (exception != null)
 				&& (exception.getClass().equals(exceptionClass) || doesExceptionTypesEqual(
 						exceptionClass, exception.getCause()));
 	}
@@ -326,9 +326,9 @@ public class GenericTest {
 		for (int i = 0; i < 15; i++) {
 			v[i] = g.createNode();
 		}
-		for (int i = 0; i < (v.length - 1) / 2; i++) {
-			g.createLink(v[i], v[i * 2 + 1]);
-			g.createLink(v[i], v[i * 2 + 2]);
+		for (int i = 0; i < ((v.length - 1) / 2); i++) {
+			g.createLink(v[i], v[(i * 2) + 1]);
+			g.createLink(v[i], v[(i * 2) + 2]);
 		}
 		return g;
 	}
