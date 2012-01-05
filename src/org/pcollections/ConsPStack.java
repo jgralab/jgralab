d786d2f1c37c8cc10fb8256b3b4c2a395cb5d831
Volker Riediger <riediger@uni-koblenz.de>
2012-01-05 11:15:41 +0100

    PCollections compile without warnings now.

--- src/org/pcollections/ConsPStack.java
+++ src/org/pcollections/ConsPStack.java
@@ -5,171 +5,213 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.ListIterator;
 
-
-
 /**
  * 
  * A simple persistent stack of non-null values.
  * <p>
- * This implementation is thread-safe (assuming Java's AbstractSequentialList is thread-safe),
- * although its iterators may not be.
+ * This implementation is thread-safe (assuming Java's AbstractSequentialList is
+ * thread-safe), although its iterators may not be.
  * 
  * @author harold
- *
+ * 
  * @param <E>
  */
-public final class ConsPStack<E> extends AbstractSequentialList<E> implements PStack<E> {
-//// STATIC FACTORY METHODS ////
+@SuppressWarnings("deprecation")
+public final class ConsPStack<E> extends AbstractSequentialList<E> implements
+		PStack<E> {
+	// // STATIC FACTORY METHODS ////
 	private static final ConsPStack<Object> EMPTY = new ConsPStack<Object>();
-	
+
 	/**
 	 * @param <E>
 	 * @return an empty stack
 	 */
 	@SuppressWarnings("unchecked")
 	public static <E> ConsPStack<E> empty() {
-		return (ConsPStack<E>)EMPTY; }
-	
+		return (ConsPStack<E>) EMPTY;
+	}
+
 	/**
 	 * @param <E>
 	 * @param e
 	 * @return empty().plus(e)
 	 */
 	public static <E> ConsPStack<E> singleton(final E e) {
-		return ConsPStack.<E>empty().plus(e); }
-	
+		return ConsPStack.<E> empty().plus(e);
+	}
+
 	/**
 	 * @param <E>
 	 * @param list
-	 * @return a stack consisting of the elements of list in the order of list.iterator()
+	 * @return a stack consisting of the elements of list in the order of
+	 *         list.iterator()
 	 */
 	@SuppressWarnings("unchecked")
 	public static <E> ConsPStack<E> from(final Collection<? extends E> list) {
-		if(list instanceof ConsPStack)
-			return (ConsPStack<E>)list; //(actually we only know it's ConsPStack<? extends E>)
-									// but that's good enough for an immutable
-									// (i.e. we can't mess someone else up by adding the wrong type to it)
+		if (list instanceof ConsPStack) {
+			return (ConsPStack<E>) list; // (actually we only know it's
+											// ConsPStack<? extends E>)
+		}
+		// but that's good enough for an immutable
+		// (i.e. we can't mess someone else up by adding the wrong type to it)
 		return from(list.iterator());
 	}
-	
+
 	private static <E> ConsPStack<E> from(final Iterator<? extends E> i) {
-		if(!i.hasNext()) return empty();
+		if (!i.hasNext()) {
+			return empty();
+		}
 		E e = i.next();
 		return from(i).plus(e);
 	}
 
-	
-//// PRIVATE CONSTRUCTORS ////
-	private final E first; private final ConsPStack<E> rest;
+	// // PRIVATE CONSTRUCTORS ////
+	private final E first;
+	private final ConsPStack<E> rest;
 	private final int size;
+
 	// not externally instantiable (or subclassable):
 	private ConsPStack() { // EMPTY constructor
-		if(EMPTY!=null)
-			throw new RuntimeException("empty constructor should only be used once");
-		size = 0; first=null; rest=null;
+		if (EMPTY != null) {
+			throw new RuntimeException(
+					"empty constructor should only be used once");
+		}
+		size = 0;
+		first = null;
+		rest = null;
 	}
+
 	private ConsPStack(final E first, final ConsPStack<E> rest) {
-		this.first = first; this.rest = rest;
-		
+		this.first = first;
+		this.rest = rest;
+
 		size = 1 + rest.size;
 	}
-	
-	
-//// REQUIRED METHODS FROM AbstractSequentialList ////
+
+	// // REQUIRED METHODS FROM AbstractSequentialList ////
 	@Override
 	public int size() {
-		return size; }
-	
+		return size;
+	}
+
 	@Override
 	public ListIterator<E> listIterator(final int index) {
-		if(index<0 || index>size) throw new IndexOutOfBoundsException();
-		
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException();
+		}
+
 		return new ListIterator<E>() {
 			int i = index;
 			ConsPStack<E> next = subList(index);
 
 			public boolean hasNext() {
-				return next.size>0; }
+				return next.size > 0;
+			}
+
 			public boolean hasPrevious() {
-				return i>0; }
+				return i > 0;
+			}
+
 			public int nextIndex() {
-				return index; }
+				return index;
+			}
+
 			public int previousIndex() {
-				return index-1; }
+				return index - 1;
+			}
+
 			public E next() {
 				E e = next.first;
 				next = next.rest;
 				return e;
 			}
+
 			public E previous() {
-				System.err.println("ConsPStack.listIterator().previous() is inefficient, don't use it!");
-				next = subList(index-1); // go from beginning...
+				System.err
+						.println("ConsPStack.listIterator().previous() is inefficient, don't use it!");
+				next = subList(index - 1); // go from beginning...
 				return next.first;
 			}
 
 			public void add(final E o) {
-				throw new UnsupportedOperationException(); }
+				throw new UnsupportedOperationException();
+			}
+
 			public void remove() {
-				throw new UnsupportedOperationException(); }
+				throw new UnsupportedOperationException();
+			}
+
 			public void set(final E o) {
-				throw new UnsupportedOperationException(); }
+				throw new UnsupportedOperationException();
+			}
 		};
 	}
 
-
-//// OVERRIDDEN METHODS FROM AbstractSequentialList ////
+	// // OVERRIDDEN METHODS FROM AbstractSequentialList ////
 	@Override
 	public ConsPStack<E> subList(final int start, final int end) {
-		if(start<0 || end>size || start>end)
+		if (start < 0 || end > size || start > end) {
 			throw new IndexOutOfBoundsException();
-		if(end==size) // want a substack
+		}
+		if (end == size) {
 			return subList(start); // this is faster
-		if(start==end) // want nothing
+		}
+		if (start == end) {
 			return empty();
-		if(start==0) // want the current element
-			return new ConsPStack<E>(first, rest.subList(0, end-1));
+		}
+		if (start == 0) {
+			return new ConsPStack<E>(first, rest.subList(0, end - 1));
+		}
 		// otherwise, don't want the current element:
-		return rest.subList(start-1, end-1);
+		return rest.subList(start - 1, end - 1);
 	}
-	
-	
-//// IMPLEMENTED METHODS OF PStack ////
+
+	// // IMPLEMENTED METHODS OF PStack ////
 	public ConsPStack<E> plus(final E e) {
 		return new ConsPStack<E>(e, this);
 	}
-	
+
 	public ConsPStack<E> plusAll(final Collection<? extends E> list) {
 		ConsPStack<E> result = this;
-		for(E e : list)
+		for (E e : list) {
 			result = result.plus(e);
+		}
 		return result;
 	}
 
 	public ConsPStack<E> plus(final int i, final E e) {
-		if(i<0 || i>size)
+		if (i < 0 || i > size) {
 			throw new IndexOutOfBoundsException();
-		if(i==0) // insert at beginning
+		}
+		if (i == 0) {
 			return plus(e);
-		return new ConsPStack<E>(first, rest.plus(i-1, e));
+		}
+		return new ConsPStack<E>(first, rest.plus(i - 1, e));
 	}
 
 	public ConsPStack<E> plusAll(final int i, final Collection<? extends E> list) {
 		// TODO inefficient if list.isEmpty()
-		if(i<0 || i>size)
+		if (i < 0 || i > size) {
 			throw new IndexOutOfBoundsException();
-		if(i==0)
+		}
+		if (i == 0) {
 			return plusAll(list);
-		return new ConsPStack<E>(first, rest.plusAll(i-1, list));
+		}
+		return new ConsPStack<E>(first, rest.plusAll(i - 1, list));
 	}
-	
+
 	public ConsPStack<E> minus(final Object e) {
-		if(size==0)
+		if (size == 0) {
 			return this;
-		if(first.equals(e)) // found it
+		}
+		if (first.equals(e)) {
 			return rest; // don't recurse (only remove one)
+		}
 		// otherwise keep looking:
 		ConsPStack<E> newRest = rest.minus(e);
-		if(newRest==rest) return this;
+		if (newRest == rest) {
+			return this;
+		}
 		return new ConsPStack<E>(first, newRest);
 	}
 
@@ -178,33 +220,59 @@ public final class ConsPStack<E> extends AbstractSequentialList<E> implements PS
 	}
 
 	public ConsPStack<E> minusAll(final Collection<?> list) {
-		if(size==0)
+		if (size == 0) {
 			return this;
-		if(list.contains(first)) // get rid of current element
+		}
+		if (list.contains(first)) {
 			return rest.minusAll(list); // recursively delete all
+		}
 		// either way keep looking:
 		ConsPStack<E> newRest = rest.minusAll(list);
-		if(newRest==rest) return this;
+		if (newRest == rest) {
+			return this;
+		}
 		return new ConsPStack<E>(first, newRest);
 	}
-	
+
 	public ConsPStack<E> with(final int i, final E e) {
-		if(i<0 || i>=size)
+		if (i < 0 || i >= size) {
 			throw new IndexOutOfBoundsException();
-		if(i==0) {
-			if(first.equals(e)) return this;
+		}
+		if (i == 0) {
+			if (first.equals(e)) {
+				return this;
+			}
 			return new ConsPStack<E>(e, rest);
 		}
-		ConsPStack<E> newRest = rest.with(i-1, e);
-		if(newRest==rest) return this;
+		ConsPStack<E> newRest = rest.with(i - 1, e);
+		if (newRest == rest) {
+			return this;
+		}
 		return new ConsPStack<E>(first, newRest);
 	}
 
 	public ConsPStack<E> subList(final int start) {
-		if(start<0 || start>size)
+		if (start < 0 || start > size) {
 			throw new IndexOutOfBoundsException();
-		if(start==0)
+		}
+		if (start == 0) {
 			return this;
-		return rest.subList(start-1);
+		}
+		return rest.subList(start - 1);
 	}
+
+	@Override
+	public boolean add(E e) {
+		throw new UnsupportedOperationException();
+	};
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public E set(int index, E element) {
+		throw new UnsupportedOperationException();
+	};
 }
--- src/org/pcollections/TreePVector.java
+++ src/org/pcollections/TreePVector.java
@@ -5,43 +5,44 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
-
-
 /**
  * 
  * A persistent vector of non-null elements.
  * <p>
- * This implementation is backed by an IntTreePMap and
- * supports logarithmic-time querying, setting, insertion,
- * and removal.
+ * This implementation is backed by an IntTreePMap and supports logarithmic-time
+ * querying, setting, insertion, and removal.
  * <p>
- * This implementation is thread-safe (assuming Java's AbstractList is thread-safe)
- * although its iterators may not be.
+ * This implementation is thread-safe (assuming Java's AbstractList is
+ * thread-safe) although its iterators may not be.
  * 
  * @author harold
- *
+ * 
  * @param <E>
  */
+@SuppressWarnings("deprecation")
 public class TreePVector<E> extends AbstractList<E> implements PVector<E> {
-//// STATIC FACTORY METHODS ////
-	private static final TreePVector<Object> EMPTY = new TreePVector<Object>(IntTreePMap.empty());
-	
+	// // STATIC FACTORY METHODS ////
+	private static final TreePVector<Object> EMPTY = new TreePVector<Object>(
+			IntTreePMap.empty());
+
 	/**
 	 * @param <E>
 	 * @return an empty vector
 	 */
 	@SuppressWarnings("unchecked")
 	public static <E> TreePVector<E> empty() {
-		return (TreePVector<E>)EMPTY; }
-	
+		return (TreePVector<E>) EMPTY;
+	}
+
 	/**
 	 * @param <E>
 	 * @param e
 	 * @return empty().plus(e)
 	 */
 	public static <E> TreePVector<E> singleton(final E e) {
-		return TreePVector.<E>empty().plus(e); }
-	
+		return TreePVector.<E> empty().plus(e);
+	}
+
 	/**
 	 * @param <E>
 	 * @param list
@@ -49,107 +50,148 @@ public class TreePVector<E> extends AbstractList<E> implements PVector<E> {
 	 */
 	@SuppressWarnings("unchecked")
 	public static <E> TreePVector<E> from(final Collection<? extends E> list) {
-		if(list instanceof TreePVector)
-			return (TreePVector<E>)list; //(actually we only know it's TreePVector<? extends E>)
-									// but that's good enough for an immutable
-									// (i.e. we can't mess someone else up by adding the wrong type to it)
-		return TreePVector.<E>empty().plusAll(list);
+		if (list instanceof TreePVector) {
+			return (TreePVector<E>) list; // (actually we only know it's
+											// TreePVector<? extends E>)
+		}
+		// but that's good enough for an immutable
+		// (i.e. we can't mess someone else up by adding the wrong type to it)
+		return TreePVector.<E> empty().plusAll(list);
 	}
 
-//// PRIVATE CONSTRUCTORS ////
+	// // PRIVATE CONSTRUCTORS ////
 	private final IntTreePMap<E> map;
-	private TreePVector(final IntTreePMap<E> map) {
-		this.map = map; }
-	
 
-//// REQUIRED METHODS FROM AbstractList ////
+	private TreePVector(final IntTreePMap<E> map) {
+		this.map = map;
+	}
+
+	// // REQUIRED METHODS FROM AbstractList ////
 	@Override
 	public int size() {
-		return map.size(); }
-	
+		return map.size();
+	}
+
 	@Override
 	public E get(final int index) {
-		if(index<0 || index>=size())
+		if (index < 0 || index >= size()) {
 			throw new IndexOutOfBoundsException();
+		}
 		return map.get(index);
 	}
-	
 
-//// OVERRIDDEN METHODS FROM AbstractList ////
+	// // OVERRIDDEN METHODS FROM AbstractList ////
 	@Override
 	public Iterator<E> iterator() {
-		return map.values().iterator(); }
+		return map.values().iterator();
+	}
 
 	@Override
 	public TreePVector<E> subList(final int start, final int end) {
 		final int size = size();
-		if(start<0 || end>size || start>end)
+		if (start < 0 || end > size || start > end) {
 			throw new IndexOutOfBoundsException();
-		if(start==end)
+		}
+		if (start == end) {
 			return empty();
-		if(start==0) {
-			if(end==size) return this;
+		}
+		if (start == 0) {
+			if (end == size) {
+				return this;
+			}
 			// remove from end:
-			return this.minus(size-1).subList(start, end);
+			return this.minus(size - 1).subList(start, end);
 		}
 		// remove from start:
-		return this.minus(0).subList(start-1,end-1);
+		return this.minus(0).subList(start - 1, end - 1);
 	}
-	
 
-////IMPLEMENTED METHODS OF PVector ////
+	// //IMPLEMENTED METHODS OF PVector ////
 	public TreePVector<E> plus(final E e) {
-		return new TreePVector<E>( map.plus(size(), e) ); }
+		return new TreePVector<E>(map.plus(size(), e));
+	}
 
 	public TreePVector<E> plus(final int i, final E e) {
-		if(i<0 || i>size())
+		if (i < 0 || i > size()) {
 			throw new IndexOutOfBoundsException();
-		return new TreePVector<E>( map.withKeysChangedAbove(i,1).plus(i,e) );
+		}
+		return new TreePVector<E>(map.withKeysChangedAbove(i, 1).plus(i, e));
 	}
 
 	public TreePVector<E> minus(final Object e) {
-		for(Entry<Integer,E> entry : map.entrySet())
-			if(entry.getValue().equals(e))
-				return minus((int)entry.getKey());
+		for (Entry<Integer, E> entry : map.entrySet()) {
+			if (entry.getValue().equals(e)) {
+				return minus((int) entry.getKey());
+			}
+		}
 		return this;
 	}
 
 	public TreePVector<E> minus(final int i) {
-		if(i<0 || i>=size())
+		if (i < 0 || i >= size()) {
 			throw new IndexOutOfBoundsException();
-		return new TreePVector<E>( map.minus(i).withKeysChangedAbove(i,-1) );
+		}
+		return new TreePVector<E>(map.minus(i).withKeysChangedAbove(i, -1));
 	}
 
 	public TreePVector<E> plusAll(final Collection<? extends E> list) {
 		TreePVector<E> result = this;
-		for(E e : list)
+		for (E e : list) {
 			result = result.plus(e);
+		}
 		return result;
 	}
 
 	public TreePVector<E> minusAll(final Collection<?> list) {
 		TreePVector<E> result = this;
-		for(Object e : list)
+		for (Object e : list) {
 			result = result.minus(e);
+		}
 		return result;
 	}
 
 	public TreePVector<E> plusAll(int i, final Collection<? extends E> list) {
-		if(i<0 || i>size())
+		if (i < 0 || i > size()) {
 			throw new IndexOutOfBoundsException();
-		if(list.size()==0)
+		}
+		if (list.size() == 0) {
 			return this;
+		}
 		IntTreePMap<E> map = this.map.withKeysChangedAbove(i, list.size());
-		for(E e : list)
+		for (E e : list) {
 			map = map.plus(i++, e);
-		return new TreePVector<E>( map );
+		}
+		return new TreePVector<E>(map);
 	}
 
 	public PVector<E> with(final int i, final E e) {
-		if(i<0 || i>=size())
+		if (i < 0 || i >= size()) {
 			throw new IndexOutOfBoundsException();
+		}
 		IntTreePMap<E> map = this.map.plus(i, e);
-		if(map==this.map) return this;
-		return new TreePVector<E>( map );
+		if (map == this.map) {
+			return this;
+		}
+		return new TreePVector<E>(map);
+	}
+
+	@Override
+	public boolean add(E e) {
+		throw new UnsupportedOperationException();
+	};
+
+	@Override
+	public void add(int index, E element) {
+		throw new UnsupportedOperationException();
+	};
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) {
+		throw new UnsupportedOperationException();
 	}
 }
