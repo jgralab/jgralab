a03a27b80ffa567ed10bad898e436507006ff46d
Daniel Janke <danijank@uni-koblenz.de>
2012-01-24 11:33:29 +0100

    Added methods to InternalGreqlEvaluator interface.

--- src/de/uni_koblenz/jgralab/greql2/evaluator/InternalGreqlEvaluator.java
+++ src/de/uni_koblenz/jgralab/greql2/evaluator/InternalGreqlEvaluator.java
@@ -3,4 +3,12 @@ package de.uni_koblenz.jgralab.greql2.evaluator;
 public interface InternalGreqlEvaluator {
 	// TODO [greqlevaluator] create internal interface
 
+	public Object setGlobalVariable(String varName, Object value);
+
+	public Object getGolabalVariableValue(String varName);
+
+	public Object setLocalVariable(String varName, Object value);
+
+	public Object getLocalVariableValue(String varName);
+
 }
