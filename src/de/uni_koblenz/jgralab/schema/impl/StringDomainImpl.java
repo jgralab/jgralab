91c8c5fd26d1d5f87007d48467618ec085b06f63
blubomski <devnull@localhost>
2012-02-03 22:34:52 +0100

    slightly optimized isConformGenericValue (it's implemented in the respective classes: no need to get the class-Object by its name in order to check if a value is an instance)

--- src/de/uni_koblenz/jgralab/schema/impl/StringDomainImpl.java
+++ src/de/uni_koblenz/jgralab/schema/impl/StringDomainImpl.java
@@ -1,29 +1,29 @@
 /*
  * JGraLab - The Java Graph Laboratory
- * 
+ *
  * Copyright (C) 2006-2011 Institute for Software Technology
  *                         University of Koblenz-Landau, Germany
  *                         ist@uni-koblenz.de
- * 
+ *
  * For bug reports, documentation and further information, visit
- * 
+ *
  *                         http://jgralab.uni-koblenz.de
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see <http://www.gnu.org/licenses>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7
- * 
+ *
  * If you modify this Program, or any covered work, by linking or combining
  * it with Eclipse (or a modified version of that program or an Eclipse
  * plugin), containing parts covered by the terms of the Eclipse Public
@@ -142,11 +142,6 @@ public final class StringDomainImpl extends BasicDomainImpl implements
 
 	@Override
 	public boolean isConformGenericValue(Object value) {
-		try {
-			return value == null
-					|| Class.forName(getJavaClassName(null)).isInstance(value);
-		} catch (ClassNotFoundException e) {
-			return false;
-		}
+		return value == null || String.class.isInstance(value);
 	}
 }
