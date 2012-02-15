d62c5174cae490758d7f7b96719d8de70649bf4b
Bernhard Lubomski <blubomski@uni-koblenz.de>
2012-02-15 15:40:29 +0100

    Fixed bug: creating instances of abstract VertexClasses and EdgeClasses is no longer possible. Added corresponding tests.

--- src/de/uni_koblenz/jgralab/impl/generic/GenericGraphImpl.java
+++ src/de/uni_koblenz/jgralab/impl/generic/GenericGraphImpl.java
@@ -1,29 +1,29 @@
 /*
  * JGraLab - The Java Graph Laboratory
- * 
+ *
  * Copyright (C) 2006-2012 Institute for Software Technology
  *                         University of Koblenz-Landau, Germany
  *                         ist@uni-koblenz.de
- * 
+ *
  * For bug reports, documentation and further information, visit
- * 
+ *
  *                         https://github.com/jgralab/jgralab
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see <http://www.gnu.org/licenses>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7
- * 
+ *
  * If you modify this Program, or any covered work, by linking or combining
  * it with Eclipse (or a modified version of that program or an Eclipse
  * plugin), containing parts covered by the terms of the Eclipse Public
@@ -248,30 +248,6 @@ public class GenericGraphImpl extends GraphImpl {
 			return null;
 		}
 	}
-
-	// protected int getAttributeIndex(AttributedElementClass<?, ?> aec,
-	// String name) {
-	// assert (aec.getAttributeCount() > 0);
-	// Map<String, Integer> indexMap = getIndexMap(aec);
-	// Integer i = indexMap.get(name);
-	// return i == null ? Integer.MAX_VALUE : i.intValue();
-	// }
-
-	// protected Map<String, Integer> getIndexMap(AttributedElementClass<?, ?>
-	// aec) {
-	// if (attributeIndexMaps.containsKey(aec)) {
-	// return attributeIndexMaps.get(aec);
-	// } else {
-	// HashMap<String, Integer> valueIndex = new HashMap<String, Integer>();
-	// int i = 0;
-	// for (Attribute a : aec.getAttributeList()) {
-	// valueIndex.put(a.getName(), i);
-	// ++i;
-	// }
-	// attributeIndexMaps.put(aec, valueIndex);
-	// return valueIndex;
-	// }
-	// }
 
 	/**
 	 * Initializes attributes of an (generic) AttributedElement with their
