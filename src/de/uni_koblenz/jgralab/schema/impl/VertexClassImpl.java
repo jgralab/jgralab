b7b0e125d71880462b2ed0190682b453613142de
Tassilo Horn <horn@uni-koblenz.de>
2012-02-27 20:29:57 +0100

    Fix bug in VertexClassImpl.getDirectedEdgeClassForFarEndRole().
    fixes #2.

--- src/de/uni_koblenz/jgralab/schema/impl/VertexClassImpl.java
+++ src/de/uni_koblenz/jgralab/schema/impl/VertexClassImpl.java
@@ -1,29 +1,29 @@
 /*
  * JGraLab - The Java Graph Laboratory
- * 
+ *
  * Copyright (C) 2006-2012 Institute for Software Technology
  *                         University of Koblenz-Landau, Germany
  *                         ist@uni-koblenz.de
- * 
+ *
  * For bug reports, documentation and further information, visit
- * 
+ *
  *                         https://github.com/jgralab/jgralab
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see <http://www.gnu.org/licenses>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7
- * 
+ *
  * If you modify this Program, or any covered work, by linking or combining
  * it with Eclipse (or a modified version of that program or an Eclipse
  * plugin), containing parts covered by the terms of the Eclipse Public
@@ -45,6 +45,7 @@ import de.uni_koblenz.jgralab.EdgeDirection;
 import de.uni_koblenz.jgralab.Vertex;
 import de.uni_koblenz.jgralab.schema.EdgeClass;
 import de.uni_koblenz.jgralab.schema.IncidenceClass;
+import de.uni_koblenz.jgralab.schema.IncidenceDirection;
 import de.uni_koblenz.jgralab.schema.VertexClass;
 import de.uni_koblenz.jgralab.schema.exception.SchemaException;
 
@@ -206,7 +207,7 @@ public final class VertexClassImpl extends
 	 * For a vertexclass A are all edgeclasses valid froms, which (1) run from A
 	 * to a B or (2) run from a superclass of A to a B and whose end b at B is
 	 * not redefined by A or a superclass of A
-	 * 
+	 *
 	 */
 
 	@Override
@@ -441,7 +442,7 @@ public final class VertexClassImpl extends
 				EdgeClass ec = ic.getEdgeClass();
 				return new DirectedSchemaEdgeClass(
 						ec,
-						(getValidFromEdgeClasses().contains(ec) ? EdgeDirection.OUT
+						(ic.getDirection() == IncidenceDirection.IN ? EdgeDirection.OUT
 								: EdgeDirection.IN));
 			}
 		}
