c307371f65656c2c96e0b06025344d50018bc418
Daniel Janke <danijank@uni-koblenz.de>
2012-04-04 16:40:00 +0200

    Continued with tests.

--- testit/de/uni_koblenz/jgralabtest/greql2/evaluator/VariableEvaluatorTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/evaluator/VariableEvaluatorTest.java
@@ -15,6 +15,7 @@ import de.uni_koblenz.jgralab.greql2.evaluator.GreqlEnvironment;
 import de.uni_koblenz.jgralab.greql2.evaluator.GreqlEnvironmentAdapter;
 import de.uni_koblenz.jgralab.greql2.evaluator.GreqlEvaluatorImpl;
 import de.uni_koblenz.jgralab.greql2.evaluator.QueryImpl;
+import de.uni_koblenz.jgralab.greql2.exception.UndefinedVariableException;
 
 public class VariableEvaluatorTest {
 
@@ -41,6 +42,12 @@ public class VariableEvaluatorTest {
 				environment).getResult();
 	}
 
+	/**
+	 * Test if a variable defined via the query<br>
+	 * 1 store as varX<br>
+	 * can be used in the query<br>
+	 * using varX: varX
+	 */
 	@Test
 	public void testStoreAsAndUsing() {
 		GreqlEnvironment environment = new GreqlEnvironmentAdapter();
@@ -54,6 +61,35 @@ public class VariableEvaluatorTest {
 		erg = evaluateQuery("using varX: varX", environment);
 		assertNotNull(erg);
 		assertEquals(1, erg);
+	}
+
+	/**
+	 * Tests whether an {@link UndefinedVariableException} is thrown if an
+	 * undefined variable is used.
+	 */
+	@Test(expected = UndefinedVariableException.class)
+	public void testUndefinedVariable() {
+		evaluateQuery("using varX: varX");
+	}
+
+	/**
+	 * Tests the query:<br>
+	 * let x:=10, y:=12 in x+y
+	 */
+	@Test
+	public void testLet() throws GraphIOException {
+		Object erg = evaluateQuery("let x:=10, y:=12 in x+y");
+		assertEquals(22, erg);
+	}
+
+	/**
+	 * Tests the query:<br>
+	 * x+y where x:=10, y:=12
+	 */
+	@Test
+	public void testWhere() {
+		Object erg = evaluateQuery("x+y where x:=10, y:=12");
+		assertEquals(22, erg);
 	}
 
 }
