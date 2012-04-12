dd1d79ab57141fa3c04b0830961313cfaf18b5b2
Daniel Janke <danijank@uni-koblenz.de>
2012-04-12 15:34:12 +0200

    added tests

--- testit/de/uni_koblenz/jgralabtest/greql2/evaluator/PathExpressionTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/evaluator/PathExpressionTest.java
@@ -1,7 +1,10 @@
 package de.uni_koblenz.jgralabtest.greql2.evaluator;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -11,9 +14,11 @@ import de.uni_koblenz.jgralab.Graph;
 import de.uni_koblenz.jgralab.GraphIO;
 import de.uni_koblenz.jgralab.GraphIOException;
 import de.uni_koblenz.jgralab.ImplementationType;
+import de.uni_koblenz.jgralab.Vertex;
 import de.uni_koblenz.jgralab.greql2.evaluator.GreqlEnvironmentAdapter;
 import de.uni_koblenz.jgralab.greql2.evaluator.GreqlEvaluatorImpl;
 import de.uni_koblenz.jgralab.greql2.evaluator.QueryImpl;
+import de.uni_koblenz.jgralab.greql2.exception.GreqlException;
 import de.uni_koblenz.jgralab.greql2.exception.UnknownTypeException;
 
 public class PathExpressionTest {
@@ -156,4 +161,342 @@ public class PathExpressionTest {
 	// evaluateQuery("getVertex(19)--(set(getEdge(1),getEdge(325)))->getVertex(2)"));
 	// }
 
+	/*
+	 * Tests of SequentialPathDescription
+	 */
+
+	/**
+	 * Test of query:<br>
+	 * v155--&gt;&lt;--v14
+	 */
+	@Test
+	public void testSequentialPathDescription_sequenceLength2() {
+		assertTrue((Boolean) evaluateQuery("getVertex(155)--><--getVertex(14)"));
+	}
+
+	/**
+	 * Test of query:<br>
+	 * v155--&gt;v17&lt;--v14
+	 */
+	@Test
+	public void testSequentialPathDescription_withVertexInBetween() {
+		assertTrue((Boolean) evaluateQuery("getVertex(155)-->getVertex(17)<--getVertex(14)"));
+	}
+
+	/**
+	 * Test of query:<br>
+	 * v155&lt;-&gt;&lt;-&gt;v13
+	 */
+	@Test
+	public void testSequentialPathDescription_bidirectional() {
+		assertFalse((Boolean) evaluateQuery("getVertex(155)<-><->getVertex(13)"));
+	}
+
+	/**
+	 * Test of query:<br>
+	 * v155&lt;-&gt;&lt;-&gt;&lt;-&gt;v13
+	 */
+	@Test
+	public void testSequentialPathDescription_sequenceLength3() {
+		assertTrue((Boolean) evaluateQuery("getVertex(155)<-><-><->getVertex(13)"));
+	}
+
+	/**
+	 * Test of query:<br>
+	 * v155&lt;&lt;&gt;---&gt;v6
+	 */
+	@Test
+	public void testSequentialPathDescription_sequenceWithAggregation() {
+		assertTrue((Boolean) evaluateQuery("getVertex(155)<>---->getVertex(6)"));
+	}
+
+	/*
+	 * OptionalPathDescription
+	 */
+
+	/**
+	 * Test of query:<br>
+	 * v153[--&gt;]136
+	 */
+	@Test
+	public void testOptionalPathDescription_edgeNeeded() {
+		assertTrue((Boolean) evaluateQuery("getVertex(153)[-->]getVertex(136)"));
+	}
+
+	/**
+	 * Test of query:<br>
+	 * v153[--&gt;]153
+	 */
+	@Test
+	public void testOptionalPathDescription_edgeNotNeeded() {
+		assertTrue((Boolean) evaluateQuery("getVertex(153)[-->]getVertex(153)"));
+	}
+
+	/**
+	 * Test of query:<br>
+	 * v136[--&gt;]136
+	 */
+	@Test
+	public void testOptionalPathDescription_optionalLoop() {
+		assertTrue((Boolean) evaluateQuery("getVertex(136)[-->]getVertex(136)"));
+	}
+
+	/**
+	 * Test of query:<br>
+	 * v155[&lt;-&gt;]v13
+	 */
+	@Test
+	public void testOptionalPathDescription_notReachable() {
+		assertFalse((Boolean) evaluateQuery("getVertex(155)[<->]getVertex(13)"));
+	}
+
+	/*
+	 * IteratedPathDescription
+	 */
+
+	@Test
+	public void testIteratedPathDescription_Star_Reflexivity() {
+		assertTrue((Boolean) evaluateQuery("getVertex(153)-->*getVertex(153)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Plus_ReflexivityWithoutLoop() {
+		assertFalse((Boolean) evaluateQuery("getVertex(153)-->+getVertex(153)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Star_withLoop() {
+		assertTrue((Boolean) evaluateQuery("getVertex(136)-->*getVertex(136)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Plus_withLoop() {
+		assertTrue((Boolean) evaluateQuery("getVertex(136)-->+getVertex(136)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Plus_ReachableWithLoop() {
+		assertTrue((Boolean) evaluateQuery("getVertex(136)<->+getVertex(153)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Star() {
+		assertTrue((Boolean) evaluateQuery("getVertex(14)-->*getVertex(16)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Plus() {
+		assertTrue((Boolean) evaluateQuery("getVertex(14)-->+getVertex(16)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Star_FailBecauseOfDirection() {
+		assertFalse((Boolean) evaluateQuery("getVertex(14)<--*getVertex(16)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Plus_FailBecauseOfDirection() {
+		assertFalse((Boolean) evaluateQuery("getVertex(14)<--+getVertex(16)"));
+	}
+
+	@Test(expected = GreqlException.class)
+	public void testIteratedPathDescription_Exponent_Reflexivity() {
+		evaluateQuery("getVertex(21)-->^0 getVertex(21)");
+	}
+
+	@Test
+	public void testIteratedPathDescription_Exponent_TooShort() {
+		assertFalse((Boolean) evaluateQuery("getVertex(21)-->^1 getVertex(13)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Exponent() {
+		assertTrue((Boolean) evaluateQuery("getVertex(21)-->^2 getVertex(13)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Exponent_TooLong() {
+		assertFalse((Boolean) evaluateQuery("getVertex(21)-->^3 getVertex(13)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Exponent_WrongDirection() {
+		assertFalse((Boolean) evaluateQuery("getVertex(21)<--^2 getVertex(13)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Exponent_TwoPossibleWays_TooShort() {
+		assertFalse((Boolean) evaluateQuery("getVertex(144)-->^1 getVertex(16)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Exponent_TwoPossibleWays_ShortMatch() {
+		assertTrue((Boolean) evaluateQuery("getVertex(144)-->^2 getVertex(16)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Exponent_TwoPossibleWays_BetweenBoth() {
+		assertFalse((Boolean) evaluateQuery("getVertex(144)-->^3 getVertex(16)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Exponent_TwoPossibleWays_LongMatch() {
+		assertTrue((Boolean) evaluateQuery("getVertex(144)-->^4 getVertex(16)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Exponent_TwoPossibleWays_TooLong() {
+		assertFalse((Boolean) evaluateQuery("getVertex(144)-->^5 getVertex(16)"));
+	}
+
+	@Test(expected = GreqlException.class)
+	public void testIteratedPathDescription_Exponent_withLoop0() {
+		evaluateQuery("getVertex(136)-->^0 getVertex(136)");
+	}
+
+	@Test
+	public void testIteratedPathDescription_Exponent_withLoop1() {
+		assertTrue((Boolean) evaluateQuery("getVertex(136)-->^1 getVertex(136)"));
+	}
+
+	@Test
+	public void testIteratedPathDescription_Exponent_withLoop2() {
+		assertTrue((Boolean) evaluateQuery("getVertex(136)-->^2 getVertex(136)"));
+	}
+
+	/*
+	 * AlternativePathDescription
+	 */
+
+	@Test
+	public void testAlternativePathDescription_BothFail() {
+		assertFalse((Boolean) evaluateQuery("getVertex(144)-->^5 |-->^3 getVertex(16)"));
+	}
+
+	@Test
+	public void testAlternativePathDescription_FirstSucceedsSecondFails() {
+		assertTrue((Boolean) evaluateQuery("getVertex(144)-->^4 |-->^3 getVertex(16)"));
+	}
+
+	@Test
+	public void testAlternativePathDescription_FirstFailsSecondSucceeds() {
+		assertTrue((Boolean) evaluateQuery("getVertex(144)-->^5 |-->^2 getVertex(16)"));
+	}
+
+	@Test
+	public void testAlternativePathDescription_BothSucceed() {
+		assertTrue((Boolean) evaluateQuery("getVertex(144)-->^4 |-->^2 getVertex(16)"));
+	}
+
+	@Test
+	public void testAlternativePathDescription_OfLength3() {
+		assertTrue((Boolean) evaluateQuery("getVertex(144)-->^4 |-->^2 |<>-- getVertex(16)"));
+	}
+
+	/*
+	 * GroupPathDescription
+	 */
+
+	@Test
+	public void testGroupPathDescription_OneElement() {
+		assertTrue((Boolean) evaluateQuery("getVertex(144)(-->^2) getVertex(16)"));
+	}
+
+	@Test
+	public void testGroupPathDescription_SeveralElements() {
+		assertTrue((Boolean) evaluateQuery("getVertex(144)(-->-->) getVertex(16)"));
+	}
+
+	@Test
+	public void testGroupPathDescription_SeveralElements_OnlyOneInGroup() {
+		assertTrue((Boolean) evaluateQuery("getVertex(144)-->(-->) getVertex(16)"));
+	}
+
+	@Test
+	public void testGroupPathDescription_SeveralElements_TwoGroups() {
+		assertTrue((Boolean) evaluateQuery("getVertex(144)(-->)(-->) getVertex(16)"));
+	}
+
+	@Test
+	public void testGroupPathDescription_SeveralBrackets() {
+		assertTrue((Boolean) evaluateQuery("getVertex(144)(((-->^2))) getVertex(16)"));
+	}
+
+	/*
+	 * VertexRestriction
+	 */
+
+	@Test
+	public void testVertexRestriction_StartVertex() {
+		@SuppressWarnings("unchecked")
+		Set<Vertex> ergSet = (Set<Vertex>) evaluateQuery("import localities.*;"
+				+ "{County}&--> getVertex(154)");
+		assertFalse(ergSet.isEmpty());
+		assertEquals(1, ergSet.size());
+		for (Vertex v : ergSet) {
+			assertEquals(144, v.getId());
+		}
+	}
+
+	@Test
+	public void testVertexRestriction_StartVertex_SeveralVertexClasses() {
+		@SuppressWarnings("unchecked")
+		Set<Vertex> ergSet = (Set<Vertex>) evaluateQuery("import localities.*;"
+				+ "{County,City}&--> getVertex(154)");
+		assertFalse(ergSet.isEmpty());
+		assertEquals(1, ergSet.size());
+		for (Vertex v : ergSet) {
+			assertEquals(144, v.getId());
+		}
+	}
+
+	@Test
+	public void testVertexRestriction_StartVertex_Predicate() {
+		@SuppressWarnings("unchecked")
+		Set<Vertex> ergSet = (Set<Vertex>) evaluateQuery("import localities.*;"
+				+ "{County @ thisVertex.name=\"Hessen\"}&--> getVertex(154)");
+		assertFalse(ergSet.isEmpty());
+		assertEquals(1, ergSet.size());
+		for (Vertex v : ergSet) {
+			assertEquals(144, v.getId());
+			assertEquals("Hessen", v.getAttribute("name"));
+		}
+	}
+
+	@Test
+	public void testVertexRestriction_EndVertex() {
+		@SuppressWarnings("unchecked")
+		Set<Vertex> ergSet = (Set<Vertex>) evaluateQuery("import localities.*;"
+				+ "getVertex(144)--> &{City}");
+		assertFalse(ergSet.isEmpty());
+		assertEquals(1, ergSet.size());
+		for (Vertex v : ergSet) {
+			assertEquals(154, v.getId());
+		}
+	}
+
+	@Test
+	public void testVertexRestriction_EndVertex_SeveralVertexClasses() {
+		@SuppressWarnings("unchecked")
+		Set<Vertex> ergSet = (Set<Vertex>) evaluateQuery("import localities.*;"
+				+ "getVertex(144)--> &{City,County}");
+		assertFalse(ergSet.isEmpty());
+		assertEquals(1, ergSet.size());
+		for (Vertex v : ergSet) {
+			assertEquals(154, v.getId());
+		}
+	}
+
+	@Test
+	public void testVertexRestriction_EndVertex_Predicate() {
+		@SuppressWarnings("unchecked")
+		Set<Vertex> ergSet = (Set<Vertex>) evaluateQuery("import localities.*;"
+				+ "getVertex(144)--> &{City @ thisVertex.name=\"Frankfurt am Main\"}");
+		assertFalse(ergSet.isEmpty());
+		assertEquals(1, ergSet.size());
+		for (Vertex v : ergSet) {
+			assertEquals(154, v.getId());
+			assertEquals("Frankfurt am Main", v.getAttribute("name"));
+		}
+	}
 }
