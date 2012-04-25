b00728023e391c310a4e6e3f8919d510249e329c
Daniel Janke <danijank@uni-koblenz.de>
2012-04-25 14:27:47 +0200

    Added tests for EdgeSetExpression

--- testit/de/uni_koblenz/jgralabtest/greql2/evaluator/ResidualEvaluatorTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql2/evaluator/ResidualEvaluatorTest.java
@@ -10,7 +10,9 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import de.uni_koblenz.jgralab.Edge;
 import de.uni_koblenz.jgralab.Graph;
+import de.uni_koblenz.jgralab.GraphElement;
 import de.uni_koblenz.jgralab.GraphIO;
 import de.uni_koblenz.jgralab.GraphIOException;
 import de.uni_koblenz.jgralab.ImplementationType;
@@ -18,7 +20,7 @@ import de.uni_koblenz.jgralab.Vertex;
 import de.uni_koblenz.jgralab.greql2.evaluator.GreqlEnvironmentAdapter;
 import de.uni_koblenz.jgralab.greql2.evaluator.GreqlEvaluatorImpl;
 import de.uni_koblenz.jgralab.greql2.evaluator.QueryImpl;
-import de.uni_koblenz.jgralab.schema.VertexClass;
+import de.uni_koblenz.jgralab.schema.GraphElementClass;
 
 public class ResidualEvaluatorTest {
 
@@ -41,13 +43,16 @@ public class ResidualEvaluatorTest {
 				new GreqlEnvironmentAdapter()).getResult();
 	}
 
-	private boolean isInstanceOf(Vertex v, String type) {
-		return v.isInstanceOf((VertexClass) datagraph.getSchema()
+	@SuppressWarnings("unchecked")
+	private <SC extends GraphElementClass<SC, IC>, IC extends GraphElement<SC, IC>> boolean isInstanceOf(
+			GraphElement<SC, IC> v, String type) {
+		return v.isInstanceOf((SC) datagraph.getSchema()
 				.getAttributedElementClass(type));
 	}
 
-	private String createContainmentMessage(Vertex v, boolean showWordNot) {
-		return v.getId() + " of type "
+	private String createContainmentMessage(GraphElement<?, ?> v,
+			boolean showWordNot) {
+		return (v instanceof Vertex ? "v" : "e") + v.getId() + " of type "
 				+ v.getAttributedElementClass().getQualifiedName() + " is "
 				+ (showWordNot ? "not" : "") + " contained.";
 	}
@@ -192,6 +197,148 @@ public class ResidualEvaluatorTest {
 	// } else {
 	// assertFalse(createContainmentMessage(v, false),
 	// ergSet.contains(v));
+	// }
+	// }
+	// }
+
+	/*
+	 * EdgeSetExpression
+	 */
+
+	@Test
+	public void testEdgeSetExpression_allEdges() {
+		@SuppressWarnings("unchecked")
+		Set<Edge> ergSet = (Set<Edge>) evaluateQuery("E");
+		assertEquals(datagraph.getECount(), ergSet.size());
+		for (Edge e : datagraph.edges()) {
+			assertTrue(createContainmentMessage(e, true), ergSet.contains(e));
+		}
+	}
+
+	@Test
+	public void testEdgeSetExpression_edgesOfOneType() {
+		@SuppressWarnings("unchecked")
+		Set<Edge> ergSet = (Set<Edge>) evaluateQuery("import connections.*;E{Street}");
+		for (Edge e : datagraph.edges()) {
+			if (isInstanceOf(e, "connections.Street")) {
+				assertTrue(createContainmentMessage(e, true),
+						ergSet.contains(e));
+			} else {
+				assertFalse(createContainmentMessage(e, false),
+						ergSet.contains(e));
+			}
+		}
+	}
+
+	@Test
+	public void testEdgeSetExpression_edgesOfExactlyOneType() {
+		@SuppressWarnings("unchecked")
+		Set<Edge> ergSet = (Set<Edge>) evaluateQuery("import connections.*;E{Street!}");
+		for (Edge e : datagraph.edges()) {
+			if (isInstanceOf(e, "connections.Street")
+					&& !isInstanceOf(e, "connections.Highway")) {
+				assertTrue(createContainmentMessage(e, true),
+						ergSet.contains(e));
+			} else {
+				assertFalse(createContainmentMessage(e, false),
+						ergSet.contains(e));
+			}
+		}
+	}
+
+	@Test
+	public void testEdgeSetExpression_edgesOfNotOneType() {
+		@SuppressWarnings("unchecked")
+		Set<Edge> ergSet = (Set<Edge>) evaluateQuery("import connections.*;E{^Street}");
+		for (Edge e : datagraph.edges()) {
+			if (!isInstanceOf(e, "connections.Street")) {
+				assertTrue(createContainmentMessage(e, true),
+						ergSet.contains(e));
+			} else {
+				assertFalse(createContainmentMessage(e, false),
+						ergSet.contains(e));
+			}
+		}
+	}
+
+	@Test
+	public void testEdgeSetExpression_edgesOfNotExactlyOneType() {
+		@SuppressWarnings("unchecked")
+		Set<Edge> ergSet = (Set<Edge>) evaluateQuery("import connections.*;E{^Street!}");
+		for (Edge e : datagraph.edges()) {
+			if (!(isInstanceOf(e, "connections.Street") && !isInstanceOf(e,
+					"connections.Highway"))) {
+				assertTrue(createContainmentMessage(e, true),
+						ergSet.contains(e));
+			} else {
+				assertFalse(createContainmentMessage(e, false),
+						ergSet.contains(e));
+			}
+		}
+	}
+
+	@Test
+	public void testEdgeSetExpression_edgesOfOneTypeButNotASubtype() {
+		@SuppressWarnings("unchecked")
+		Set<Edge> ergSet = (Set<Edge>) evaluateQuery("import connections.*;E{Street,^Highway}");
+		for (Edge e : datagraph.edges()) {
+			if (isInstanceOf(e, "connections.Street")
+					&& !isInstanceOf(e, "connections.Highway")) {
+				assertTrue(createContainmentMessage(e, true),
+						ergSet.contains(e));
+			} else {
+				assertFalse(createContainmentMessage(e, false),
+						ergSet.contains(e));
+			}
+		}
+	}
+
+	@Test
+	public void testEdgeSetExpression_edgesOfNotASubtypeButOneType() {
+		@SuppressWarnings("unchecked")
+		Set<Edge> ergSet = (Set<Edge>) evaluateQuery("import connections.*;E{^Highway,Street}");
+		for (Edge e : datagraph.edges()) {
+			if (isInstanceOf(e, "connections.Street")
+					&& !isInstanceOf(e, "connections.Highway")) {
+				assertTrue(createContainmentMessage(e, true),
+						ergSet.contains(e));
+			} else {
+				assertFalse(createContainmentMessage(e, false),
+						ergSet.contains(e));
+			}
+		}
+	}
+
+	// @Test
+	// public void testEdgeSetExpression_edgesOfNotOneTypeButASubtype() {
+	// @SuppressWarnings("unchecked")
+	// Set<Edge> ergSet = (Set<Edge>)
+	// evaluateQuery("import connections.*;E{^Street,Highway}");
+	// for (Edge e : datagraph.edges()) {
+	// if (!isInstanceOf(e, "connections.Street")
+	// || isInstanceOf(e, "connections.Highway")) {
+	// assertTrue(createContainmentMessage(e, true),
+	// ergSet.contains(e));
+	// } else {
+	// assertFalse(createContainmentMessage(e, false),
+	// ergSet.contains(e));
+	// }
+	// }
+	// }
+	//
+	// @Test
+	// public void testEdgeSetExpression_edgesOfASubtypeNotButOneType() {
+	// @SuppressWarnings("unchecked")
+	// Set<Edge> ergSet = (Set<Edge>)
+	// evaluateQuery("import connections.*;E{Highway,^Street}");
+	// for (Edge e : datagraph.edges()) {
+	// if (!isInstanceOf(e, "connections.Street")
+	// || isInstanceOf(e, "connections.Highway")) {
+	// assertTrue(createContainmentMessage(e, true),
+	// ergSet.contains(e));
+	// } else {
+	// assertFalse(createContainmentMessage(e, false),
+	// ergSet.contains(e));
 	// }
 	// }
 	// }
