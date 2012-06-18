900a5617238ced5e8c5a020d7a6f32c67edc3cb3
Sascha Strauß <strauss@uni-koblenz.de>
2012-06-18 11:28:34 +0200

    added forgotten test classes to RunAlgorithmTests.

--- testit/de/uni_koblenz/jgralabtest/algolib/algorithms/RunAlgorithmTests.java
+++ testit/de/uni_koblenz/jgralabtest/algolib/algorithms/RunAlgorithmTests.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ BreadthFirstSearchTest.class, DepthFirstSearchTest.class,
 		ShortestPathsWithBFSTest.class, TopologicalOrderTest.class,
+		WarshallTest.class, ReachableWithSearchTest.class,
 		StrongComponentsTest.class, WeakComponentsTest.class, IsTreeTest.class,
 		FloydTest.class, FordMooreTest.class, DijkstraAndAStarTest.class })
 public class RunAlgorithmTests {
