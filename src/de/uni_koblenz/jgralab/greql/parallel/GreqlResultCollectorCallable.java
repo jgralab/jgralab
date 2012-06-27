dc1baeb5b9d609fea37f7f25a13b9e00966fa2c9
Daniel Janke <danijank@uni-koblenz.de>
2012-06-27 16:13:50 +0200

    created further classes

---
+++ src/de/uni_koblenz/jgralab/greql/parallel/GreqlResultCollectorCallable.java
@@ -0,0 +1,13 @@
+package de.uni_koblenz.jgralab.greql.parallel;
+
+import java.util.concurrent.Callable;
+
+public class GreqlResultCollectorCallable implements Callable<Object> {
+
+	@Override
+	public Object call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/de/uni_koblenz/jgralab/greql/parallel/ParallelGreqlEvaluator.java
@@ -0,0 +1,5 @@
+package de.uni_koblenz.jgralab.greql.parallel;
+
+public class ParallelGreqlEvaluator {
+
+}
