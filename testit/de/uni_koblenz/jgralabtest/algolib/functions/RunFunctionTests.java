8980f6a0d1ca67025b41257d0c536a788c899d34
Sascha Strauß <strauss@uni-koblenz.de>
2012-06-27 11:40:17 +0200

    Added testcase for binary functions.
    The array implementations for binary functions are now covered by the testcase ArrayBinaryFunctionsTest. This testcase has also been integrated into the RunFunctionsTests suite.

--- testit/de/uni_koblenz/jgralabtest/algolib/functions/RunFunctionTests.java
+++ testit/de/uni_koblenz/jgralabtest/algolib/functions/RunFunctionTests.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ ArrayPermutationTest.class })
+@Suite.SuiteClasses({ ArrayPermutationTest.class,
+		ArrayBinaryFunctionsTest.class })
 public class RunFunctionTests {
 
 }
