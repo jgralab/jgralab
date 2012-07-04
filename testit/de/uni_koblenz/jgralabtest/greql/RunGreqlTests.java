5d948a5e3cbd13d191113b294e0221803a2260f8
Sascha Strauß <strauss@uni-koblenz.de>
2012-07-04 17:06:33 +0200

    Added several new test suite classes.
    Also added new RunGreqlTest suite to RunTests suite, so the newly created tests will appear in the coverage report.

---
+++ testit/de/uni_koblenz/jgralabtest/greql/RunGreqlTests.java
@@ -0,0 +1,12 @@
+package de.uni_koblenz.jgralabtest.greql;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import de.uni_koblenz.jgralabtest.greql.funlib.RunFunlibTests;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RunFunlibTests.class })
+public class RunGreqlTests {
+
+}
---
+++ testit/de/uni_koblenz/jgralabtest/greql/funlib/RunFunlibTests.java
@@ -0,0 +1,12 @@
+package de.uni_koblenz.jgralabtest.greql.funlib;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import de.uni_koblenz.jgralabtest.greql.funlib.arithmetics.RunArithmeticTests;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RunArithmeticTests.class })
+public class RunFunlibTests {
+
+}
