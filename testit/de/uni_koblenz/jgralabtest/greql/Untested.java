063951702eb0c7cd86fa3826d39293fde1b155e0
Sascha Strauß <strauss@uni-koblenz.de>
2012-07-09 15:38:34 +0200

    Integrated existing GReQL test cases into test suite.
    Removed old GReQL test suite and moved all contained test cases/suites to new test suite.
    Created a test suite containing all test cases that are not contained in the GReQL test suite.
    Renamed "VertexEvaluatorTest" to "RunVertexEvaluatorTests" for matching the naming schema of all other test suites.
    Moved the ParserTest into its own package for analogy reasons.

---
+++ testit/de/uni_koblenz/jgralabtest/greql/Untested.java
@@ -0,0 +1,13 @@
+package de.uni_koblenz.jgralabtest.greql;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ AdditionalTest.class, ExecutableGreqlTest.class,
+		GreqlConstructions.class, RecordTest.class,
+		ReflectionFunctionTest.class, StoreJValueTest.class,
+		StoreValuesTest.class, SubgraphRestrictionTest.class })
+public class Untested {
+
+}
