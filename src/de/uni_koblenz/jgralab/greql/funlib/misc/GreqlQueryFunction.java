1adb1f85fde95efd24728a650b8fd59cf4ba517b
Daniel Janke <danijank@uni-koblenz.de>
2012-07-18 17:24:57 +0200

    Adapted to use getUsedVariables instead of extraction from graph

--- src/de/uni_koblenz/jgralab/greql/funlib/misc/GreqlQueryFunction.java
+++ src/de/uni_koblenz/jgralab/greql/funlib/misc/GreqlQueryFunction.java
@@ -1,14 +1,12 @@
 package de.uni_koblenz.jgralab.greql.funlib.misc;
 
-import de.uni_koblenz.jgralab.EdgeDirection;
+import java.util.Set;
+
 import de.uni_koblenz.jgralab.greql.GreqlEnvironment;
 import de.uni_koblenz.jgralab.greql.GreqlQuery;
 import de.uni_koblenz.jgralab.greql.evaluator.GreqlEnvironmentAdapter;
 import de.uni_koblenz.jgralab.greql.exception.GreqlException;
 import de.uni_koblenz.jgralab.greql.funlib.Function;
-import de.uni_koblenz.jgralab.greql.schema.Greql2Expression;
-import de.uni_koblenz.jgralab.greql.schema.IsBoundVarOf;
-import de.uni_koblenz.jgralab.greql.schema.Variable;
 
 public class GreqlQueryFunction extends Function {
 
@@ -29,14 +27,11 @@ public class GreqlQueryFunction extends Function {
 
 	protected void initialize(GreqlQuery query) {
 		this.query = query;
-		Greql2Expression greql2Expression = query.getQueryGraph()
-				.getFirstGreql2Expression();
-		parameterNames = new String[greql2Expression.getDegree(IsBoundVarOf.EC,
-				EdgeDirection.IN)];
+		Set<String> usedVariables = query.getUsedVariables();
+		parameterNames = new String[usedVariables.size()];
 		int i = 0;
-		for (IsBoundVarOf ibvo : greql2Expression
-				.getIsBoundVarOfIncidences(EdgeDirection.IN)) {
-			parameterNames[i++] = ((Variable) ibvo.getThat()).get_name();
+		for (String varName : usedVariables) {
+			parameterNames[i++] = varName;
 		}
 	}
 
