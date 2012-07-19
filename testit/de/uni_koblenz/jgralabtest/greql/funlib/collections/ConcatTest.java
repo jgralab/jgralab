91bd12432b66e710a14633b055aef65757c05fec
Sascha Strauß <strauss@uni-koblenz.de>
2012-07-19 14:58:23 +0200

    Prepared test case for collection concatenation.

---
+++ testit/de/uni_koblenz/jgralabtest/greql/funlib/collections/ConcatTest.java
@@ -0,0 +1,11 @@
+package de.uni_koblenz.jgralabtest.greql.funlib.collections;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ConcatTest {
+	@Test
+	public void testCollection() {
+		fail("Not implemented yet.");
+	}
+}
