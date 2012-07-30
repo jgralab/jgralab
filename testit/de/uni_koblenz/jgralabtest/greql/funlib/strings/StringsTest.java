27e3ca57b9efbb4ad850b51d12301700d6c3122b
Sascha Strauß <strauss@uni-koblenz.de>
2012-07-30 13:30:36 +0200

    removed non-greql Objects from test arrays

--- testit/de/uni_koblenz/jgralabtest/greql/funlib/strings/StringsTest.java
+++ testit/de/uni_koblenz/jgralabtest/greql/funlib/strings/StringsTest.java
@@ -1,10 +1,8 @@
 package de.uni_koblenz.jgralabtest.greql.funlib.strings;
 
-import java.io.File;
-
 public abstract class StringsTest {
 	protected String[] stringValues = new String[] { "Hugo", "foo", "bar", "",
 			"Hello World", "b", "H", "o", "oo", "ar", "r", "Hello", "World" };
 	protected Object[] objectValues = new Object[] { new Integer(2),
-			new Double(2.5), new File("."), System.class, this };
+			new Double(2.5), "TestString" };
 }
