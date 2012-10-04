4b45085bb45bc480275f719bebc61da9a5912cd1
Daniel Janke <danijank@uni-koblenz.de>
2012-10-04 13:13:53 +0200

    deleted old executable test

--- testit/de/uni_koblenz/jgralabtest/greql/Untested.java
+++ testit/de/uni_koblenz/jgralabtest/greql/Untested.java
@@ -4,9 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ AdditionalTest.class, ExecutableGreqlTest.class,
-		GreqlConstructions.class, RecordTest.class, StoreJValueTest.class,
-		StoreValuesTest.class, SubgraphRestrictionTest.class })
+@Suite.SuiteClasses({ AdditionalTest.class, GreqlConstructions.class,
+		RecordTest.class, StoreJValueTest.class, StoreValuesTest.class,
+		SubgraphRestrictionTest.class })
 public class Untested {
 
 }
