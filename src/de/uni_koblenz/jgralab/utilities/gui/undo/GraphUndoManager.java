6eefa70b07a8f57681f17e3b880343f9d6ac6d2e
Volker Riediger <riediger@uni-koblenz.de>
2013-02-27 15:11:19 +0100

    Incidence order preserved by delete vertex/edge edits.

--- src/de/uni_koblenz/jgralab/utilities/gui/undo/GraphUndoManager.java
+++ src/de/uni_koblenz/jgralab/utilities/gui/undo/GraphUndoManager.java
@@ -1,7 +1,7 @@
 package de.uni_koblenz.jgralab.utilities.gui.undo;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 import javax.swing.undo.AbstractUndoableEdit;
@@ -15,49 +15,114 @@ import de.uni_koblenz.jgralab.Edge;
 import de.uni_koblenz.jgralab.Graph;
 import de.uni_koblenz.jgralab.GraphChangeListener;
 import de.uni_koblenz.jgralab.GraphElement;
+import de.uni_koblenz.jgralab.TraversalContext;
 import de.uni_koblenz.jgralab.Vertex;
 import de.uni_koblenz.jgralab.schema.Attribute;
 import de.uni_koblenz.jgralab.schema.AttributedElementClass;
 import de.uni_koblenz.jgralab.schema.EdgeClass;
 import de.uni_koblenz.jgralab.schema.VertexClass;
 
+/**
+ * @author riediger
+ * 
+ */
 public class GraphUndoManager extends UndoManager implements
 		GraphChangeListener {
 	private static final long serialVersionUID = -1959515066823695788L;
-	private boolean working;
-	private Graph graph;
-	private Stack<GraphEdit> deleteVertexEditStack;
-	protected Map<Integer, AttributedElement<?, ?>> referencedElements;
-	protected Map<AttributedElement<?, ?>, Integer> elementRefs;
 
+	/**
+	 * The {@link Graph} for which this {@link GraphUndoManager} is registered.
+	 */
+	private Graph graph;
+
+	/**
+	 * true when this {@link GraphUndoManager} is doing an undo or redo
+	 * (prevents recording of new edits when undo/redo change the graph)
+	 */
+	private boolean working;
+
+	/**
+	 * Version number keeps track of versions of {@link Vertex} and {@link Edge}
+	 * objects. Versioning is needed since the IDs of resurrected elements (upon
+	 * redo) can change, and an element with the same ID can be deleted/created
+	 * many times in complex {@link GraphEdit} sequences. Each time an element
+	 * is referenced for the first time, or when it's recreated, the version
+	 * counter is increased.
+	 */
+	private int version;
+
+	/**
+	 * Keeps track of most recent version of elements. Key is negative for
+	 * edges, positive for vertices.
+	 */
+	private HashMap<Integer, Integer> versions = new HashMap<Integer, Integer>();
+
+	/**
+	 * first and last {@link GraphEdit} in a doubly linked list of all live
+	 * edits. This list is needed to update element references when the versions
+	 * of resurrected elements change.
+	 */
 	private GraphEdit first;
 	private GraphEdit last;
-	private int version;
-	private Stack<CompoundGraphEdit> compoundEditStack = new Stack<CompoundGraphEdit>();
-	private HashMap<AttributedElement<?, ?>, Integer> versions = new HashMap<AttributedElement<?, ?>, Integer>();
 
-	protected enum Event {
+	/**
+	 * For convenience, {@link #beginEdit(String)} and {@link #endEdit()}
+	 * methods can be used to group multiple graph changes into single
+	 * {@link CompoundGraphEdit}s. This stack keeps track of the active
+	 * {@link CompoundGraphEdit}s.
+	 */
+	private Stack<CompoundGraphEdit> compoundEditStack = new Stack<CompoundGraphEdit>();
+
+	/**
+	 * Data needed to delete/resurrect a {@link Vertex} and all incident
+	 * {@link Edge}s.
+	 */
+	private CompoundEdit deleteVertexCompound;
+	private DeleteVertexEdit deleteVertexEdit;
+	private boolean undoDeleteVertex;
+	private ArrayList<Edge> correctIncidences = new ArrayList<Edge>();
+	private ArrayList<Integer> correctPositions = new ArrayList<Integer>();
+
+	/**
+	 * GraphEdit events.
+	 */
+	protected enum GraphEditEvent {
 		CREATE_VERTEX, CREATE_EDGE, DELETE_VERTEX, DELETE_EDGE, CHANGE_OMEGA, CHANGE_ALPHA, CHANGE_ATTRIBUTE
 	}
 
+	/**
+	 * Base class of all {@link GraphEdit} operations.
+	 */
 	protected abstract class GraphEdit extends AbstractUndoableEdit {
 		private static final long serialVersionUID = 1100368833451887175L;
+
+		/**
+		 * Previous/next edit in the doubly linked list of all live
+		 * {@link GraphEdit}s.
+		 */
 		GraphEdit prev;
 		GraphEdit next;
 
+		/**
+		 * Element reference and version of this {@link GraphEdit}. elementId is
+		 * positive for vertices, negative for edges and 0 for the graph itself.
+		 */
 		int elementId;
 		int elementVersion;
 
+		/**
+		 * Element class and event type.
+		 */
 		AttributedElementClass<?, ?> aec;
-		Event event;
+		GraphEditEvent event;
 
-		GraphEdit(Event event, AttributedElement<?, ?> el) {
+		GraphEdit(GraphEditEvent event, AttributedElement<?, ?> el) {
 			this.event = event;
 			aec = el.getAttributedElementClass();
 			elementId = (el instanceof Vertex) ? ((Vertex) el).getId()
 					: (el instanceof Edge) ? -((Edge) el).getId() : 0;
 
-			elementVersion = ev(el);
+			elementVersion = elementVersion(elementId);
 			if (first == null) {
 				first = last = this;
 			} else {
@@ -67,10 +132,19 @@ public class GraphUndoManager extends UndoManager implements
 			}
 		}
 
-		int ev(AttributedElement<?, ?> el) {
-			Integer v = versions.get(el);
+		/**
+		 * Returns the most recent version for the specified
+		 * <code>elementId</code> or creates a new version when the element was
+		 * never referenced.
+		 * 
+		 * @param elementId
+		 *            an element id
+		 * @return version number for <code>elementId</code>
+		 */
+		int elementVersion(Integer elementId) {
+			Integer v = versions.get(elementId);
 			if (v == null) {
-				versions.put(el, ++version);
+				versions.put(elementId, ++version);
 				return version;
 			}
 			return v;
@@ -89,6 +163,7 @@ public class GraphUndoManager extends UndoManager implements
 		@Override
 		public void die() {
 			super.die();
+			// remove this edit from the live edit list
 			if (first == this && first == last) {
 				first = last = null;
 			} else {
@@ -105,8 +180,10 @@ public class GraphUndoManager extends UndoManager implements
 			}
 		}
 
-		public void changeVertexId(int from, int fromVersion, int to,
-				int toVersion) {
+		/**
+		 * Changes vertex references of this {@link GraphEdit} to new version.
+		 */
+		void changeVertexId(int from, int fromVersion, int to, int toVersion) {
 			if (elementId > 0 && elementId == from
 					&& elementVersion == fromVersion) {
 				elementId = to;
@@ -114,32 +191,40 @@ public class GraphUndoManager extends UndoManager implements
 			}
 		}
 
-		public void changeEdgeId(int from, int fromVersion, int to,
-				int toVersion) {
+		/**
+		 * Changes edge references of this {@link GraphEdit} to new version.
+		 */
+		void changeEdgeId(int from, int fromVersion, int to, int toVersion) {
 			if (elementId < 0 && elementId == from
 					&& elementVersion == fromVersion) {
 				elementId = to;
 				elementVersion = fromVersion;
 			}
 		}
-		// @Override
-		// public void undo() throws CannotUndoException {
-		// System.out.println("UNDO " + this);
-		// super.undo();
-		// }
-		//
-		// @Override
-		// public void redo() throws CannotRedoException {
-		// System.out.println("REDO " + this);
-		// super.redo();
-		// }
+
+		// remove comments below to print undo/redo operations to System.out
+
+		@Override
+		public void undo() throws CannotUndoException {
+			System.out.println("UNDO " + this);
+			super.undo();
+		}
+
+		@Override
+		public void redo() throws CannotRedoException {
+			System.out.println("REDO " + this);
+			super.redo();
+		}
 	}
 
+	/**
+	 * Records creation of a {@link Vertex}.
+	 */
 	protected class CreateVertexEdit extends GraphEdit {
 		private static final long serialVersionUID = 7455868109487075050L;
 
 		CreateVertexEdit(Vertex v) {
-			super(Event.CREATE_VERTEX, v);
+			super(GraphEditEvent.CREATE_VERTEX, v);
 		}
 
 		@Override
@@ -156,7 +241,7 @@ public class GraphUndoManager extends UndoManager implements
 			int oldVersion = elementVersion;
 			elementId = v.getId();
 			elementVersion = ++version;
-			versions.put(v, elementVersion);
+			versions.put(elementId, elementVersion);
 			// System.out.println("\tWAS v" + oldId + "-" + oldVersion +
 			// ", is v"
 			// + elementId + "-" + elementVersion);
@@ -166,16 +251,20 @@ public class GraphUndoManager extends UndoManager implements
 		}
 	}
 
+	/**
+	 * Records creation of an {@link Edge}.
+	 */
 	protected class CreateEdgeEdit extends GraphEdit {
 		private static final long serialVersionUID = 8840782477572584494L;
 		private int alphaId, omegaId, alphaVersion, omegaVersion;
 
 		CreateEdgeEdit(Edge e) {
-			super(Event.CREATE_EDGE, e);
+			super(GraphEditEvent.CREATE_EDGE, e);
+			assert e.isNormal();
 			alphaId = e.getAlpha().getId();
-			alphaVersion = ev(e.getAlpha());
+			alphaVersion = elementVersion(alphaId);
 			omegaId = e.getOmega().getId();
-			omegaVersion = ev(e.getOmega());
+			omegaVersion = elementVersion(omegaId);
 		}
 
 		@Override
@@ -194,7 +283,7 @@ public class GraphUndoManager extends UndoManager implements
 			int oldVersion = elementVersion;
 			elementId = -e.getId();
 			elementVersion = ++version;
-			versions.put(e, elementVersion);
+			versions.put(elementId, elementVersion);
 			// System.out.println("\tWAS e" + (-oldId) + "-" + oldVersion
 			// + ", is e" + (-elementId) + "-" + elementVersion);
 			for (GraphEdit g = first; g != null; g = g.next) {
@@ -222,11 +311,14 @@ public class GraphUndoManager extends UndoManager implements
 		}
 	}
 
+	/**
+	 * Base class of Delete edits, records attribute values before deletion.
+	 */
 	protected abstract class DeleteElementEdit extends GraphEdit {
 		private static final long serialVersionUID = -6211867897158716199L;
 		private Object[] attrValues;
 
-		public DeleteElementEdit(Event event, GraphElement<?, ?> el) {
+		public DeleteElementEdit(GraphEditEvent event, GraphElement<?, ?> el) {
 			super(event, el);
 			attrValues = new Object[aec.getAttributeCount()];
 			int i = 0;
@@ -245,11 +337,16 @@ public class GraphUndoManager extends UndoManager implements
 		}
 	}
 
+	/**
+	 * Records deletion of a {@link Vertex}. This edit does not represent
+	 * automatic deletions of incident edges, those are recorded in a special
+	 * {@link CompoundEdit}.
+	 */
 	protected class DeleteVertexEdit extends DeleteElementEdit {
 		private static final long serialVersionUID = -4787860787912669387L;
 
 		public DeleteVertexEdit(Vertex v) {
-			super(Event.DELETE_VERTEX, v);
+			super(GraphEditEvent.DELETE_VERTEX, v);
 		}
 
 		@Override
@@ -260,7 +357,7 @@ public class GraphUndoManager extends UndoManager implements
 			int oldVersion = elementVersion;
 			elementId = v.getId();
 			elementVersion = ++version;
-			versions.put(v, elementVersion);
+			versions.put(elementId, elementVersion);
 			// System.out.println("\tWAS v" + oldId + "-" + oldVersion +
 			// ", is v"
 			// + elementId + "-" + elementVersion);
@@ -277,16 +374,30 @@ public class GraphUndoManager extends UndoManager implements
 		}
 	}
 
+	/**
+	 * Records deletion of an {@link Edge}.
+	 * 
+	 * Upon undo, the resurrected edge is put into the correct places in the
+	 * incidence sequences. If this {@link DeleteEdgeEdit} is part of a
+	 * {@link Vertex} deletion, incidence numbers are corrected only after all
+	 * incident edges are resurrected.
+	 */
 	protected class DeleteEdgeEdit extends DeleteElementEdit {
 		private static final long serialVersionUID = 6430961207052596036L;
 		private int alphaId, omegaId, alphaVersion, omegaVersion;
+		private int alphaInc, omegaInc;
 
 		public DeleteEdgeEdit(Edge e) {
-			super(Event.DELETE_EDGE, e);
+			super(GraphEditEvent.DELETE_EDGE, e);
+			assert e.isNormal();
 			alphaId = e.getAlpha().getId();
-			alphaVersion = ev(e.getAlpha());
+			alphaVersion = elementVersion(alphaId);
 			omegaId = e.getOmega().getId();
-			omegaVersion = ev(e.getOmega());
+			omegaVersion = elementVersion(omegaId);
+			TraversalContext tc = graph.setTraversalContext(null);
+			alphaInc = incidenceNumber(e.getAlpha(), e);
+			omegaInc = incidenceNumber(e.getOmega(), e.getReversedEdge());
+			graph.setTraversalContext(tc);
 		}
 
 		@Override
@@ -299,13 +410,23 @@ public class GraphUndoManager extends UndoManager implements
 			int oldVersion = elementVersion;
 			elementId = -e.getId();
 			elementVersion = ++version;
-			versions.put(e, elementVersion);
+			versions.put(elementId, elementVersion);
 			// System.out.println("\tWAS e" + (-oldId) + "-" + oldVersion
 			// + ", is e" + (-elementId) + "-" + elementVersion);
 			for (GraphEdit g = first; g != null; g = g.next) {
 				g.changeEdgeId(oldId, oldVersion, elementId, elementVersion);
 			}
 			restoreAttributes(e);
+			if (!undoDeleteVertex) {
+				TraversalContext tc = graph.setTraversalContext(null);
+				putIncidenceAt(e.getAlpha(), e, alphaInc);
+				putIncidenceAt(e.getOmega(), e.getReversedEdge(), omegaInc);
+				graph.setTraversalContext(tc);
+			} else {
+				correctIncidences.add(e);
+				correctPositions.add(alphaInc);
+				correctPositions.add(omegaInc);
+			}
 		}
 
 		@Override
@@ -339,13 +460,13 @@ public class GraphUndoManager extends UndoManager implements
 		private int oldVertexId, newVertexId, oldVertexVersion,
 				newVertexVersion;
 
-		ChangeIncidenceEdit(Event event, Edge e, Vertex oldVertex,
+		ChangeIncidenceEdit(GraphEditEvent event, Edge e, Vertex oldVertex,
 				Vertex newVertex) {
 			super(event, e);
 			oldVertexId = oldVertex.getId();
-			oldVertexVersion = ev(oldVertex);
+			oldVertexVersion = elementVersion(oldVertexId);
 			newVertexId = newVertex.getId();
-			newVertexVersion = ev(newVertex);
+			newVertexVersion = elementVersion(newVertexId);
 		}
 
 		@Override
@@ -353,7 +474,7 @@ public class GraphUndoManager extends UndoManager implements
 			super.undo();
 			Vertex v = graph.getVertex(oldVertexId);
 			Edge e = graph.getEdge(elementId);
-			if (event == Event.CHANGE_ALPHA) {
+			if (event == GraphEditEvent.CHANGE_ALPHA) {
 				e.setAlpha(v);
 			} else {
 				e.setOmega(v);
@@ -365,7 +486,7 @@ public class GraphUndoManager extends UndoManager implements
 			super.redo();
 			Vertex v = graph.getVertex(newVertexId);
 			Edge e = graph.getEdge(elementId);
-			if (event == Event.CHANGE_ALPHA) {
+			if (event == GraphEditEvent.CHANGE_ALPHA) {
 				e.setAlpha(v);
 			} else {
 				e.setOmega(v);
@@ -394,7 +515,7 @@ public class GraphUndoManager extends UndoManager implements
 
 		ChangeAttributeEdit(AttributedElement<?, ?> el, String attributeName,
 				Object oldValue, Object newValue) {
-			super(Event.CHANGE_ATTRIBUTE, el);
+			super(GraphEditEvent.CHANGE_ATTRIBUTE, el);
 			this.attributeName = attributeName;
 			this.oldValue = oldValue;
 			this.newValue = newValue;
@@ -486,25 +607,36 @@ public class GraphUndoManager extends UndoManager implements
 	}
 
 	@Override
-	public void beforeDeleteVertex(Vertex element) {
+	public void beforeDeleteVertex(Vertex v) {
 		if (!isWorking()) {
-			assert deleteVertexEditStack == null;
-			deleteVertexEditStack = new Stack<GraphEdit>();
-			deleteVertexEditStack.push(new DeleteVertexEdit(element));
+			assert deleteVertexCompound == null;
+			deleteVertexEdit = new DeleteVertexEdit(v);
+			deleteVertexCompound = new CompoundEdit() {
+				private static final long serialVersionUID = -2775885260931823100L;
+
+				@Override
+				public void undo() throws CannotUndoException {
+					undoDeleteVertex = true;
+					try {
+						super.undo();
+						restoreIncidencePositions();
+					} finally {
+						undoDeleteVertex = false;
+					}
+				}
+			};
+			addEdit(deleteVertexCompound);
 		}
 	}
 
 	@Override
-	public void afterDeleteVertex(VertexClass elementClass) {
+	public void afterDeleteVertex(VertexClass vc) {
 		if (!isWorking()) {
-			assert deleteVertexEditStack != null;
-			CompoundEdit deleteVertexCompound = new CompoundEdit();
-			while (!deleteVertexEditStack.isEmpty()) {
-				deleteVertexCompound.addEdit(deleteVertexEditStack.pop());
-			}
+			assert deleteVertexCompound != null;
+			deleteVertexCompound.addEdit(deleteVertexEdit);
 			deleteVertexCompound.end();
-			addEdit(deleteVertexCompound);
-			deleteVertexEditStack = null;
+			deleteVertexCompound = null;
+			deleteVertexEdit = null;
 		}
 	}
 
@@ -541,8 +673,8 @@ public class GraphUndoManager extends UndoManager implements
 	public void afterChangeAlpha(Edge element, Vertex oldVertex,
 			Vertex newVertex) {
 		if (!isWorking()) {
-			addEdit(new ChangeIncidenceEdit(Event.CHANGE_ALPHA, element,
-					oldVertex, newVertex));
+			addEdit(new ChangeIncidenceEdit(GraphEditEvent.CHANGE_ALPHA,
+					element, oldVertex, newVertex));
 		}
 	}
 
@@ -555,8 +687,8 @@ public class GraphUndoManager extends UndoManager implements
 	public void afterChangeOmega(Edge element, Vertex oldVertex,
 			Vertex newVertex) {
 		if (!isWorking()) {
-			addEdit(new ChangeIncidenceEdit(Event.CHANGE_OMEGA, element,
-					oldVertex, newVertex));
+			addEdit(new ChangeIncidenceEdit(GraphEditEvent.CHANGE_OMEGA,
+					element, oldVertex, newVertex));
 		}
 	}
 
@@ -573,6 +705,53 @@ public class GraphUndoManager extends UndoManager implements
 		if (!isWorking()) {
 			addEdit(new ChangeAttributeEdit(element, attributeName, oldValue,
 					newValue));
+		}
+	}
+
+	private int incidenceNumber(Vertex v, Edge e) {
+		assert v != null && v.isValid();
+		assert e != null && e.isValid();
+		int n = 0;
+		for (Edge i : v.incidences()) {
+			if (i.equals(e)) {
+				return n;
+			}
+			++n;
+		}
+		return -1;
+	}
+
+	private void restoreIncidencePositions() {
+		TraversalContext tc = graph.setTraversalContext(null);
+		int i = 0;
+		for (Edge e : correctIncidences) {
+			putIncidenceAt(e.getAlpha(), e, correctPositions.get(i++));
+			putIncidenceAt(e.getOmega(), e.getReversedEdge(),
+					correctPositions.get(i++));
+		}
+		correctIncidences.clear();
+		correctPositions.clear();
+		graph.setTraversalContext(tc);
+	}
+
+	private void putIncidenceAt(Vertex v, Edge e, int p) {
+		assert v != null && v.isValid();
+		assert e != null && e.isValid();
+		assert e.getThis().equals(v);
+		Edge i = v.getFirstIncidence();
+		int n = 0;
+		while (i != null && n != p) {
+			if (!i.equals(e)) {
+				++n;
+			}
+			i = i.getNextIncidence();
+		}
+		if (i != null) {
+			if (!i.equals(e)) {
+				e.putIncidenceBefore(i);
+			}
+		} else {
+			e.putIncidenceAfter(v.getLastIncidence());
 		}
 	}
 
--- testit/de/uni_koblenz/jgralabtest/undo/UndoTest.java
+++ testit/de/uni_koblenz/jgralabtest/undo/UndoTest.java
@@ -1,29 +1,41 @@
 package de.uni_koblenz.jgralabtest.undo;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Random;
+import java.util.TreeMap;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.uni_koblenz.jgralab.Edge;
 import de.uni_koblenz.jgralab.ImplementationType;
+import de.uni_koblenz.jgralab.Vertex;
 import de.uni_koblenz.jgralab.utilities.gui.undo.GraphUndoManager;
-import de.uni_koblenz.jgralabtest.schemas.algolib.simple.SimpleEdge;
-import de.uni_koblenz.jgralabtest.schemas.algolib.simple.SimpleGraph;
-import de.uni_koblenz.jgralabtest.schemas.algolib.simple.SimpleSchema;
-import de.uni_koblenz.jgralabtest.schemas.algolib.simple.SimpleVertex;
+import de.uni_koblenz.jgralabtest.schemas.undo.Comp;
+import de.uni_koblenz.jgralabtest.schemas.undo.Link;
+import de.uni_koblenz.jgralabtest.schemas.undo.Node;
+import de.uni_koblenz.jgralabtest.schemas.undo.UndoTestGraph;
+import de.uni_koblenz.jgralabtest.schemas.undo.UndoTestSchema;
 
 public class UndoTest {
-	private SimpleGraph g;
+	private static final boolean RESPECT_GLOBAL_ORDER = false;
+	private Random rnd;
+
+	private UndoTestGraph g;
 	private GraphUndoManager mgr;
 
 	@Before
 	public void setUp() {
-		g = SimpleSchema.instance().createSimpleGraph(
+		g = UndoTestSchema.instance().createUndoTestGraph(
 				ImplementationType.STANDARD);
 		mgr = new GraphUndoManager(g);
 		g.addGraphChangeListener(mgr);
+		rnd = new Random(0);
 	}
 
 	@After
@@ -33,114 +45,197 @@ public class UndoTest {
 		g = null;
 	}
 
+	@Test
+	public void testUndoToEmptyGraph() {
+		Map<String, String> before = storeState();
+		createRandomGraph();
+		mgr.undo();
+		Map<String, String> after = storeState();
+		assertEquals(before, after);
+	}
+
+	@Test
+	public void testCascadingDelete() {
+		Comp root = g.createComp();
+		root.set_name("root");
+
+		Comp c1 = g.createComp();
+		c1.set_name("c1");
+
+		Node n1 = g.createNode();
+		n1.set_name("n1");
+		root.add_children(c1).set_name("root<>-c1");
+		root.add_children(n1).set_name("root<>-n1");
+
+		Node n2 = g.createNode();
+		n2.set_name("n2");
+		c1.add_children(n2).set_name("c1<>-n2");
+
+		Node n3 = g.createNode();
+		n3.set_name("n3");
+		c1.add_children(n3).set_name("c1<>-n3");
+
+		g.createLink(n2, root).set_name("n2-root");
+		g.createLink(n1, n3).set_name("n1-n3");
+
+		Comp c2 = g.createComp();
+		c2.set_name("c2");
+		root.add_children(c2).set_name("root<>-c2");
+
+		g.createLink(c1, c2).set_name("c1-c2");
+		mgr.discardAllEdits();
+
+		Map<String, String> initial = storeState();
+
+		c1.delete(); // removes n2, n3
+
+		Map<String, String> afterModification = storeState();
+
+		while (mgr.canUndo()) {
+			mgr.undo();
+		}
+		Map<String, String> afterUndo = storeState();
+		assertEquals(initial, afterUndo);
+
+		while (mgr.canRedo()) {
+			mgr.redo();
+		}
+		Map<String, String> afterRedo = storeState();
+		assertEquals(afterModification, afterRedo);
+
+	}
+
+	@Test
+	public void deleteVertexTest() {
+		createRandomGraph();
+		mgr.discardAllEdits();
+
+		Map<String, String> beforeModification = storeState();
+
+		ArrayList<Node> l = new ArrayList<Node>(g.getVCount());
+		for (Node v : g.getNodeVertices()) {
+			l.add(v);
+		}
+		Collections.shuffle(l, rnd);
+		for (Node v : l) {
+			v.delete();
+		}
+		Map<String, String> afterModification = storeState();
+		while (mgr.canUndo()) {
+			mgr.undo();
+		}
+		Map<String, String> afterUndo = storeState();
+		assertEquals(beforeModification, afterUndo);
+
+		while (mgr.canRedo()) {
+			mgr.redo();
+		}
+
+		Map<String, String> afterRedo = storeState();
+		assertEquals(afterModification, afterRedo);
+
+	}
+
+	@Test
+	public void deleteEdgeTest() {
+		createRandomGraph();
+		mgr.discardAllEdits();
+
+		Map<String, String> beforeModification = storeState();
+
+		ArrayList<Link> l = new ArrayList<Link>(g.getECount());
+		for (Link e : g.getLinkEdges()) {
+			l.add(e);
+		}
+		Collections.shuffle(l, rnd);
+		for (Link e : l) {
+			e.delete();
+		}
+
+		Map<String, String> afterModification = storeState();
+		while (mgr.canUndo()) {
+			mgr.undo();
+		}
+		Map<String, String> afterUndo = storeState();
+		assertEquals(beforeModification, afterUndo);
+
+		while (mgr.canRedo()) {
+			mgr.redo();
+		}
+
+		Map<String, String> afterRedo = storeState();
+		assertEquals(afterModification, afterRedo);
+	}
+
 	private char vn = 'a';
 
-	private SimpleVertex cv() {
-		SimpleVertex v = g.createSimpleVertex();
+	private Node cv() {
+		Node v = g.createNode();
 		v.set_name("" + vn);
 		++vn;
 		return v;
 	}
 
-	private SimpleEdge ce(SimpleVertex a, SimpleVertex o) {
-		SimpleEdge e = g.createSimpleEdge(a, o);
-		e.set_name(a.get_name() + o.get_name());
+	private int en = 0;
+
+	private Link ce(Node a, Node o) {
+		++en;
+		Link e = g.createLink(a, o);
+		e.set_name(a.get_name() + o.get_name() + en);
 		return e;
 	}
 
-	public void createRandomGraph(long seed) {
-		Random rnd = new Random(seed);
-		ArrayList<SimpleVertex> v = new ArrayList<SimpleVertex>();
-		for (int i = 1; i <= 10; ++i) {
-			v.add(cv());
-		}
-		for (int i = 1; i <= 20; ++i) {
-			SimpleVertex a = v.get(rnd.nextInt(v.size()));
-			SimpleVertex o = v.get(rnd.nextInt(v.size()));
-			ce(a, o);
-		}
-		mgr.discardAllEdits();
-	}
-
-	@Test
-	public void deleteVertexTest() {
-		createRandomGraph(0);
-		print();
-	}
-
-	// @Test
-	public void testCreateVertex() {
-		g.set_name("world");
-		{// create gap in vertex ids
-			SimpleVertex v1 = cv();
-			SimpleVertex v2 = cv();
-			SimpleVertex v3 = cv();
-			SimpleEdge e = ce(v1, v3);
-			v3.set_name("o3");
-			e.setAlpha(v2);
-			e.set_name(e.getAlpha().get_name() + e.getOmega().get_name());
-			e.setOmega(v2);
-			e.set_name(e.getAlpha().get_name() + e.getOmega().get_name());
-			v2.delete();
-		}
-
-		print();
-		g.getVertex(3).setAttribute("name",
-				"former " + g.getVertex(3).getAttribute("name"));
-		g.getVertex(3).delete();
-
-		mgr.undo(); // delete
-		mgr.undo();
-		print();
-		mgr.redo();
-		mgr.redo();
-		print();
-
-		SimpleVertex v2 = cv();
-		print();
-
-		SimpleVertex v1 = cv();
-		print();
-
-		SimpleEdge e = ce(v1, v2);
-		print();
-
-		SimpleVertex v3 = cv();
-		print();
-
-		g.deleteVertex(v1);
-		print();
-
-		System.out
-				.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-
-		while (mgr.canUndo()) {
-			mgr.undo();
-			print();
-		}
-		System.out
-				.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-		while (mgr.canRedo()) {
-			mgr.redo();
-			print();
-		}
-	}
-
-	private void print() {
-		System.out
-				.println("-----------------------------------------------------");
-		// System.out.println(mgr);
-		System.out.println("graph " + g.get_name());
-		for (SimpleVertex v : g.getSimpleVertexVertices()) {
-			System.out.println("\tv" + v.getId() + " " + v.get_name());
-			for (SimpleEdge e : v.getSimpleEdgeIncidences()) {
-				System.out.println("\t\te" + e.getId() + " " + e.get_name());
-
+	private void createRandomGraph() {
+		mgr.beginEdit("create random graph");
+		try {
+			ArrayList<Node> v = new ArrayList<Node>();
+			for (int i = 1; i <= 10; ++i) {
+				v.add(cv());
 			}
+			for (int i = 1; i <= 30; ++i) {
+				Node a = v.get(rnd.nextInt(v.size()));
+				Node o = v.get(rnd.nextInt(v.size()));
+				ce(a, o);
+			}
+		} finally {
+			mgr.endEdit();
 		}
-		for (SimpleEdge e : g.getSimpleEdgeEdges()) {
-			System.out.println("\te" + e.getId() + "(v" + e.getAlpha().getId()
-					+ " -> v" + e.getOmega().getId() + ") " + e.get_name());
+	}
+
+	private Map<String, String> storeState() {
+		System.out
+				.println("-------------------------------------------------------------");
+		Map<String, String> state = new TreeMap<String, String>();
+		@SuppressWarnings("unused")
+		int i = 0;
+		for (Vertex v : g.vertices()) {
+			++i;
+			String key = "v" + v.getAttribute("name")
+					+ (RESPECT_GLOBAL_ORDER ? "-" + i : "");
+			StringBuilder sb = new StringBuilder().append("v").append(
+					v.getAttribute("name"));
+			for (Edge e : v.incidences()) {
+				sb.append(" ").append(e.isNormal() ? "+" : "-")
+						.append(e.getAttribute("name"));
+			}
+			String val = sb.toString();
+			state.put(key, val);
+			System.out.println(v + "\t" + val);
 		}
+		i = 0;
+		for (Edge e : g.edges()) {
+			++i;
+			String key = "e" + e.getAttribute("name")
+					+ (RESPECT_GLOBAL_ORDER ? "-" + i : "");
+			String val = "e" + e.getAttribute("name") + " "
+					+ e.getAlpha().getAttribute("name") + "->"
+					+ e.getOmega().getAttribute("name");
+			state.put(key, val);
+			System.out.println(e + "\t" + val);
+		}
+		// for (String key : state.keySet()) {
+		// System.out.println(key + "\t" + state.get(key));
+		// }
+		return state;
 	}
 }
